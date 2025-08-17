833e97bd365b3e3dcc9bc54b6cd882cc470667bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:33 +0000

    Add package greenideas/0.4.0/greenideas-0.4.0-py3-none-any.whl

---
+++ packages/greenideas/greenideas-0.4.0-py3-none-any.whl/greenideas/parts_of_speech/pos_node.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from typing import List, Self
+
+from greenideas.attributes.attribute_set import AttributeSet
+from greenideas.parts_of_speech.pos_types import POSType
+
+
+@dataclass
+class POSNode:
+    type: POSType
+    children: List[Self] = field(default_factory=list)
+    attributes: AttributeSet = field(default_factory=AttributeSet)
+
+    def __str__(self):
+        children = (
+            f"[{', '.join(str(child) for child in self.children)}]"
+            if self.children
+            else None
+        )
+        return f"{self.type.name}{children if children else ''}"
