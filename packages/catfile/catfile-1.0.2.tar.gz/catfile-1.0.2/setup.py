9737d17ee0b6f62782542258ec52cea74ad2e6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:31 +0000

    Add package catfile/1.0.2/catfile-1.0.2.tar.gz

---
+++ packages/catfile/catfile-1.0.2.tar.gz/catfile-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    description = fh.read()
+
+setup(
+    name='catfile',
+    version='1.0.2',
+    packages=find_packages(),
+    author='Johann',
+    install_requires=[
+        'argparse',
+        'shutil',
+        'os',
+    ],
+    entry_points={
+        'console_scripts': [
+            'catfile=catfile.main:main',
+        ],
+    },
+    long_description=description,
+    long_description_content_type='text/markdown',)
