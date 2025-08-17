9361ff57c67bb6c0e64c4c683dc957507b27bf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:44 +0000

    Add package mcp-deep-research/0.1.2/mcp_deep_research-0.1.2-py3-none-any.whl

---
+++ packages/mcp-deep-research/mcp_deep_research-0.1.2-py3-none-any.whl/mcp_deep_research/__init__.py
@@ -0,0 +1,7 @@
+"""An MCP (Model-Context-Protocol) Server for Deep Academic Research"""
+
+__version__ = "0.1.2"
+
+from .__main__ import search_scholar_papers
+from .__main__ import fetch_md
+from .__main__ import fetch_paper
