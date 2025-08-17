ee550d43c43590178c90cf7bee1c97224356d396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:24 +0000

    Add package anise/0.6.8/anise-0.6.8-cp311-cp311-win32.whl

---
+++ packages/anise/anise-0.6.8-cp311-cp311-win32.whl/anise/__init__.py
@@ -0,0 +1,28 @@
+from anise._anise import (
+    Aberration,
+    Almanac,
+    MetaAlmanac,
+    MetaFile,
+    exec_gui,
+    time,
+    astro,
+    constants,
+    rotation,
+    utils,
+)
+
+__all__ = [
+    # modules
+    "astro",
+    "constants",
+    "time",
+    "rotation",
+    "utils",
+    # root
+    "Aberration",
+    "Almanac",
+    "MetaAlmanac",
+    "MetaFile",
+    # functions
+    "exec_gui",
+]
