b6b4950bada41d5ecdfbb7c9af5ce1774edfa758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:33 +0000

    Add package vujade/0.0.0/vujade-0.0.0.tar.gz

---
+++ packages/vujade/vujade-0.0.0.tar.gz/vujade-0.0.0/vujade/__init__.py
@@ -0,0 +1,14 @@
+"""
+Developer: vujadeyoon
+Email: vujadeyoon@gmail.com
+Github: https://github.com/vujadeyoon/vujade-python
+
+Title: __init__.py
+Description: A method-wrapper for the package, vujade.
+"""
+
+
+__date__ = '250817'
+__version__ = '0.0.0'
+__all__ = [
+    ]
