5bdf9d2070ed0690cacb9fba7166645fd4174cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:44 +0000

    Add package nichexpert/0.1.0/nichexpert-0.1.0.tar.gz

---
+++ packages/nichexpert/nichexpert-0.1.0.tar.gz/nichexpert-0.1.0/setup.py
@@ -0,0 +1,37 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+# Package meta-data.
+NAME = 'nichexpert'
+DESCRIPTION = 'A tool to identify niche for spatial transcriptomics data.'
+EMAIL = '599568651@qq.com'
+URL="https://github.com/YANG-ERA/NicheXpert"
+AUTHOR ='Jiyuan Yang'
+VERSION = '0.1.0'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+	license='MIT',
+    description=DESCRIPTION,
+	url=URL,
+    long_description_content_type="text/markdown",
+    long_description=long_description, 
+    packages=find_packages(),
+    install_requires=[
+    ],
+    python_requires=">=3.9"
+    
+)
