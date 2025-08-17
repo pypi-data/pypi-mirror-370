635f45c34689e0261f7e7e78c667ddacc86250b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:29 +0000

    Add package hyperspex/0.2.0/hyperspex-0.2.0.tar.gz

---
+++ packages/hyperspex/hyperspex-0.2.0.tar.gz/hyperspex-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperspex",
+    version="0.2.0",
+    author="Adrien Rousseau",
+    description="A Python package to help 3D hyperspectral data exploration through a Graphical User Interface.",
+    packages=find_packages(),
+    package_data={
+        'hyperspex': ['gui/*.ui', 'gui/style/*.qss', 'gui/colorbar/*.ui'], # Include all .ui files in the 'ui' subdirectory
+    },
+    install_requires=[
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "lmfit",
+    ],
+)
