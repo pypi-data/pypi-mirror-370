42ea999f595c14dd01db60e862d79abf33b979d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:45 +0000

    Add package llmbrix/0.1.3/llmbrix-0.1.3-py3-none-any.whl

---
+++ packages/llmbrix/llmbrix-0.1.3-py3-none-any.whl/llmbrix/exceptions.py
@@ -0,0 +1,12 @@
+class PromptRenderError(Exception):
+    """
+    Raised when render or partial_render receives invalid or mismatched variables.
+    """
+
+    pass
+
+
+class OpenAIResponseError(Exception):
+    """
+    Raised when OpenAI API call fails and returns error field instead of a response.
+    """
