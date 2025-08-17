9a87c9864cdd754acd005f0704aa7e66e725701c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-17 14:03:20 +0000

    Add package inventree-brother-plugin/2.0.1/inventree_brother_plugin-2.0.1.tar.gz

---
+++ packages/inventree-brother-plugin/inventree_brother_plugin-2.0.1.tar.gz/inventree_brother_plugin-2.0.1/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+from inventree_brother.version import BROTHER_PLUGIN_VERSION
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="inventree-brother-plugin",
+
+    version=BROTHER_PLUGIN_VERSION,
+
+    author="Oliver Walters",
+
+    author_email="oliver.henry.walters@gmail.com",
+
+    description="Brother label printer plugin for InvenTree",
+
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
+
+    keywords="inventree label printer printing inventory",
+
+    url="https://github.com/inventree/inventree-brother-plugin",
+
+    license="MIT",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+        'brother-ql-inventree>=1.1',
+    ],
+
+    setup_requires=[
+        "wheel",
+        "twine",
+    ],
+
+    python_requires=">=3.9",
+
+    entry_points={
+        "inventree_plugins": [
+            "BrotherLabeLPlugin = inventree_brother.brother_plugin:BrotherLabelPlugin"
+        ]
+    },
+)
