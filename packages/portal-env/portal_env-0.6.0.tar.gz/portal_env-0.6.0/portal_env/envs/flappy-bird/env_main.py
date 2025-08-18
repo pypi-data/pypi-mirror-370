9796ae3db1cb94265f06aba3b0b08ed6124829a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 03:32:05 +0000

    Add package portal-env/0.6.0/portal_env-0.6.0.tar.gz

---
+++ packages/portal-env/portal_env-0.6.0.tar.gz/portal_env-0.6.0/portal_env/envs/flappy-bird/env_main.py
@@ -0,0 +1,18 @@
+from portal_env import EnvSidePortal
+import gymnasium
+import os
+os.environ.setdefault("SDL_VIDEODRIVER", "dummy")
+os.environ.setdefault("SDL_AUDIODRIVER", "dummy")
+os.environ.setdefault("PYGAME_HIDE_SUPPORT_PROMPT", "1")
+import flappy_bird_env  # noqa
+from functools import partial
+
+
+def main():
+    env_factory = partial(gymnasium.make, render_mode="rgb_array")
+    portal = EnvSidePortal(env_factory=env_factory)
+    portal.start()
+
+
+if __name__ == '__main__':
+    main()
