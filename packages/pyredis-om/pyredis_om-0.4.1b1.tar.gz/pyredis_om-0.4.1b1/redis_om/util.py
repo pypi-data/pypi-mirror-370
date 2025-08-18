5f8c1013f319caec514b58a7b5691847fc2baf2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 03:32:13 +0000

    Add package pyredis-om/0.4.1b1/pyredis_om-0.4.1b1.tar.gz

---
+++ packages/pyredis-om/pyredis_om-0.4.1b1.tar.gz/pyredis_om-0.4.1b1/redis_om/util.py
@@ -0,0 +1,39 @@
+import inspect
+import decimal
+from typing import Any, Type, get_args
+
+
+def is_async_mode() -> bool:
+    def f() -> None:
+        """Unasync transforms async functions in sync functions"""
+        return None
+
+    return inspect.iscoroutinefunction(f)
+
+
+ASYNC_MODE = is_async_mode()
+
+# Keep numeric helpers in sync with the asynchronous variant
+NUMERIC_TYPES = (float, int, decimal.Decimal)
+
+
+def is_numeric_type(type_: Type[Any]) -> bool:
+    try:
+        return issubclass(type_, NUMERIC_TYPES)
+    except TypeError:
+        return False
+
+
+def has_numeric_inner_type(type_: Type[Any]) -> bool:
+    """
+    Check if the type has a numeric inner type.
+    """
+    args = get_args(type_)
+
+    if not args:
+        return False
+
+    try:
+        return issubclass(args[0], NUMERIC_TYPES)
+    except TypeError:
+        return False
