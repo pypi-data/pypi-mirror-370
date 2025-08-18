bb6615c0e249ce0ec535f7664073db5023ba8409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:03:55 +0000

    Add package mandelbrot-benchmark/0.0.0/mandelbrot_benchmark-0.0.0.tar.gz

---
+++ packages/mandelbrot-benchmark/mandelbrot_benchmark-0.0.0.tar.gz/mandelbrot_benchmark-0.0.0/tests/test_dunder_main.py
@@ -0,0 +1,13 @@
+import subprocess
+import sys
+
+
+def test_can_run_as_python_module():
+    """Run the CLI as a Python module."""
+    result = subprocess.run(  # noqa: S603
+        [sys.executable, "-m", "mandelbrot_benchmark", "--help"],
+        check=True,
+        capture_output=True,
+    )
+    assert result.returncode == 0
+    assert b"mandelbrot-benchmark [OPTIONS]" in result.stdout
