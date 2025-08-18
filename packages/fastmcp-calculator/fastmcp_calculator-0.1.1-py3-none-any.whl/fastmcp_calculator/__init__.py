850561d9fc68ae5cc218cff1a25fa3185fcada25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:18:41 +0000

    Add package fastmcp-calculator/0.1.1/fastmcp_calculator-0.1.1-py3-none-any.whl

---
+++ packages/fastmcp-calculator/fastmcp_calculator-0.1.1-py3-none-any.whl/fastmcp_calculator/__init__.py
@@ -0,0 +1,5 @@
+"""
+FastMCP Calculator - 一个基于FastMCP的简单计算器包
+"""
+
+__version__ = "0.1.0"
---
+++ packages/fastmcp-calculator/fastmcp_calculator-0.1.1-py3-none-any.whl/fastmcp_calculator/__main__.py
@@ -0,0 +1,9 @@
+"""
+FastMCP Calculator 包的入口点
+"""
+
+
+from fastmcp_calculator.calculator import main
+
+if __name__ == "__main__":
+    main()
