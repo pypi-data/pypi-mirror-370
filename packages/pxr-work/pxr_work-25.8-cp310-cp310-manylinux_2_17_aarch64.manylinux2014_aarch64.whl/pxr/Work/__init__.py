91396d44fb032ea75552d3e850bcae9fcfcaf6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 06:04:10 +0000

    Add package pxr-work/25.8/pxr_work-25.8-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/pxr-work/pxr_work-25.8-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/pxr/Work/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2016 Pixar
+#
+# Licensed under the terms set forth in the LICENSE.txt file available at
+# https://openusd.org/license.
+#
+# Modified by Jeremy Retailleau.
+
+"""
+Work
+
+Allows for configuration of the system's multithreading subsystem.
+
+"""
+
+from pxr import Tf
+Tf.PreparePythonModule()
+del Tf
