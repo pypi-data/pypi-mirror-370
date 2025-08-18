d6ed0b08211d604a8d14dc7a198c46202f515f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:38 +0000

    Add package pyfemtet/1.1.0/pyfemtet-1.1.0-py3-none-any.whl

---
+++ packages/pyfemtet/pyfemtet-1.1.0-py3-none-any.whl/pyfemtet/_util/df_util.py
@@ -0,0 +1,49 @@
+from math import isnan
+import pandas as pd
+
+
+__all__ = [
+    'apply_partial_df',
+    'get_partial_df',
+]
+
+
+def get_index(df, equality_filters):
+    # 値渡しに変換
+    equality_filters = equality_filters.copy()
+
+    # フィルタ条件に一致する行のインデックスを取得
+
+    # na との == での比較は常に False なので別処理するために別リストを作る
+    want_na_keys = []
+    for key, value in equality_filters.items():
+        if isinstance(value, float):
+            if isnan(value):
+                want_na_keys.append(key)
+    [equality_filters.pop(key) for key in want_na_keys]
+
+    # na 以外の比較
+    # noinspection PyUnresolvedReferences
+    out: pd.Series = (df[list(equality_filters.keys())] == pd.Series(equality_filters)).all(axis=1)
+
+    # na との比較
+    for key in want_na_keys:
+        out = out & df[key].isna()
+
+    return out
+
+
+def get_partial_df(df: pd.DataFrame, equality_filters: dict):
+    return df[get_index(df, equality_filters)]
+
+
+def apply_partial_df(df: pd.DataFrame, partial_df: pd.DataFrame, equality_filters: dict):
+
+    idx = get_index(df, equality_filters)
+
+    # インデクスに対応する部分を上書き
+    assert len(df[idx]) == len(partial_df), ('equality_filters の実行結果と'
+                                             '与えられた partial_df の長さが一致しません。')
+    df[idx] = partial_df
+
+    return df
