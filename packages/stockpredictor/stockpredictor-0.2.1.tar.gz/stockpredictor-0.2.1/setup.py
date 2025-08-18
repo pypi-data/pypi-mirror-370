09ffa1d88c7487fd596c79d0f7dc4d8919380a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:06 +0000

    Add package stockpredictor/0.2.1/stockpredictor-0.2.1.tar.gz

---
+++ packages/stockpredictor/stockpredictor-0.2.1.tar.gz/stockpredictor-0.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stockpredictor",  # PyPI name (must be unique)
+    version="0.2.1",        # Start with 0.1.0, update with each release
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "yfinance",
+        "matplotlib",
+        "scikit-learn"
+    ],
+    author="DeviprasadGurrana",
+    author_email="deviprasadgurrana@gmail.com",
+    description="A Python library for predicting stock index trends using historical data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Deviprasad/stockpredictor",
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
