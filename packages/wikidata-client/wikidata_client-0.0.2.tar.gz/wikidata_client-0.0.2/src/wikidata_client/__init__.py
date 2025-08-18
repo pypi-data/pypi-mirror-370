cbfcf7f6a954fb61b23b8a92566774057b3e9985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:04:48 +0000

    Add package wikidata-client/0.0.2/wikidata_client-0.0.2.tar.gz

---
+++ packages/wikidata-client/wikidata_client-0.0.2.tar.gz/wikidata_client-0.0.2/src/wikidata_client/__init__.py
@@ -0,0 +1,24 @@
+"""Interact with Wikidata programmatically."""
+
+from .api import get_entity_by_property, get_image, get_label, query
+from .properties import (
+    get_entity_by_arxiv,
+    get_entity_by_biorxiv,
+    get_entity_by_orcid,
+    get_entity_by_pubchem_compound,
+    get_entity_by_pubmed,
+    get_entity_by_ror,
+)
+
+__all__ = [
+    "get_entity_by_arxiv",
+    "get_entity_by_biorxiv",
+    "get_entity_by_orcid",
+    "get_entity_by_property",
+    "get_entity_by_pubchem_compound",
+    "get_entity_by_pubmed",
+    "get_entity_by_ror",
+    "get_image",
+    "get_label",
+    "query",
+]
