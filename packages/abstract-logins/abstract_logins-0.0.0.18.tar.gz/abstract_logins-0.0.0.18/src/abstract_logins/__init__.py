6589ad30aeeba666b19ebeaf635a85095d8fbee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:17:01 +0000

    Add package abstract-logins/0.0.0.18/abstract_logins-0.0.0.18.tar.gz

---
+++ packages/abstract-logins/abstract_logins-0.0.0.18.tar.gz/abstract_logins-0.0.0.18/src/abstract_logins/__init__.py
@@ -0,0 +1,6 @@
+from .functions import *
+from .imports import *
+from abstract_paths.secure_paths import *
+from .endpoints import *
+from .templates import *
+#from .chats import secure_chat_bp
---
+++ packages/abstract-logins/abstract_logins-0.0.0.18.tar.gz/abstract_logins-0.0.0.18/src/abstract_logins/endpoints/settings/__init__.py
@@ -0,0 +1,1 @@
+from .routes import secure_settings_bp,secure_endpoints_bp
