88c895dfdc48c090ac44ce31c037d3d44737bcad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:33 +0000

    Add package dslash/0.7.2/dslash-0.7.2-py3-none-any.whl

---
+++ packages/dslash/dslash-0.7.2-py3-none-any.whl/dslash/__init__.py
@@ -0,0 +1,34 @@
+"""A library to supplement Discord.py by adding support for slash commands."""
+from .choices import Choice, Choices
+from .client import CommandClient
+from .commands import SlashCommandInvokeError
+from .groups import CommandGroup, CommandSubGroup, subcommand
+from .options import Channel, Mentionable
+from .permissions import (
+    allow_roles,
+    allow_users,
+    disallow_roles,
+    disallow_users,
+    global_permissions,
+    guild_permissions,
+)
+
+__version__ = "0.7.2"
+__all__ = (
+    "__version__",
+    "CommandClient",
+    "CommandGroup",
+    "CommandSubGroup",
+    "subcommand",
+    "Channel",
+    "Mentionable",
+    "Choices",
+    "Choice",
+    "allow_roles",
+    "allow_users",
+    "disallow_roles",
+    "disallow_users",
+    "guild_permissions",
+    "global_permissions",
+    "SlashCommandInvokeError",
+)
