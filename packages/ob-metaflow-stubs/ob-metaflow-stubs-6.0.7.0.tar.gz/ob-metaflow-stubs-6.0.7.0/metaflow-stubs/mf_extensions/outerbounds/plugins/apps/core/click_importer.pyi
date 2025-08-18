f79adb68fcc94a75e2b69933d3db51d76abd1c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:25 +0000

    Add package ob-metaflow-stubs/6.0.7.0/ob-metaflow-stubs-6.0.7.0.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.7.0.tar.gz/ob-metaflow-stubs-6.0.7.0/metaflow-stubs/mf_extensions/outerbounds/plugins/apps/core/click_importer.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.17.1.0+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-18T05:26:57.989859                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ......_vendor import click as metaflow_click
+from ......_vendor import click as click
+
