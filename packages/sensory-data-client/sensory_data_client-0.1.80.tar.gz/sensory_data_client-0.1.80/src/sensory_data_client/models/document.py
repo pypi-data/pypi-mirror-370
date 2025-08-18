73370f1720c8794c5a039c9202da1bfd9b1eca38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:05:13 +0000

    Add package sensory-data-client/0.1.80/sensory_data_client-0.1.80.tar.gz

---
+++ packages/sensory-data-client/sensory_data_client-0.1.80.tar.gz/sensory_data_client-0.1.80/src/sensory_data_client/models/document.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timezone
+from typing import Optional, List
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, Field
+
+    
+class DocumentMetadata(BaseModel):
+    processing_status: Optional[str] = None
+    image_object_paths: Optional[List[str]] = None
+    extra: Optional[dict] = None
+
+class DocumentCreate(BaseModel):
+    user_document_id: str
+    name: str
+    owner_id: UUID
+    access_group_id: Optional[UUID] = None 
+    
+    metadata: Optional[DocumentMetadata] = None
+
+class DocumentInDB(DocumentCreate):
+    id: UUID = Field(default_factory=uuid4)
+    created: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
+    edited: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
+    # Поля из связанной таблицы stored_files
+    is_sync_enabled: bool
+    extension: str
+    doc_type: str
+    content_hash: str
+    object_path: str
+
+    class Config:
+        from_attributes = True 
