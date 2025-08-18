4b5e008abc8236d8f0d26cebbc3742efba6727b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:00 +0000

    Add package ErisPulse/2.2.0.dev2/erispulse-2.2.0.dev2-py3-none-any.whl

---
+++ packages/ErisPulse/erispulse-2.2.0.dev2-py3-none-any.whl/ErisPulse/Core/Event/message.py
@@ -0,0 +1,84 @@
+"""
+ErisPulse 消息处理模块
+
+提供基于装饰器的消息事件处理功能
+
+{!--< tips >!--}
+1. 支持私聊、群聊消息分类处理
+2. 支持@消息特殊处理
+3. 支持自定义条件过滤
+{!--< /tips >!--}
+"""
+
+from .base import BaseEventHandler
+from typing import Callable, Dict, Any
+
+class MessageHandler:
+    def __init__(self):
+        self.handler = BaseEventHandler("message", "message")
+    
+    def on_message(self, priority: int = 0):
+        """
+        消息事件装饰器
+        
+        :param priority: 处理器优先级
+        :return: 装饰器函数
+        """
+        def decorator(func: Callable):
+            self.handler.register(func, priority)
+            return func
+        return decorator
+    
+    def on_private_message(self, priority: int = 0):
+        """
+        私聊消息事件装饰器
+        
+        :param priority: 处理器优先级
+        :return: 装饰器函数
+        """
+        def condition(event: Dict[str, Any]) -> bool:
+            return event.get("detail_type") == "private"
+        
+        def decorator(func: Callable):
+            self.handler.register(func, priority, condition)
+            return func
+        return decorator
+    
+    def on_group_message(self, priority: int = 0):
+        """
+        群聊消息事件装饰器
+        
+        :param priority: 处理器优先级
+        :return: 装饰器函数
+        """
+        def condition(event: Dict[str, Any]) -> bool:
+            return event.get("detail_type") == "group"
+        
+        def decorator(func: Callable):
+            self.handler.register(func, priority, condition)
+            return func
+        return decorator
+    
+    def on_at_message(self, priority: int = 0):
+        """
+        @消息事件装饰器
+        
+        :param priority: 处理器优先级
+        :return: 装饰器函数
+        """
+        def condition(event: Dict[str, Any]) -> bool:
+            # 检查消息中是否有@机器人
+            message_segments = event.get("message", [])
+            self_id = event.get("self", {}).get("user_id")
+            
+            for segment in message_segments:
+                if segment.get("type") == "mention" and segment.get("data", {}).get("user_id") == self_id:
+                    return True
+            return False
+        
+        def decorator(func: Callable):
+            self.handler.register(func, priority, condition)
+            return func
+        return decorator
+
+message = MessageHandler()
