149b60a17510ab0aec0fd044c853d51af8ce684f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:05:11 +0000

    Add package opennote/2.0.2/opennote-2.0.2.tar.gz

---
+++ packages/opennote/opennote-2.0.2.tar.gz/opennote-2.0.2/opennote/__init__.py
@@ -0,0 +1,46 @@
+from opennote.sync_sdk import OpennoteClient, Opennote
+from opennote.async_sdk import AsyncOpennote, AsyncOpennoteClient
+from opennote.base_client import (
+    OpennoteAPIError,
+    AuthenticationError,
+    InsufficientCreditsError,
+    ValidationError,
+    RateLimitError,
+    ServerError,
+)
+from opennote.api_types import (
+    VideoCreateJobRequest,
+    VideoCreateJobResponse,
+    VideoJobStatusResponse,
+    JournalsResponse,
+    JournalContentResponse,
+    VideoAPIRequestMessage,
+    VideoResponse,
+    VideoSource,
+    ApiResponseJournal,
+)
+
+__all__ = [
+    # Clients
+    "OpennoteClient",
+    "Opennote",
+    "AsyncOpennote",
+    "AsyncOpennoteClient",
+    # Exceptions
+    "OpennoteAPIError",
+    "AuthenticationError",
+    "InsufficientCreditsError",
+    "ValidationError",
+    "RateLimitError",
+    "ServerError",
+    # Types
+    "VideoCreateJobRequest",
+    "VideoCreateJobResponse",
+    "VideoJobStatusResponse",
+    "JournalsResponse",
+    "JournalContentResponse",
+    "VideoAPIRequestMessage",
+    "VideoResponse",
+    "VideoSource",
+    "ApiResponseJournal",
+]
