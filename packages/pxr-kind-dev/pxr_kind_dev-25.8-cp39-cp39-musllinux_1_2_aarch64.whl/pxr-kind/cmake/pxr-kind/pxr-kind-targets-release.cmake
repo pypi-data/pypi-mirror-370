1413387f71535f096abb703e29dd117b7d9ba12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:21 +0000

    Add package pxr-kind-dev/25.8/pxr_kind_dev-25.8-cp39-cp39-musllinux_1_2_aarch64.whl

---
+++ packages/pxr-kind-dev/pxr_kind_dev-25.8-cp39-cp39-musllinux_1_2_aarch64.whl/pxr-kind/cmake/pxr-kind/pxr-kind-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pxr::pyKind" for configuration "Release"
+set_property(TARGET pxr::pyKind APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::pyKind PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-kind/lib/python/pxr/Kind/_kind.so"
+  IMPORTED_SONAME_RELEASE "_kind.so"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::pyKind )
+list(APPEND _cmake_import_check_files_for_pxr::pyKind "${_IMPORT_PREFIX}/pxr-kind/lib/python/pxr/Kind/_kind.so" )
+
+# Import target "pxr::kind" for configuration "Release"
+set_property(TARGET pxr::kind APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::kind PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-kind/lib/libPxrKind.so"
+  IMPORTED_SONAME_RELEASE "libPxrKind.so"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::kind )
+list(APPEND _cmake_import_check_files_for_pxr::kind "${_IMPORT_PREFIX}/pxr-kind/lib/libPxrKind.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
