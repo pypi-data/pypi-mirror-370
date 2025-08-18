38a7031707f9618061f22ab2cfcf11b1b085aa2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:17:11 +0000

    Add package scCAMEL/0.42b0/sccamel-0.42b0.tar.gz

---
+++ packages/scCAMEL/sccamel-0.42b0.tar.gz/sccamel-0.42b0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scCAMEL",
+    version="0.42b",
+    author="Yizhou Hu",
+    author_email="yizhou.hu@ki.se",
+    description="scCAMEL: single cell Cross- Annotation and Multimodal Estimation on Lineage trajectory;License: GPL version 3;Developed by: Yizhou Hu, Department of Laboratory Medicine, Karolinska Institutet;Tutorials and other informations in :https://sccamel.readthedocs.io/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://sccamel.readthedocs.io/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+install_requires=[
+        'arboreto','openpyxl','skorch','kneed','captum','scanpy'],
+)
---
+++ packages/scCAMEL/sccamel-0.42b0.tar.gz/sccamel-0.42b0/src/scCAMEL/__init__.py
@@ -0,0 +1,5 @@
+import scCAMEL as scm
+from scCAMEL import CamelPrefiltering
+from scCAMEL import CamelSwapline
+from scCAMEL import CamelEvo
+from scCAMEL import CamelVicuna
