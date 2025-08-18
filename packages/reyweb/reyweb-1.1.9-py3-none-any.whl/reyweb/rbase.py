f12cfdfba91152d94902365c50e4536f18b47570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:16:30 +0000

    Add package reyweb/1.1.9/reyweb-1.1.9-py3-none-any.whl

---
+++ packages/reyweb/reyweb-1.1.9-py3-none-any.whl/reyweb/rbase.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2025-07-17 22:32:37
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Base methods.
+"""
+
+
+from reykit.rbase import Base
+
+
+__all__ = (
+    'BaseWeb',
+)
+
+
+class BaseWeb(Base):
+    """
+    Web base type.
+    """
