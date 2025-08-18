bf82cf013b490541ebb506b1216d08e56491c97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:04:34 +0000

    Add package basicsystem/0.3.0/basicsystem-0.3.0.tar.gz

---
+++ packages/basicsystem/basicsystem-0.3.0.tar.gz/basicsystem-0.3.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="basicsystem",
+    version="0.3.0",
+    author="Mehmet",
+    description="Saf Python ile gelişmiş oyun motoru altyapısı (tek dosya)",
+    py_modules=["basicsystem"],  # Tek dosya
+    python_requires=">=3.9",
+)
