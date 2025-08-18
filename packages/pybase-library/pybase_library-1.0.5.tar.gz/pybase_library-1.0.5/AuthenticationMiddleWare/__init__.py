e53b3c6fb79cd02125f476a060b2721de843f083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:04:48 +0000

    Add package pybase-library/1.0.5/pybase_library-1.0.5.tar.gz

---
+++ packages/pybase-library/pybase_library-1.0.5.tar.gz/pybase_library-1.0.5/AuthenticationMiddleWare/__init__.py
@@ -0,0 +1,7 @@
+from .middleware import  AuthMiddleware as AuthMiddleware, AuthConfig as AuthConfig, JWTConfig as JWTConfig, Settings as Settings
+from .middleware import get_current_user as get_current_user_claims
+from .middleware import setup_openapi_security as setup_swagger_security
+from .access_control import AccessController, RoleEnum, PermissionEnum
+from .logger import setup_logging
+
+__all__ = ["AuthMiddleware", "AuthConfig", "JWTConfig", "Settings", "AccessController", "RoleEnum", "PermissionEnum", "get_current_user_claims", "setup_swagger_security", "setup_logging"]
