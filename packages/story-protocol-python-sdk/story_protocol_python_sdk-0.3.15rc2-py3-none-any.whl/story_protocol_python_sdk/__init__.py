abe897b9813cd0ab298b524d84b158ec9608527c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:04:56 +0000

    Add package story-protocol-python-sdk/0.3.15rc2/story_protocol_python_sdk-0.3.15rc2-py3-none-any.whl

---
+++ packages/story-protocol-python-sdk/story_protocol_python_sdk-0.3.15rc2-py3-none-any.whl/story_protocol_python_sdk/__init__.py
@@ -0,0 +1,45 @@
+__version__ = "0.3.15-rc.2"
+
+from .resources.Dispute import Dispute
+from .resources.IPAccount import IPAccount
+from .resources.IPAsset import IPAsset
+from .resources.License import License
+from .resources.Royalty import Royalty
+from .resources.WIP import WIP
+from .story_client import StoryClient
+from .types.common import AccessPermission
+from .types.resource.IPAsset import RegistrationResponse
+from .utils.constants import (
+    DEFAULT_FUNCTION_SELECTOR,
+    MAX_ROYALTY_TOKEN,
+    ROYALTY_POLICY_LAP_ADDRESS,
+    ROYALTY_POLICY_LRP_ADDRESS,
+    ZERO_ADDRESS,
+    ZERO_FUNC,
+    ZERO_HASH,
+)
+from .utils.derivative_data import DerivativeDataInput
+from .utils.ip_metadata import IPMetadataInput
+
+__all__ = [
+    "StoryClient",
+    "IPAsset",
+    "License",
+    "Royalty",
+    "IPAccount",
+    "Dispute",
+    "WIP",
+    # Types
+    "AccessPermission",
+    "DerivativeDataInput",
+    "IPMetadataInput",
+    "RegistrationResponse",
+    # Constants
+    "ZERO_ADDRESS",
+    "ZERO_HASH",
+    "ROYALTY_POLICY_LAP_ADDRESS",
+    "ROYALTY_POLICY_LRP_ADDRESS",
+    "ZERO_FUNC",
+    "DEFAULT_FUNCTION_SELECTOR",
+    "MAX_ROYALTY_TOKEN",
+]
