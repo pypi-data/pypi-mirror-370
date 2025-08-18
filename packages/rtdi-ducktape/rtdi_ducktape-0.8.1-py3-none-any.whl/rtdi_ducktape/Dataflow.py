cb405660f1c04e60d036ed676b9e5378845c6ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:18:14 +0000

    Add package rtdi-ducktape/0.8.1/rtdi_ducktape-0.8.1-py3-none-any.whl

---
+++ packages/rtdi-ducktape/rtdi_ducktape-0.8.1-py3-none-any.whl/rtdi_ducktape/Dataflow.py
@@ -0,0 +1,32 @@
+import logging
+from logging import Logger
+from typing import Union
+
+from rtdi_ducktape.Metadata import Step, OperationalMetadata, Table
+
+
+class Dataflow:
+
+    def __init__(self, logger: Union[None, Logger] = None):
+        self.nodes = []
+        if logger is None:
+            self.logger = logging.getLogger("Dataflow")
+        else:
+            self.logger = logger
+        self.last_execution = None
+
+    def add(self, step: Step):
+        self.nodes.append(step)
+        return step
+
+    def start(self, duckdb):
+        if len(self.nodes) > 0:
+            self.last_execution = OperationalMetadata()
+            self.nodes[0].start(duckdb)
+            rows_loaded = 0
+            for node in self.nodes:
+                if isinstance(node, Table) and node.last_execution is not None:
+                    rows_loaded += node.last_execution.rows_processed
+            self.last_execution.processed(rows_loaded)
+            self.nodes[0].completed()
+            self.logger.info(f"Dataflow() - {self.last_execution}")
