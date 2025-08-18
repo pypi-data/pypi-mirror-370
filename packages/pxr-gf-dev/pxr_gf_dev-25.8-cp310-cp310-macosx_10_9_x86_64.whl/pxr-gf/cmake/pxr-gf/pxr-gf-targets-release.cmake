b2029ac3977855c329587bd7e543598b32f2f63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:07:00 +0000

    Add package pxr-gf-dev/25.8/pxr_gf_dev-25.8-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/pxr-gf-dev/pxr_gf_dev-25.8-cp310-cp310-macosx_10_9_x86_64.whl/pxr-gf/cmake/pxr-gf/pxr-gf-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pxr::pyGf" for configuration "Release"
+set_property(TARGET pxr::pyGf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::pyGf PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-gf/lib/python/pxr/Gf/_gf.so"
+  IMPORTED_SONAME_RELEASE "@rpath/_gf.so"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::pyGf )
+list(APPEND _cmake_import_check_files_for_pxr::pyGf "${_IMPORT_PREFIX}/pxr-gf/lib/python/pxr/Gf/_gf.so" )
+
+# Import target "pxr::gf" for configuration "Release"
+set_property(TARGET pxr::gf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::gf PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-gf/lib/libPxrGf.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libPxrGf.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::gf )
+list(APPEND _cmake_import_check_files_for_pxr::gf "${_IMPORT_PREFIX}/pxr-gf/lib/libPxrGf.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
