30d71fa3e5b3dd787d22723ceb99daa04de088ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:04:06 +0000

    Add package dodrio/0.3.6/dodrio-0.3.6-py3-none-any.whl

---
+++ packages/dodrio/dodrio-0.3.6-py3-none-any.whl/dodrio/__init__.py
@@ -0,0 +1,36 @@
+'''
+FilePath: /python-Dodrio/dodrio/__init__.py
+Descripttion: 
+Author: Yixiang Chen
+version: 
+Date: 2025-03-24 15:50:51
+LastEditors: Yixiang Chen
+LastEditTime: 2025-08-18 16:17:33
+'''
+
+#from . import tool
+#from . import genlist 
+#from . import afeat
+
+
+from dodrio.core.parquet_base import (
+    gen_parquet, parquet2wav
+)
+
+from dodrio.core.package_base import (
+    parquet2package, package2wav, gen_package
+)
+
+from dodrio.core.datainfo_process import gen_infodir
+
+from dodrio.genlist.gendatalist import (
+    genListDir, gen_datalist, check_func
+)
+
+from dodrio.afeat.feat_extractor import (
+    extract_feat, get_utt2spk, extract_feat_multi, extract_feat_align, extract_feat_extrainfo
+)
+
+from dodrio.tools.load_data import (
+    load_data_from_line, load_feat_single, load_audio_single, load_data_from_line_align
+)
