8bdd92c38052856e82af5e609d3641516e62941d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:05:20 +0000

    Add package fbusl/0.5.17/fbusl-0.5.17.tar.gz

---
+++ packages/fbusl/fbusl-0.5.17.tar.gz/fbusl-0.5.17/fbusl/__init__.py
@@ -0,0 +1,31 @@
+"""
+The FreeBody Universal Shader Language is a Transpiled Shader Language.
+It includes features present in many shader languages so it can
+be easily compiled into them.
+"""
+
+import sys
+
+
+class Position:
+    def __init__(self, line: int = 0, file: str = None):
+        self.line = line
+        self.file = file
+
+    def __repr__(self):
+        return f"{self.line}"
+
+def fbusl_error(msg, position: Position=Position()):
+    f = position.file
+    if position.file == None:
+        f = "Unkown File"
+    print(f'\033[91mFBUSL ERROR: {msg} in file "{f}", line {position.line}.\033[0m')
+    sys.exit()
+
+
+from fbusl import parser
+from fbusl.compiler import compile
+
+
+
+__all__ = ["fbusl_error", "parser", "compile"]
