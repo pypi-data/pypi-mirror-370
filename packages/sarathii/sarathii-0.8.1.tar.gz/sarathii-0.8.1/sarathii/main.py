5f58463947bbc19064f18fe658a66867521bc86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:35 +0000

    Add package sarathii/0.8.1/sarathii-0.8.1.tar.gz

---
+++ packages/sarathii/sarathii-0.8.1.tar.gz/sarathii-0.8.1/sarathii/main.py
@@ -0,0 +1,15 @@
+from datetime import date
+def age():
+		birthyear=2004
+		currentyear=date.today().year
+		age=currentyear-birthyear
+		print(f"AGE:{age}")
+def full_name():
+		print("NAME:VIJAYASARATHI.A")
+def dob():
+		print("DOB:02/MAY/2004")
+	
+def degree():
+		print("DEGREE:B.TECH-AI&DS")
+def fan():
+		print("THALAPATHY VIJAY")
