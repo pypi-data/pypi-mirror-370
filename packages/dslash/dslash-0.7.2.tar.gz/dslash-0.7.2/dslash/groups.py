88b70f682d573912dd06175ce2e6c13c253b93df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:33 +0000

    Add package dslash/0.7.2/dslash-0.7.2.tar.gz

---
+++ packages/dslash/dslash-0.7.2.tar.gz/dslash-0.7.2/dslash/groups.py
@@ -0,0 +1,114 @@
+"""Definitions for the class-based command (sub)group system."""
+from __future__ import annotations
+
+import re
+from typing import Any, Generic, Optional, TypeVar, Union
+
+from .client import GUILD_ID_DEFAULT, GuildID
+from .commands import (
+    ChildSlashCommand,
+    SlashCommandGroup,
+    SlashCommandSubGroup,
+    SlashSubCommand,
+    SlashSubCommandConstructor,
+)
+from .permissions import warn_permissions_deprecation
+
+
+def class_name_to_group_name(class_name: str) -> str:
+    """Convert a class name to a group name."""
+    return re.sub(r"([a-z])([A-Z0-9])", r"\1_\2", class_name).lower()
+
+
+GT = TypeVar("GT", SlashCommandGroup, SlashCommandSubGroup)
+
+
+class BaseCommandGroupMeta(type, Generic[GT]):
+    """Base metaclass for class-nased command groups."""
+
+    @classmethod
+    def _construct_group(cls, name: str, description: str, **kwargs: Any) -> GT:
+        """Construct a the actual group object."""
+        raise NotImplementedError
+
+    def __new__(
+        cls,
+        class_name: str,
+        parents: tuple[type],
+        attrs: dict[str, Any],
+        name: Optional[str] = None,
+        _base_class: bool = False,
+        **kwargs: Any,
+    ) -> Union[GT, type]:
+        """Create a new command group class."""
+        new_class = super().__new__(cls, class_name, parents, attrs)
+        if _base_class:
+            return new_class
+        group = cls._construct_group(
+            name=name or class_name_to_group_name(class_name),
+            description=attrs["__doc__"] or "No description.",
+            **kwargs,
+        )
+        # Command groups are singletons.
+        instance = new_class()
+        for attr in attrs.values():
+            if isinstance(attr, SlashSubCommand):
+                attr.prepend_params = (instance,)
+            elif not isinstance(attr, ChildSlashCommand):
+                continue
+            group.subcommands[attr.name] = attr
+        return group
+
+
+class CommandGroupMeta(BaseCommandGroupMeta[SlashCommandGroup]):
+    """Metaclass for class-based top-level command groups."""
+
+    @classmethod
+    def _construct_group(
+        cls,
+        name: str,
+        description: str,
+        guild_id: GuildID = GUILD_ID_DEFAULT,
+        # This option only exists for legacy reasons, and has no effect.
+        default_permission: bool | None = None,
+    ) -> SlashCommandGroup:
+        """Construct a the actual group object."""
+        if default_permission is not None:
+            warn_permissions_deprecation()
+        return SlashCommandGroup(
+            # The client will replace the guild ID default when the group is registered.
+            guild_id=guild_id,  # type: ignore
+            name=name,
+            description=description,
+        )
+
+
+class CommandSubGroupMeta(BaseCommandGroupMeta[SlashCommandSubGroup]):
+    """Metaclass for class-based command subgroups."""
+
+    @classmethod
+    def _construct_group(cls, name: str, description: str) -> SlashCommandSubGroup:
+        """Construct a the actual group object."""
+        return SlashCommandSubGroup(name=name, description=description)
+
+
+class CommandGroup(metaclass=CommandGroupMeta, _base_class=True):
+    """Class to inherit to build command groups.
+
+    See `CommandClient.group` for more information and examples.
+    """
+
+
+class CommandSubGroup(metaclass=CommandSubGroupMeta, _base_class=True):
+    """Class to inherit to build command subgroups."""
+
+
+def subcommand(
+    name: Optional[str] = None, description: Optional[str] = None
+) -> SlashSubCommandConstructor:
+    """Build a subcommand for within a group."""
+    return SlashSubCommandConstructor(
+        parent=None,
+        name=name,
+        description=description,
+    )
---
+++ packages/dslash/dslash-0.7.2.tar.gz/dslash-0.7.2/dslash/options.py
@@ -0,0 +1,164 @@
+"""Models to represent arguments to slash commands."""
+import types
+import typing
+from typing import Any, Optional, Type, Union
+
+import nextcord
+from nextcord.enums import Enum
+
+from .choices import Choices
+
+Channel = nextcord.abc.GuildChannel
+Mentionable = Union[nextcord.User, nextcord.Member, nextcord.Role]
+
+
+class ApplicationCommandOptionType(Enum):
+    """The type of an argument to a slash command."""
+
+    sub_command = 1
+    sub_command_group = 2
+    string = 3
+    integer = 4
+    boolean = 5
+    user = 6
+    channel = 7
+    role = 8
+    mentionable = 9
+    number = 10
+    attachment = 11
+
+
+class CommandOption:
+    """An argument to a slash command."""
+
+    def __init__(self, *, name: str, description: str, type: Type):
+        """Set up the command option."""
+        self.description = description
+        self.name = name
+        self.type, self.choices, self.required = self._get_type_metadata(type)
+
+    def _get_optional_type(self, param_type: Type) -> Optional[Type]:
+        """Return the root type if 'type' is optional."""
+        if typing.get_origin(param_type) in (typing.Union, types.UnionType):
+            args = typing.get_args(param_type)
+            if len(args) == 2 and isinstance(None, args[1]):
+                return args[0]
+        return None
+
+    def _get_type_metadata(self, type: Type) -> tuple[Type, Optional[Type[Choices]], bool]:
+        """Get the root type, associated choices, and whether it is required."""
+        if root_type := self._get_optional_type(type):
+            type = root_type
+            required = False
+        else:
+            required = True
+        if issubclass(type, Choices):
+            choices = type
+            type = choices._choice_type
+        else:
+            choices = None
+        return type, choices, required
+
+    def dump(self) -> dict[str, Any]:
+        """Get the JSON data for registering this option with the API."""
+        if self.choices:
+            choice_dump = self.choices._choice_data
+        else:
+            choice_dump = None
+        return {
+            "name": self.name,
+            "description": self.description,
+            "required": self.required,
+            "type": self.type_value.value,
+            "choices": choice_dump,
+        }
+
+    @property
+    def _is_user_type(self) -> bool:
+        """Check if the option type is 'user'."""
+        return (
+            issubclass(self.type, (nextcord.User, nextcord.Member))
+            or self.type is nextcord.abc.User
+        )
+
+    @property
+    def _is_channel_type(self) -> bool:
+        """Check if the option type is 'channel'."""
+        return (
+            issubclass(
+                self.type,
+                (nextcord.TextChannel, nextcord.VoiceChannel, nextcord.CategoryChannel),
+            )
+            or self.type is nextcord.abc.GuildChannel
+        )
+
+    @property
+    def type_value(self) -> ApplicationCommandOptionType:
+        """Get the Discord API code for the option's type."""
+        if (not self.type) or issubclass(self.type, str):
+            return ApplicationCommandOptionType.string
+        if issubclass(self.type, bool):
+            return ApplicationCommandOptionType.boolean
+        if issubclass(self.type, int):
+            return ApplicationCommandOptionType.integer
+        if issubclass(self.type, float):
+            return ApplicationCommandOptionType.number
+        if self._is_user_type:
+            return ApplicationCommandOptionType.user
+        if self._is_channel_type:
+            return ApplicationCommandOptionType.channel
+        if issubclass(self.type, nextcord.Role):
+            return ApplicationCommandOptionType.role
+        if issubclass(self.type, nextcord.Attachment):
+            return ApplicationCommandOptionType.attachment
+        if typing.get_origin(self.type) is typing.Union:  # type: ignore
+            if typing.get_args(self.type) == typing.get_args(Mentionable):
+                return ApplicationCommandOptionType.mentionable
+            raise TypeError("Union type not allowed for option type (except Mentionable).")
+        raise TypeError(f"Unsupported option type {self.type!r}.")
+
+    async def __call__(
+        self, data: Optional[dict[str, Any]], interaction: nextcord.Interaction
+    ) -> Any:
+        """Process option data from the API."""
+        if (not data) or "value" not in data:
+            return None
+        value = data["value"]
+        type = ApplicationCommandOptionType(data["type"])
+        if type in (
+            ApplicationCommandOptionType.string,
+            ApplicationCommandOptionType.boolean,
+            ApplicationCommandOptionType.integer,
+            ApplicationCommandOptionType.number,
+        ):
+            if self.choices:
+                return self.choices._get_by_value(value)
+            return value
+        if type == ApplicationCommandOptionType.attachment:
+            data = (
+                interaction.data.get("resolved", {}).get("attachments", {}).get(value)
+                if interaction.data
+                else None
+            )
+            if data:
+                return nextcord.Attachment(data=data, state=interaction._state)
+            raise ValueError("Attachment option recieved without resolved attachment.")
+        # We could use `resolved` for users/roles/channels, but working with
+        # discord.py/nextcord internals is more hassle than it's worth.
+        value = int(value)
+        if not (guild := interaction.guild):
+            raise ValueError("User/role/channel option recieved without a guild.")
+        if type == ApplicationCommandOptionType.mentionable:
+            if role := guild.get_role(value):
+                return role
+        if type in (
+            ApplicationCommandOptionType.user,
+            ApplicationCommandOptionType.mentionable,
+        ):
+            if not (user := guild.get_member(value)):
+                user = await guild.fetch_member(value)
+            return user
+        if type == ApplicationCommandOptionType.role:
+            return guild.get_role(value)
+        if type == ApplicationCommandOptionType.channel:
+            return guild.get_channel(value)
