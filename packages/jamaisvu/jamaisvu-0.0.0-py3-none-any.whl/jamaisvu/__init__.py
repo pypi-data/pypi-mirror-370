ffd406ed9a47591d40488a995e3d24443f0a53a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:58 +0000

    Add package jamaisvu/0.0.0/jamaisvu-0.0.0-py3-none-any.whl

---
+++ packages/jamaisvu/jamaisvu-0.0.0-py3-none-any.whl/jamaisvu/__init__.py
@@ -0,0 +1,14 @@
+"""
+Developer: vujadeyoon
+Email: vujadeyoon@gmail.com
+Github: https://github.com/vujadeyoon/jamaisvu-python
+
+Title: __init__.py
+Description: A method-wrapper for the package, jamaisvu.
+"""
+
+
+__date__ = '250818'
+__version__ = '0.0.0'
+__all__ = [
+    ]
