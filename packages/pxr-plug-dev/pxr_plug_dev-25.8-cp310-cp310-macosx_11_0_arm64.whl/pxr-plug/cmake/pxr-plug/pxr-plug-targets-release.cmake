8a63bbaa869a15ab6de8c852bd1dd11e91760137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:15 +0000

    Add package pxr-plug-dev/25.8/pxr_plug_dev-25.8-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pxr-plug-dev/pxr_plug_dev-25.8-cp310-cp310-macosx_11_0_arm64.whl/pxr-plug/cmake/pxr-plug/pxr-plug-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pxr::pyPlug" for configuration "Release"
+set_property(TARGET pxr::pyPlug APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::pyPlug PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-plug/lib/python/pxr/Plug/_plug.so"
+  IMPORTED_SONAME_RELEASE "@rpath/_plug.so"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::pyPlug )
+list(APPEND _cmake_import_check_files_for_pxr::pyPlug "${_IMPORT_PREFIX}/pxr-plug/lib/python/pxr/Plug/_plug.so" )
+
+# Import target "pxr::plug" for configuration "Release"
+set_property(TARGET pxr::plug APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::plug PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-plug/lib/libPxrPlug.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libPxrPlug.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::plug )
+list(APPEND _cmake_import_check_files_for_pxr::plug "${_IMPORT_PREFIX}/pxr-plug/lib/libPxrPlug.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
