b09cd547c7e4ba7b0eee31d617900e714ac55c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:04:12 +0000

    Add package yamkix/0.12.0/yamkix-0.12.0-py3-none-any.whl

---
+++ packages/yamkix/yamkix-0.12.0-py3-none-any.whl/yamkix/yamkix.py
@@ -0,0 +1,100 @@
+"""Load a yaml file and save it formatted according to some rules."""
+
+import sys
+from pathlib import Path
+from typing import TextIO
+
+from ruamel.yaml import YAML
+from ruamel.yaml.comments import CommentedBase
+from ruamel.yaml.parser import ParserError
+from ruamel.yaml.scanner import ScannerError
+
+from yamkix.comments import process_comments
+from yamkix.config import YamkixConfig
+from yamkix.errors import InvalidYamlContentError
+from yamkix.helpers import strip_leading_double_space
+from yamkix.yaml_writer import get_opinionated_yaml_writer
+
+
+def round_trip_and_format(yamkix_config: YamkixConfig) -> None:
+    """Load a file and save it formatted.
+
+    Arguments:
+        yamkix_config: The configuration for the Yamkix processing.
+
+    Returns:
+        None
+
+    Raises:
+        InvalidYamlContentError: If the YAML content is invalid.
+    """
+    yaml = get_opinionated_yaml_writer(yamkix_config)
+    yamkix_io_config = yamkix_config.io_config
+    input_file = yamkix_io_config.input
+    output_file = yamkix_io_config.output
+    dash_inwards = yamkix_config.dash_inwards
+    spaces_before_comment = yamkix_config.spaces_before_comment
+    if input_file is not None:
+        with Path(input_file).open(encoding="UTF-8") as f_input:
+            parsed = yaml.load_all(f_input.read())
+    else:
+        parsed = yaml.load_all(sys.stdin.read())
+    ready_for_dump = []
+    try:
+        # Read the parsed content to force the scanner to issue errors if any
+        ready_for_dump = list(parsed)
+
+    except (ScannerError, ParserError) as parsing_error:
+        raise InvalidYamlContentError from parsing_error
+    yamkix_dump_all(
+        one_or_more_items=ready_for_dump,
+        yaml=yaml,
+        dash_inwards=dash_inwards,
+        output_file=output_file,
+        spaces_before_comment=spaces_before_comment,
+    )
+
+
+def yamkix_dump_all(
+    one_or_more_items: list[CommentedBase],
+    yaml: YAML,
+    dash_inwards: bool,
+    output_file: str | None,
+    spaces_before_comment: int | None,
+) -> None:
+    """Dump all the documents from the input structure."""
+    # Clear the output file if it is a file and it exists
+    if output_file is not None and (output_file_path := Path(output_file)).is_file():  # Walrus baby
+        with output_file_path.open(mode="w", encoding="UTF-8") as _:
+            pass
+    for doc in one_or_more_items:
+        if output_file is None:
+            out = sys.stdout
+            yamkix_dump_one(
+                single_item=doc,
+                yaml=yaml,
+                dash_inwards=dash_inwards,
+                out=out,
+                spaces_before_comment=spaces_before_comment,
+            )
+        else:
+            with Path(output_file).open(mode="a", encoding="UTF-8") as out:
+                yamkix_dump_one(
+                    single_item=doc,
+                    yaml=yaml,
+                    dash_inwards=dash_inwards,
+                    out=out,
+                    spaces_before_comment=spaces_before_comment,
+                )
+
+
+def yamkix_dump_one(
+    single_item: CommentedBase, yaml: YAML, dash_inwards: bool, out: TextIO, spaces_before_comment: int | None
+) -> None:
+    """Dump a single document."""
+    if spaces_before_comment is not None:
+        process_comments(data=single_item, column=spaces_before_comment)  # pyright: ignore[reportArgumentType]
+    if dash_inwards and type(single_item).__name__ == "CommentedSeq":
+        yaml.dump(data=single_item, stream=out, transform=strip_leading_double_space)
+    else:
+        yaml.dump(data=single_item, stream=out)
