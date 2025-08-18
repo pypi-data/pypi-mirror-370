89c13145bfada8ba77387a79bb02f2d3a864ac20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:05:05 +0000

    Add package mc-pack-generator/0.1.1/mc_pack_generator-0.1.1.tar.gz

---
+++ packages/mc-pack-generator/mc_pack_generator-0.1.1.tar.gz/mc_pack_generator-0.1.1/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+# Caminho absoluto do diretório atual
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+# Caminho para o README.md
+readme_path = os.path.join(this_directory, "README.md")
+
+# Lê o conteúdo do README.md
+try:
+    with open(readme_path, "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+setup(
+    name='mc-pack-generator',
+    version='0.1.1',
+    author='Seu Nome',
+    description='Gerador de pacotes para Minecraft Bedrock (Behavior, Resource, Skin)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'mc-pack-generator = mc_pack_generator.gerador:main',
+            'mc-pack-publish = mc_pack_generator.publicador:main',
+        ],
+    },
+)
