65287a2c78b5b2bd81e9eafb941ae097c6546be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:39 +0000

    Add package pxr-vt-dev/25.8/pxr_vt_dev-25.8-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/pxr-vt-dev/pxr_vt_dev-25.8-cp311-cp311-macosx_11_0_arm64.whl/pxr-vt/cmake/pxr-vt/pxr-vt-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pxr::pyVt" for configuration "Release"
+set_property(TARGET pxr::pyVt APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::pyVt PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-vt/lib/python/pxr/Vt/_vt.so"
+  IMPORTED_SONAME_RELEASE "@rpath/_vt.so"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::pyVt )
+list(APPEND _cmake_import_check_files_for_pxr::pyVt "${_IMPORT_PREFIX}/pxr-vt/lib/python/pxr/Vt/_vt.so" )
+
+# Import target "pxr::vt" for configuration "Release"
+set_property(TARGET pxr::vt APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::vt PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-vt/lib/libPxrVt.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libPxrVt.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::vt )
+list(APPEND _cmake_import_check_files_for_pxr::vt "${_IMPORT_PREFIX}/pxr-vt/lib/libPxrVt.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
