5da402cbbb1a01c4c9a70e114784af156fd74e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:43 +0000

    Add package appwrite-lab/0.1.4/appwrite_lab-0.1.4.tar.gz

---
+++ packages/appwrite-lab/appwrite_lab-0.1.4.tar.gz/appwrite_lab-0.1.4/appwrite_lab/cli/list_menu.py
@@ -0,0 +1,22 @@
+from appwrite_lab import get_global_labs
+from appwrite_lab.utils import print_table
+from appwrite_lab._orchestrator import get_template_versions
+import typer
+
+list_menu = typer.Typer(name="list", rich_markup_mode=typer.rich_utils.MARKUP_MODE_RICH)
+
+labs = get_global_labs()
+
+
+@list_menu.command(name="labs", help="List resources.")
+def get_labs():
+    """List all ephemeral Appwrite instances."""
+    headers, pods = labs.orchestrator.get_formatted_labs(collapsed=True)
+    print_table(pods, headers)
+
+
+@list_menu.command()
+def versions():
+    """List all available Appwrite versions."""
+    versions = get_template_versions()
+    print_table([versions], ["Version"])
