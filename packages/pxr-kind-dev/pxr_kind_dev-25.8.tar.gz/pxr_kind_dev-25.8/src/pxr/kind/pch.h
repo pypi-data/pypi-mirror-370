5e181bb24d277f625206ad4e6d4d87a81d3f7bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:21 +0000

    Add package pxr-kind-dev/25.8/pxr_kind_dev-25.8.tar.gz

---
+++ packages/pxr-kind-dev/pxr_kind_dev-25.8.tar.gz/pxr_kind_dev-25.8/src/pxr/kind/pch.h
@@ -0,0 +1,54 @@
+// Copyright 2017 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Modified by Jeremy Retailleau.
+
+#define TF_MAX_ARITY 7
+#include "pxr/kind/pxr.h"
+#include <pxr/arch/defines.h>
+#if defined(ARCH_OS_LINUX)
+#include <unistd.h>
+#endif
+#include <algorithm>
+#include <any>
+#include <atomic>
+#include <cinttypes>
+#include <cmath>
+#include <complex>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iosfwd>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <memory>
+#include <mutex>
+#include <ostream>
+#include <set>
+#include <sstream>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string>
+#include <sys/types.h>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#ifdef PXR_PYTHON_SUPPORT_ENABLED
+#include <pxr/tf/pySafePython.h>
+#endif // PXR_PYTHON_SUPPORT_ENABLED
---
+++ packages/pxr-kind-dev/pxr_kind_dev-25.8.tar.gz/pxr_kind_dev-25.8/src/python/wrapRegistry.cpp
@@ -0,0 +1,41 @@
+// Copyright 2016 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Modified by Jeremy Retailleau.
+
+#include <pxr/kind/pxr.h>
+#include <pxr/kind/registry.h>
+#include <pxr/tf/pyResultConversions.h>
+#include <pxr/tf/pySingleton.h>
+
+#include <pxr/boost/python.hpp>
+
+KIND_NAMESPACE_USING_DIRECTIVE
+
+using namespace pxr_boost::python;
+
+void wrapRegistry()
+{
+    typedef KindRegistry This;
+    typedef TfWeakPtr<KindRegistry> ThisPtr;
+
+   class_<This, ThisPtr, noncopyable>("Registry", no_init)
+        .def(TfPySingleton())
+        .def("HasKind", &This::HasKind)
+        .staticmethod("HasKind")
+        .def("GetAllKinds", &This::GetAllKinds,
+             return_value_policy<TfPySequenceToList>())
+        .staticmethod("GetAllKinds")
+        .def("GetBaseKind", &This::GetBaseKind)
+        .staticmethod("GetBaseKind")
+        .def("IsA", &This::IsA)
+        .def("IsModel", &This::IsModel)
+        .def("IsGroup", &This::IsGroup)
+        .def("IsAssembly", &This::IsAssembly)
+        .def("IsComponent", &This::IsComponent)
+        .def("IsSubComponent", &This::IsSubComponent)
+        .staticmethod("IsA")
+        ;
+}
