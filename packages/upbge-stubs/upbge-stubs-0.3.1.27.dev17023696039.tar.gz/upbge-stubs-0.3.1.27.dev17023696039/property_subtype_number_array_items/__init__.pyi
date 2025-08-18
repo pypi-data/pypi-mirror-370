1f6d8d394a58f9a0b9b5d232b0c17315fa5a1dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:06:55 +0000

    Add package upbge-stubs/0.3.1.27.dev17023696039/upbge-stubs-0.3.1.27.dev17023696039.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev17023696039.tar.gz/upbge-stubs-0.3.1.27.dev17023696039/property_subtype_number_array_items/__init__.pyi
@@ -0,0 +1,69 @@
+"""
+
+
+Property Subtype Number Array Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:COLOR:           
+  Linear Color.
+
+  Color in the linear space.
+
+:TRANSLATION:     
+  Translation.
+
+  Color in the gamma corrected space.
+
+:DIRECTION:       
+  Direction.
+
+:VELOCITY:        
+  Velocity.
+
+:ACCELERATION:    
+  Acceleration.
+
+:MATRIX:          
+  Matrix.
+
+:EULER:           
+  Euler Angles.
+
+  Euler rotation angles in radians.
+
+:QUATERNION:      
+  Quaternion.
+
+  Quaternion rotation (affects NLA blending).
+
+:AXISANGLE:       
+  Axis-Angle.
+
+  Angle and axis to rotate around.
+
+:XYZ:             
+  XYZ.
+
+:XYZ_LENGTH:      
+  XYZ Length.
+
+:COLOR_GAMMA:     
+  Gamma-Corrected Color.
+
+:COORDINATES:     
+  Coordinates.
+
+:LAYER:           
+  Layer.
+
+:LAYER_MEMBER:    
+  Layer Member.
+
+:NONE:            
+  None.
+
+.. _rna-enum-property-subtype-number-array-items:
+
+"""
+
+import typing
