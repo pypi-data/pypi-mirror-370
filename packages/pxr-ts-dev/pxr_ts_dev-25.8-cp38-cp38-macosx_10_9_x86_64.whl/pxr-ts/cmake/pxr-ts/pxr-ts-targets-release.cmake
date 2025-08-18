e99519bf97c428ad0fbc8b0ca1062c017a93777d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:04:23 +0000

    Add package pxr-ts-dev/25.8/pxr_ts_dev-25.8-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/pxr-ts-dev/pxr_ts_dev-25.8-cp38-cp38-macosx_10_9_x86_64.whl/pxr-ts/cmake/pxr-ts/pxr-ts-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pxr::pyTs" for configuration "Release"
+set_property(TARGET pxr::pyTs APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::pyTs PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-ts/lib/python/pxr/Ts/_ts.so"
+  IMPORTED_SONAME_RELEASE "@rpath/_ts.so"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::pyTs )
+list(APPEND _cmake_import_check_files_for_pxr::pyTs "${_IMPORT_PREFIX}/pxr-ts/lib/python/pxr/Ts/_ts.so" )
+
+# Import target "pxr::ts" for configuration "Release"
+set_property(TARGET pxr::ts APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::ts PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-ts/lib/libPxrTs.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libPxrTs.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::ts )
+list(APPEND _cmake_import_check_files_for_pxr::ts "${_IMPORT_PREFIX}/pxr-ts/lib/libPxrTs.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
