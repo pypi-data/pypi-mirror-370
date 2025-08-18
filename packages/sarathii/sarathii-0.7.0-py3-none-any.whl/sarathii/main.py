8014b59c09f9dec10c4a7adbf4bb36e420aa4e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:17:12 +0000

    Add package sarathii/0.7.0/sarathii-0.7.0-py3-none-any.whl

---
+++ packages/sarathii/sarathii-0.7.0-py3-none-any.whl/sarathii/main.py
@@ -0,0 +1,23 @@
+from datetime import date
+def age():
+
+    # Enter your birth year
+    birth_year = 2004  # change this to your year of        birth
+
+    # Get the current year
+    current_year = date.today().year
+
+    # Calculate age
+    age = current_year - birth_year
+
+    print(f"AGE : {age}")
+    
+
+def full_name():
+	print("NAME : VIJAYASARATHI.A")
+def dob():
+	print("DOB : 02/MAY/2004")
+def degree():
+	print("DEGREE : B.TECH-AI&DS")
+def fan():
+	print("FAN : THALAPATHI VIJAY")
