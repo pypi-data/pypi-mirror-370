40420203465ca344192057adcbe18c892589896c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 06:04:43 +0000

    Add package lila-framework/0.6.4/lila_framework-0.6.4.tar.gz

---
+++ packages/lila-framework/lila_framework-0.6.4.tar.gz/lila_framework-0.6.4/lila/core/app.py
@@ -0,0 +1,42 @@
+from starlette.applications import Starlette
+from lila.core.responses import HTMLResponse
+from starlette.middleware.cors import CORSMiddleware
+from lila.core.logger import Logger
+from lila.core.middleware import ErrorHandlerMiddleware
+from lila.core.templates import render
+import os 
+
+class App(Starlette):
+    def __init__(self, debug: bool = False, routes=[], cors=None):
+        super().__init__(debug=debug, routes=routes)
+        try:
+        
+            self.add_exception_handler(404, self._404_page)
+            self.add_middleware(ErrorHandlerMiddleware)
+            
+            if cors:
+                self.add_middleware(
+                    CORSMiddleware,
+                    allow_origins=cors.get("origin", ["*"]),
+                    allow_credentials=cors.get("allow_credentials", True),
+                    allow_methods=cors.get("allow_methods", ["*"]),
+                    allow_headers=cors.get("allow_headers", ["*"]),
+                )
+
+            Logger.info("Application started successfully")
+        except Exception as e:
+            Logger.error(f"Error: {e}", exception=e)
+
+    
+    async def _404_page(self, request, exc):
+        excluded_words={"public", "service-worker", "css","js","img","favicon.ico"}
+
+        if all(word not in request.url.path for word in excluded_words ):
+            Logger.warning(await Logger.request(request=request))
+        
+        verify_404 = os.path.exists(f"templates/html/404.html")
+        if not verify_404:
+            html_content = "<h1>404 Not Found</h1><p>The requested resource was not found on this server.</p>"
+            return HTMLResponse(content=html_content, status_code=404) 
+        response = render(request=request, template="404")
+        return response
