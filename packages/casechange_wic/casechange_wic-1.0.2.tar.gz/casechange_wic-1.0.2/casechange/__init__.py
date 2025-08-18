551bb8df9c9aa77c1524e5a77671a3fa975cf79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:25 +0000

    Add package casechange_wic/1.0.2/casechange_wic-1.0.2.tar.gz

---
+++ packages/casechange_wic/casechange_wic-1.0.2.tar.gz/casechange_wic-1.0.2/casechange/__init__.py
@@ -0,0 +1,13 @@
+"""Case Change: A package for changing the case of strings."""
+__version__ = "1.0.2"
+
+def case_change(text: str, mode: str) -> str:
+    if mode == "snake":
+        return text.replace(" ", "_").lower()
+    elif mode == "kebab":
+        return text.replace(" ", "-").lower()
+    elif mode == "camel":
+        words = text.split(" ")
+        return words[0].lower() + "".join(word.capitalize() for word in words[1:])
+    else:
+        raise ValueError("Invalid mode. Choose 'snake', 'kebab', or 'camel'.")
