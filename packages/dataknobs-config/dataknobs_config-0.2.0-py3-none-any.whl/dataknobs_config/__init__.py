228c31a622f5a5663ee211bd19b396d6387d0d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:46 +0000

    Add package dataknobs-config/0.2.0/dataknobs_config-0.2.0-py3-none-any.whl

---
+++ packages/dataknobs-config/dataknobs_config-0.2.0-py3-none-any.whl/dataknobs_config/__init__.py
@@ -0,0 +1,26 @@
+"""DataKnobs Config Package
+
+A modular, reusable configuration system for composable settings.
+"""
+
+from .builders import ConfigurableBase, FactoryBase
+from .config import Config
+from .exceptions import (
+    ConfigError,
+    ConfigNotFoundError,
+    InvalidReferenceError,
+    ValidationError,
+)
+from .substitution import VariableSubstitution
+
+__version__ = "0.1.0"
+__all__ = [
+    "Config",
+    "ConfigError",
+    "ConfigNotFoundError",
+    "ConfigurableBase",
+    "FactoryBase",
+    "InvalidReferenceError",
+    "ValidationError",
+    "VariableSubstitution",
+]
