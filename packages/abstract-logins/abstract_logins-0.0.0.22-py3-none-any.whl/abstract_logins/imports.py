66ea4b39bca5299da11952d4f703acc2abe73833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:19:37 +0000

    Add package abstract-logins/0.0.0.22/abstract_logins-0.0.0.22-py3-none-any.whl

---
+++ packages/abstract-logins/abstract_logins-0.0.0.22-py3-none-any.whl/abstract_logins/imports.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+from typing import *
+from pathlib import Path
+from dotenv import load_dotenv
+from datetime import datetime
+import os,jwt,logging,glob,shutil, hashlib, argparse, time, sys,secrets, string
+from abstract_utilities.time_utils import *
+from abstract_security import get_env_value
+from functools import wraps
+from flask import has_request_context,Request
+from abstract_ai import get_json_call_response,is_number
+from abstract_queries import *
+from abstract_database import insert_any_combo,update_any_combo,fetch_any_combo,remove_any_combo
+from abstract_paths.secure_paths import *
+from abstract_flask import *
+from abstract_flask import (
+    get_request_info,
+    get_ip_addr,
+    get_user_name,
+    get_user_filename,
+    get_safe_subdir,
+    get_subdir,
+    get_request_safe_filename,
+    get_request_filename,
+    get_request_file,
+    get_request_files
+    )
+from abstract_utilities import get_logFile,SingletonMeta
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
+    QLabel, QLineEdit, QPushButton, QComboBox, QCheckBox, QMessageBox
+)
+from PyQt5.QtCore import QTimer, Qt
+from abstract_logins.functions import *
+from abstract_logins.functions.routes import (
+    verify_password,
+    ensure_users_table_exists
+)
+from abstract_logins.functions.auth_utils.user_store.table_utils.users_utils import (
+    get_existing_users,
+    get_user,
+    add_or_update_user
+    )
+from abstract_logins.functions.auth_utils.query_utils import *
+from abstract_logins.functions import *
+from .config import *
+logger = get_logFile(__name__)
+
+
+
+
+
