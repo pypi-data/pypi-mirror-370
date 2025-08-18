8bd068815e20be76b2baacbe0afa6b0cdc3c638c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:16:34 +0000

    Add package payloop/0.3.2/payloop-0.3.2.tar.gz

---
+++ packages/payloop/payloop-0.3.2.tar.gz/payloop-0.3.2/payloop/_utils.py
@@ -0,0 +1,15 @@
+def merge_chunk(data: dict, chunk: dict):
+    for key, chunk_value in chunk.items():
+        if key in data:
+            data_value = data[key]
+
+            if isinstance(data_value, list) and isinstance(chunk_value, list):
+                data[key].extend(chunk_value)
+            elif isinstance(data_value, dict) and isinstance(chunk_value, dict):
+                merge_chunk(data_value, chunk_value)
+            else:
+                data[key] = chunk_value
+        else:
+            data[key] = chunk_value
+
+    return data
