37b87107429ca88d4401eb73dbc053a038218f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:07:00 +0000

    Add package splitter-mr/0.6.1/splitter_mr-0.6.1-py3-none-any.whl

---
+++ packages/splitter-mr/splitter_mr-0.6.1-py3-none-any.whl/splitter_mr/embedding/__init__.py
@@ -0,0 +1,4 @@
+from .base_embedding import BaseEmbedding
+from .embeddings import AzureOpenAIEmbedding, OpenAIEmbedding
+
+__all__ = ["BaseEmbedding", "AzureOpenAIEmbedding", "OpenAIEmbedding"]
---
+++ packages/splitter-mr/splitter_mr-0.6.1-py3-none-any.whl/splitter_mr/embedding/embeddings/__init__.py
@@ -0,0 +1,4 @@
+from .azure_openai_embedding import AzureOpenAIEmbedding
+from .openai_embedding import OpenAIEmbedding
+
+__all__ = ["AzureOpenAIEmbedding", "OpenAIEmbedding"]
