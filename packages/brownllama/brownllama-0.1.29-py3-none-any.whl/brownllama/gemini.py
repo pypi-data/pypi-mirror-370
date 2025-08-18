bb2d4f2a0545e094e3900433cd6b7315021e499f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:06:53 +0000

    Add package brownllama/0.1.29/brownllama-0.1.29-py3-none-any.whl

---
+++ packages/brownllama/brownllama-0.1.29-py3-none-any.whl/brownllama/gemini.py
@@ -0,0 +1,36 @@
+"""
+Google AI Studio.
+
+This module provides a class for generating responses, chat using the GenAI API.
+"""
+
+from google import genai
+
+
+class GenAI:
+    """A class for generating responses using the GenAI API."""
+
+    def __init__(self, api_key: str) -> None:
+        """
+        Initialize the GenAIResponseGenerator class.
+
+        Args:
+            api_key (str): The API key for accessing the GenAI API.
+
+        """
+        genai.configure(api_key=api_key)
+        self.client = genai.Client()
+        self.model = "gemini-2.5-flash"
+
+    def generate_response(self, prompt: str) -> str:
+        """
+        Generate a response using the GenAI API.
+
+        Args:
+            prompt (str): The prompt for generating the response.
+
+        Returns:
+            The generated response.
+
+        """
+        return self.client.models.generate_content(model=self.model, contents=prompt)
