b4ac2789052efe38f46edc51654638836950a2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:04:26 +0000

    Add package taxdumpy/1.1.4/taxdumpy-1.1.4-py3-none-any.whl

---
+++ packages/taxdumpy/taxdumpy-1.1.4-py3-none-any.whl/taxdumpy/__init__.py
@@ -0,0 +1,38 @@
+"""Taxdumpy: A Python package for parsing NCBI taxdump database and resolving taxonomy lineage."""
+
+__version__ = "1.1.4"
+
+from taxdumpy.taxdb import TaxDb
+from taxdumpy.taxsqlite import TaxSQLite
+from taxdumpy.taxon import Taxon
+from taxdumpy.functions import upper_rank_id
+from taxdumpy.basic import (
+    TaxdumpyError,
+    TaxDbError,
+    TaxidError,
+    TaxRankError,
+    TaxdumpFileError,
+    DatabaseCorruptionError,
+    ValidationError,
+)
+from taxdumpy.database import TaxonomyDatabase, create_database
+
+__all__ = [
+    # Exceptions
+    "TaxdumpyError",
+    "TaxDbError",
+    "TaxidError",
+    "TaxRankError",
+    "TaxdumpFileError",
+    "DatabaseCorruptionError",
+    "ValidationError",
+    # Database classes
+    "TaxonomyDatabase",
+    "TaxDb",
+    "TaxSQLite",
+    "Taxon",
+    # Factory functions
+    "create_database",
+    # utilities
+    "upper_rank_id",
+]
