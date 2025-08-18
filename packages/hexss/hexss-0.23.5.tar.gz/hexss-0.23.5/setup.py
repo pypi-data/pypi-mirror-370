c6b4dac1ba0c56deebf85e731eabd353feec2f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 16:04:11 +0000

    Add package hexss/0.23.5/hexss-0.23.5.tar.gz

---
+++ packages/hexss/hexss-0.23.5.tar.gz/hexss-0.23.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from hexss import __version__ as version
+
+setup(
+    name='hexss',
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    url='https://github.com/hexs/hexss',
+    license="MIT",
+    entry_points={
+        'console_scripts': [
+            'hexss_camera_server = hexss.server.camera_server:run',
+            'hexss_file_manager_server = hexss.server.file_manager_server:run',
+            'hexss_robot_server = hexss.control_robot.server:main',
+            "hexss = hexss.__main__:main",
+        ]
+    }
+)
