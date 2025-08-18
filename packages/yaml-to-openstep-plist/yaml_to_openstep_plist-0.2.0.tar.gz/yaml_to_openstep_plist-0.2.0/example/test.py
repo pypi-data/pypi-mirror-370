cbca7f702c005f4aa9ab2ce2ea877ae2c96b78ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 04:18:48 +0000

    Add package yaml-to-openstep-plist/0.2.0/yaml_to_openstep_plist-0.2.0.tar.gz

---
+++ packages/yaml-to-openstep-plist/yaml_to_openstep_plist-0.2.0.tar.gz/yaml_to_openstep_plist-0.2.0/example/test.py
@@ -0,0 +1,9 @@
+import yaml_to_openstep_plist
+import yaml
+
+if __name__ == "__main__":
+    # Load YAML data from a file
+    yaml_to_openstep_plist.to_yaml_file("input.yaml", "output.plist")
+    
+
+
