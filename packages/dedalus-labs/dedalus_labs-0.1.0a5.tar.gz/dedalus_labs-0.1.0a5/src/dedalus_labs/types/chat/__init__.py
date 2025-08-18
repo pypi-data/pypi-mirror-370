137414ec31b8e7ddf2669fba66ce01f489088644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:04:21 +0000

    Add package dedalus-labs/0.1.0a5/dedalus_labs-0.1.0a5.tar.gz

---
+++ packages/dedalus-labs/dedalus_labs-0.1.0a5.tar.gz/dedalus_labs-0.1.0a5/src/dedalus_labs/types/chat/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .top_logprob import TopLogprob as TopLogprob
+from .stream_chunk import StreamChunk as StreamChunk
+from .completion_create_params import CompletionCreateParams as CompletionCreateParams
+from .chat_completion_token_logprob import ChatCompletionTokenLogprob as ChatCompletionTokenLogprob
