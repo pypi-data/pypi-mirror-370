6526113296539711d3c5821101a3a189d3b9afbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:06:53 +0000

    Add package extra-streamlit-components/0.1.81/extra_streamlit_components-0.1.81.tar.gz

---
+++ packages/extra-streamlit-components/extra_streamlit_components-0.1.81.tar.gz/extra_streamlit_components-0.1.81/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="extra_streamlit_components",
+    version="0.1.81",
+    author="Mohamed Abdou",
+    author_email="matex512@gmail.com",
+    description="An all-in-one place, to find complex or just natively unavailable components on streamlit.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mohamed-512/Extra-Streamlit-Components",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["Python", "Streamlit", "React", "JavaScript"],
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 1.40.1",
+    ],
+)
