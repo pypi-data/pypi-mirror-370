a1148bf5ae40264ee897ec5f7cf5e6379f71c9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:04:15 +0000

    Add package hduq/0.0.3/hduq-0.0.3.tar.gz

---
+++ packages/hduq/hduq-0.0.3.tar.gz/hduq-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hduq',
+    version='0.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'hduq.assets': ['*']},
+    # entry_points={
+    #     'console_scripts': [
+    #         'hduq=hduq.cli:main',
+    #     ],
+    # },
+    install_requires=[
+        'numpy',
+        'scipy',
+        'Pillow',
+    ],
+    license='GPLv3',
+    license_files='LICENSE',
+    python_requires='>=3.9',
+    description='HDUQ Python utils',
+    author='Chao-Ning Hu',
+)
