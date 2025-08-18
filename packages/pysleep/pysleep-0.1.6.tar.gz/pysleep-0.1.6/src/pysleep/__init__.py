b9da5f381baae52edc736e81d6403173f753e7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:02 +0000

    Add package pysleep/0.1.6/pysleep-0.1.6.tar.gz

---
+++ packages/pysleep/pysleep-0.1.6.tar.gz/pysleep-0.1.6/src/pysleep/__init__.py
@@ -0,0 +1,29 @@
+"""
+pysleep
+=====
+
+Provides
+  1. Definition of the sleep in python ecosystem
+  2. Metrics used in medical field
+"""
+
+from .hypnogram_report import generate_hypnogram_report
+from .label import APNEA, DEEP, HYPOPNEA, LIGHT, NO_EVENT, REM, SNORE, WAKE
+from .stat import SleepStat, SleepStatDelta
+
+__version__ = "0.1.6"
+
+
+__all__ = [
+    "APNEA",
+    "DEEP",
+    "HYPOPNEA",
+    "LIGHT",
+    "NO_EVENT",
+    "REM",
+    "SNORE",
+    "WAKE",
+    "SleepStat",
+    "SleepStatDelta",
+    "generate_hypnogram_report",
+]
