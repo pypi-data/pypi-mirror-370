92a4db38e4a9bba76aa47aedef6f9066922fd72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:04:32 +0000

    Add package hockey-scraper-by-max/0.0.1.dev83/hockey_scraper_by_max-0.0.1.dev83.tar.gz

---
+++ packages/hockey-scraper-by-max/hockey_scraper_by_max-0.0.1.dev83.tar.gz/hockey_scraper_by_max-0.0.1.dev83/hockey_scraper/src/core/output_handler.py
@@ -0,0 +1,3 @@
+# hockey_scraper/src/core/output_handler.py
+# Output handler for the hockey scraper project
+# This file contains functions for handling output data, including saving to CSV and JSON formats.
