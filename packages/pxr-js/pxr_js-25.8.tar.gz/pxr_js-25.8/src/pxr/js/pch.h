3f01dea358c215ae777935922b32b744136f083c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:06:54 +0000

    Add package pxr-js/25.8/pxr_js-25.8.tar.gz

---
+++ packages/pxr-js/pxr_js-25.8.tar.gz/pxr_js-25.8/src/pxr/js/pch.h
@@ -0,0 +1,42 @@
+// Copyright 2017 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Modified by Jeremy Retailleau.
+
+#define TF_MAX_ARITY 7
+#include "pxr/js/pxr.h"
+#include <pxr/arch/defines.h>
+#if defined(ARCH_OS_LINUX)
+#include <unistd.h>
+#endif
+#include <algorithm>
+#include <any>
+#include <atomic>
+#include <cinttypes>
+#include <cmath>
+#include <cstdarg>
+#include <cstdint>
+#include <cstring>
+#include <iosfwd>
+#include <iostream>
+#include <list>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <memory>
+#include <optional>
+#include <set>
+#include <sstream>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string>
+#include <sys/types.h>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
+#include <variant>
+#include <vector>
