f32259be2dfc4fb15b13dd649053d67e968aaad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:04:17 +0000

    Add package pybase-library/1.0.0/pybase_library-1.0.0.tar.gz

---
+++ packages/pybase-library/pybase_library-1.0.0.tar.gz/pybase_library-1.0.0/AuthenticationMiddleWare/settings.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+from typing import List
+
+class Settings(BaseSettings):
+    public_key_url: str = "https://staging-iam.gt.vng.vn"
+    algorithm: str = "RS256"
+    token_header: str = "authorization"
+    token_prefix: str = "Bearer "
+    cache_enabled: bool = True
+    cache_ttl_seconds: int = 3600
+    excluded_paths: List[str] = ["/", "/docs", "/redoc", "/openapi.json", "/health", "/favicon.ico", "/mock-token"]
+    unauthorized_message: str = "Token không hợp lệ"
