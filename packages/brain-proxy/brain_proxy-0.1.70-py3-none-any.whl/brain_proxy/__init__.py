f7f80858bd5e13cb58a1dac4603eb5da606145c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:08:21 +0000

    Add package brain-proxy/0.1.70/brain_proxy-0.1.70-py3-none-any.whl

---
+++ packages/brain-proxy/brain_proxy-0.1.70-py3-none-any.whl/brain_proxy/__init__.py
@@ -0,0 +1,5 @@
+from .brain_proxy import BrainProxy
+from .brain_proxy_langchain import BrainProxyLangChainModel
+from .tools import tool
+
+__all__ = ['BrainProxy', 'BrainProxyLangChainModel', 'tool']
