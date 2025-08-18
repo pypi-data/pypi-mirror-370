290def88e37a74f3f73bfea411c9366d5c07684f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 05:08:21 +0000

    Add package pytensorlite/0.1.2/pytensorlite-0.1.2.tar.gz

---
+++ packages/pytensorlite/pytensorlite-0.1.2.tar.gz/pytensorlite-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pytensorlite",
+    version="0.1.2",
+    packages=find_packages(),
+    description="Lightweight tensor utilities for Python AI modules.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sami Haddadi",
+    author_email="garnet_smitham18209@vonju.org",
+    url="https://github.com/93dk99-ui/pytensorlite",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.6",
+)
