4d838a851982223c874be2a578e156dc25330ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 15:05:22 +0000

    Add package bgboxmaker/0.1/bgboxmaker-0.1.tar.gz

---
+++ packages/bgboxmaker/bgboxmaker-0.1.tar.gz/bgboxmaker-0.1/bgboxmaker/__init__.py
@@ -0,0 +1,3 @@
+def main():
+    print("This is BG Boxmaker")
+    return
---
+++ packages/bgboxmaker/bgboxmaker-0.1.tar.gz/bgboxmaker-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bgboxmaker',
+    version='0.1',
+    description='Board Game Tuck Box Generator',
+    url='https://github.com/thetalorian/bgboxmake',
+    author='Samuel Plum',
+    author_email='splum@taloria.com',
+    license='MIT',
+    packages=['bgboxmaker'],
+    zip_safe=False)
