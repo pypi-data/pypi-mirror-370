126440c4c6406cecea1948682ba5067bbd2ea27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 06:04:11 +0000

    Add package pxr-work-dev/25.8/pxr_work_dev-25.8-cp310-cp310-win_amd64.whl

---
+++ packages/pxr-work-dev/pxr_work_dev-25.8-cp310-cp310-win_amd64.whl/pxr-work/cmake/pxr-work/pxr-work-targets-release.cmake
@@ -0,0 +1,28 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pxr::pyWork" for configuration "Release"
+set_property(TARGET pxr::pyWork APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::pyWork PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-work/lib/python/pxr/Work/_work.pyd"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::pyWork )
+list(APPEND _cmake_import_check_files_for_pxr::pyWork "${_IMPORT_PREFIX}/pxr-work/lib/python/pxr/Work/_work.pyd" )
+
+# Import target "pxr::work" for configuration "Release"
+set_property(TARGET pxr::work APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pxr::work PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/pxr-work/lib/PxrWork.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/pxr-work/lib/PxrWork.dll"
+  )
+
+list(APPEND _cmake_import_check_targets pxr::work )
+list(APPEND _cmake_import_check_files_for_pxr::work "${_IMPORT_PREFIX}/pxr-work/lib/PxrWork.lib" "${_IMPORT_PREFIX}/pxr-work/lib/PxrWork.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
