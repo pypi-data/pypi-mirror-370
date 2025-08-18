7220f8dc4e68ec26903f7c2351f59a1c229e0391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-18 14:05:19 +0000

    Add package lasagna-ai/0.18.0/lasagna_ai-0.18.0-py3-none-any.whl

---
+++ packages/lasagna-ai/lasagna_ai-0.18.0-py3-none-any.whl/lasagna/__init__.py
@@ -0,0 +1,61 @@
+from .types import *
+
+from .agent_runner import run
+
+from .agent_util import (
+    make_model_binder,
+    make_partial_model_binder,
+    recursive_extract_messages,
+    extract_last_message,
+    flat_messages,
+    parallel_runs,
+    chained_runs,
+    extraction,
+    override_system_prompt,
+    strip_tool_calls_and_results,
+    strip_all_but_last_human_message,
+    recursive_sum_costs,
+    noop_callback,
+    MessageExtractor,
+    build_standard_message_extractor,
+    default_message_extractor,
+    build_simple_agent,
+    build_extraction_agent,
+    build_agent_chainer,
+    build_agent_router,
+    build_static_output_agent,
+)
+
+from .easy import (
+    easy_ask,
+    easy_extract,
+)
+
+__all__ = [
+    'run',
+    'make_model_binder',
+    'make_partial_model_binder',
+    'recursive_extract_messages',
+    'extract_last_message',
+    'flat_messages',
+    'parallel_runs',
+    'chained_runs',
+    'extraction',
+    'override_system_prompt',
+    'strip_tool_calls_and_results',
+    'strip_all_but_last_human_message',
+    'recursive_sum_costs',
+    'noop_callback',
+    'MessageExtractor',
+    'build_standard_message_extractor',
+    'default_message_extractor',
+    'build_simple_agent',
+    'build_extraction_agent',
+    'build_agent_chainer',
+    'build_agent_router',
+    'build_static_output_agent',
+    'easy_ask',
+    'easy_extract',
+]
+
+__version__ = "0.18.0"
