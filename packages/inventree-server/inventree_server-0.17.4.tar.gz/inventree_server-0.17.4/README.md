a35ffaef53c1c9f5cba710a9d9cb2cbd6333571a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 06:05:44 +0000

    Add package inventree-server/0.17.4/inventree_server-0.17.4.tar.gz

---
+++ packages/inventree-server/inventree_server-0.17.4.tar.gz/inventree_server-0.17.4/README.md
@@ -0,0 +1,3 @@
+Dummy project to set contstraints for the InvenTree plugin resolution.
+
+This package is empty, do not install it.
---
+++ packages/inventree-server/inventree_server-0.17.4.tar.gz/inventree_server-0.17.4/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("This is a dummy, you do not need to install it!")
+
+
+if __name__ == "__main__":
+    main()
