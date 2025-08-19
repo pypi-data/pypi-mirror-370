3d5828ef6e43f8c9ba7f11e5db155f2b846c5cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:20 +0000

    Add package pyapi-service-kit/0.0.8/pyapi_service_kit-0.0.8-py3-none-any.whl

---
+++ packages/pyapi-service-kit/pyapi_service_kit-0.0.8-py3-none-any.whl/pyapi_service_kit/nats/config.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List
+
+@dataclass
+class StreamConfig:
+    name: str
+    options: Dict[str, Any]
+    recreate_if_exists: bool = False
+
+
+@dataclass
+class NatsConfig:
+    servers: List[str]
+    options: Dict[str, Any]
+    streams: List[StreamConfig]
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> "NatsConfig":
+        return cls(
+            streams=[
+                StreamConfig(**stream)
+                for stream in data["streams"]
+                if isinstance(stream, dict)
+            ],
+            options=data.get("options", {}),
+            servers=data.get("servers", []),
+        )
