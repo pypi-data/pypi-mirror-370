c09a315c6dea1cb1721a591f806809d7abde53f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:06:38 +0000

    Add package second-module/0.2/second_module-0.2.tar.gz

---
+++ packages/second-module/second_module-0.2.tar.gz/second_module-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,Extension,find_packages
+
+setup(
+    name="second_module",
+    version="0.2",
+    packages=find_packages(),
+    description="My second module for practice",
+)
