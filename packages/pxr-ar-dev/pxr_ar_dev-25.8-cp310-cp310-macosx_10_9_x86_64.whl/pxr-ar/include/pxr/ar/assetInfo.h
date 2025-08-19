45a9625e695965bbf37053c8ce2782315da6e790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 07:04:13 +0000

    Add package pxr-ar-dev/25.8/pxr_ar_dev-25.8-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/pxr-ar-dev/pxr_ar_dev-25.8-cp310-cp310-macosx_10_9_x86_64.whl/pxr-ar/include/pxr/ar/assetInfo.h
@@ -0,0 +1,78 @@
+// Copyright 2016 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Modified by Jeremy Retailleau.
+
+#ifndef PXR_AR_ASSET_INFO_H
+#define PXR_AR_ASSET_INFO_H
+
+/// \file ar/assetInfo.h
+
+#include "pxr/ar/pxr.h"
+#include "pxr/ar/api.h"
+#include <pxr/tf/hash.h>
+#include <pxr/vt/value.h>
+#include <string>
+
+AR_NAMESPACE_OPEN_SCOPE
+
+/// \class ArAssetInfo
+///
+/// Contains information about a resolved asset.
+///
+class ArAssetInfo 
+{
+public:
+    /// Version of the resolved asset, if any.
+    std::string version;
+
+    /// The name of the asset represented by the resolved
+    /// asset, if any.
+    std::string assetName;
+
+    /// \deprecated
+    /// The repository path corresponding to the resolved asset.
+    std::string repoPath;
+
+    /// Additional information specific to the active plugin
+    /// asset resolver implementation.
+    VtValue resolverInfo;
+};
+
+template <class HashState>
+void TfHashAppend(HashState& h, const ArAssetInfo& info)
+{
+    h.Append(info.version, info.assetName, info.repoPath, info.resolverInfo);
+}
+
+inline
+size_t hash_value(const ArAssetInfo& info)
+{
+    return TfHash()(info);
+}
+
+/// \relates ArAssetInfo
+inline
+void swap(ArAssetInfo& lhs, ArAssetInfo& rhs)
+{
+    lhs.version.swap(rhs.version);
+    lhs.assetName.swap(rhs.assetName);
+    lhs.repoPath.swap(rhs.repoPath);
+    lhs.resolverInfo.Swap(rhs.resolverInfo);
+}
+
+/// \relates ArAssetInfo
+AR_API
+bool 
+operator==(const ArAssetInfo& lhs, const ArAssetInfo& rhs);
+
+/// \relates ArAssetInfo
+AR_API
+bool 
+operator!=(const ArAssetInfo& lhs, const ArAssetInfo& rhs);
+
+AR_NAMESPACE_CLOSE_SCOPE
+
+#endif // PXR_AR_ASSET_INFO_H
