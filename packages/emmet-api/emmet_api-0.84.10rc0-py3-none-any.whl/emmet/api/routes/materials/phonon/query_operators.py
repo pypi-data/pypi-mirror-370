588aefbf1aab69b3e9cabe8661839d9e1892fd1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 07:04:27 +0000

    Add package emmet-api/0.84.10rc0/emmet_api-0.84.10rc0-py3-none-any.whl

---
+++ packages/emmet-api/emmet_api-0.84.10rc0-py3-none-any.whl/emmet/api/routes/materials/phonon/query_operators.py
@@ -0,0 +1,41 @@
+from fastapi import Query
+from emmet.core.mpid import MPID
+
+from maggma.api.query_operator import QueryOperator
+from maggma.api.utils import STORE_PARAMS
+
+from fastapi import Path
+
+
+class PhononImgQuery(QueryOperator):
+    """
+    Method to generate a query on phonon image data.
+    """
+
+    def query(
+        self,
+        task_id: MPID = Path(
+            ...,
+            description="The calculation (task) ID associated with the data object",
+        ),
+    ) -> STORE_PARAMS:
+        return {"criteria": {"task_id": str(task_id)}}
+
+
+class PhononMethodQuery(QueryOperator):
+    """
+    Method to query phonon method
+    """
+
+    def query(
+        self,
+        phonon_method: str = Query(
+            None,
+            description="Phonon Method to search for",
+        ),
+    ) -> STORE_PARAMS:
+        crit = {}
+        if phonon_method in {"dfpt", "phonopy", "pheasy"}:
+            crit = {"phonon_method": phonon_method}
+
+        return {"criteria": crit}
