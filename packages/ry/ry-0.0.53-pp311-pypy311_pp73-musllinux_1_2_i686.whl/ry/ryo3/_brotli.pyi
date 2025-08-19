3d5e86fa1117ee4b87886c3742ee4f6ed942c624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:39 +0000

    Add package ry/0.0.53/ry-0.0.53-pp311-pypy311_pp73-musllinux_1_2_i686.whl

---
+++ packages/ry/ry-0.0.53-pp311-pypy311_pp73-musllinux_1_2_i686.whl/ry/ryo3/_brotli.pyi
@@ -0,0 +1,11 @@
+"""ryo3-brotli types"""
+
+# =============================================================================
+# BROTLI
+# =============================================================================
+def brotli_encode(
+    input: bytes, quality: int = 11, magic_number: bool = False
+) -> bytes: ...
+def brotli_decode(input: bytes) -> bytes: ...
+def brotli(input: bytes, quality: int = 11, magic_number: bool = False) -> bytes:
+    """Alias for brotli_encode"""
