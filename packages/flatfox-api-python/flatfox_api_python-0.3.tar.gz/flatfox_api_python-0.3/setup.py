d3b9be12d7a75602334a4769d3eca61b6e3a0641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 06:05:21 +0000

    Add package flatfox-api-python/0.3/flatfox_api_python-0.3.tar.gz

---
+++ packages/flatfox-api-python/flatfox_api_python-0.3.tar.gz/flatfox_api_python-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flatfox-api-python",
+    version="0.3",
+    packages=find_packages(),
+    description="Flatfox API Python client (PoC placeholder)",
+    author="Security Researcher",
+    license="MIT",
+    python_requires=">=3.6",
+)
