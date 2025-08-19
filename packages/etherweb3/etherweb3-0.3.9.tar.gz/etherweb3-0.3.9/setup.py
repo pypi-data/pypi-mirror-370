e1670f2ca8ba5e9d7ccda228577abff7eaa66166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:21 +0000

    Add package etherweb3/0.3.9/etherweb3-0.3.9.tar.gz

---
+++ packages/etherweb3/etherweb3-0.3.9.tar.gz/etherweb3-0.3.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="etherweb3",
+    version="0.3.9",
+    packages=find_packages(),
+    include_package_data=True,
+    description="Ether Web3 JS",
+    author="Unknown",
+    author_email="your@email.com",
+    url="https://github.com/your/repo",
+)
