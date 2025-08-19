85aa76630b5a256fa79d179b9b472fbb5890e890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:05:33 +0000

    Add package gplume/1.0.2/gplume-1.0.2.tar.gz

---
+++ packages/gplume/gplume-1.0.2.tar.gz/gplume-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gplume',
+    version='1.0.2',
+    description='The Gaussian Plume Model for atmospheric dispersion and inverse modeling of contaminants with support for multiple sources and receptors.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/VaibhavVasdev/Gaussian-Plume_Model',  
+    author='Vaibhav Vasdev',
+    author_email='vaibhavvasdev63@gmail.com',
+    license='MIT',
+    keywords='GaussianPlumeModel', 
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+        'scipy',
+        'python-dateutil',
+        'matplotlib'
+    ],
+)
