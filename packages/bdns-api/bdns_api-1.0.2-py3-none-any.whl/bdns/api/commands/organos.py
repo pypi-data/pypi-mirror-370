c8275ead5c63633af6e2df7e4494484fa57d1b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 05:06:12 +0000

    Add package bdns-api/1.0.2/bdns_api-1.0.2-py3-none-any.whl

---
+++ packages/bdns-api/bdns_api-1.0.2-py3-none-any.whl/bdns/api/commands/organos.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+
+import typer
+
+from bdns.api.utils import format_url
+from bdns.api.types import TipoAdministracion
+from bdns.api.fetch_write import fetch_and_write
+from bdns.api.commands import options
+from bdns.api.endpoints import BDNS_API_ENDPOINT_ORGANOS
+
+
+def organos(
+    ctx: typer.Context,
+    vpd: str = options.vpd,
+    idAdmon: TipoAdministracion = options.idAdmon,
+) -> None:
+    """
+    Fetches all types of organs from the BDNS API ordered by the 'descripcion' field.
+    """
+    params = {
+        "vpd": vpd,
+        "idAdmon": idAdmon.value if idAdmon else None,
+    }
+    fetch_and_write(
+        url=format_url(BDNS_API_ENDPOINT_ORGANOS, params),
+        output_file=ctx.obj["output_file"],
+    )
