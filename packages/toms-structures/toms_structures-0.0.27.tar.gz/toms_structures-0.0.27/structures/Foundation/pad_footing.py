e2d800cc9eb8f53bc6dd6a7d884b3cdd590bcfda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:35 +0000

    Add package toms-structures/0.0.27/toms_structures-0.0.27.tar.gz

---
+++ packages/toms-structures/toms_structures-0.0.27.tar.gz/toms_structures-0.0.27/structures/Foundation/pad_footing.py
@@ -0,0 +1,16 @@
+
+
+class PadFooting:
+    M:float = 0
+    N:float = 0
+    N_eccentricity_x: float = 0
+    N_eccentricity_y: float = 0
+    depth:float = 0
+    length:float = 0
+    width:float = 0
+    fuc:float = 20
+
+    def max_allowable_bearing(self):
+        
+        return 
+
