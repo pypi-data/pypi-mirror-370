ca94e6d328fa3cd0a2ecb509b8df33f73a4b0f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:08 +0000

    Add package nemesis-eleos/0.9.8/nemesis_eleos-0.9.8-py3-none-any.whl

---
+++ packages/nemesis-eleos/nemesis_eleos-0.9.8-py3-none-any.whl/eleos/__init__.py
@@ -0,0 +1,10 @@
+"""Reading from __init__.py"""
+
+from .constants import *
+from .cores import *
+from .parsers import *
+from .profiles import *
+from .results import *
+from .shapes import *
+from .spectra import *
+from .utils import *
