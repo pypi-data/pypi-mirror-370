c900c3b95dac320e22b137d6df23b532a4082a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 05:05:10 +0000

    Add package poetry-polylith-plugin/1.40.1/poetry_polylith_plugin-1.40.1.tar.gz

---
+++ packages/poetry-polylith-plugin/poetry_polylith_plugin-1.40.1.tar.gz/poetry_polylith_plugin-1.40.1/polylith/poetry/commands/diff.py
@@ -0,0 +1,48 @@
+from cleo.helpers import option
+from poetry.console.commands.command import Command
+from polylith import commands
+
+command_options = [
+    option(
+        long_name="short",
+        short_name="s",
+        description="Print short view",
+        flag=True,
+    ),
+    option(
+        long_name="since",
+        description="Changed since a specific tag",
+        flag=False,
+    ),
+]
+
+
+class DiffCommand(Command):
+    name = "poly diff"
+    description = "Shows changed bricks compared to the latest git tag."
+
+    options = command_options + [
+        option(
+            long_name="bricks",
+            description="Print changed bricks",
+            flag=True,
+        ),
+        option(
+            long_name="deps",
+            description="Print bricks that depend on the changes. Use with --bricks.",
+            flag=True,
+        ),
+    ]
+
+    def handle(self) -> int:
+        since = self.option("since")
+
+        options = {
+            "short": self.option("short"),
+            "bricks": self.option("bricks"),
+            "deps": self.option("deps"),
+        }
+
+        commands.diff.run(since, options)
+
+        return 0
