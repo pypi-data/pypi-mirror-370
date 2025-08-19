d1d698eb9fb68e4ac4d115617dffa6eccf9be258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:06:03 +0000

    Add package cloudfoundry-client/1.39.0/cloudfoundry_client-1.39.0-py3-none-any.whl

---
+++ packages/cloudfoundry-client/cloudfoundry_client-1.39.0-py3-none-any.whl/cloudfoundry_client/v3/apps.py
@@ -0,0 +1,29 @@
+from typing import TYPE_CHECKING, Optional
+
+from cloudfoundry_client.common_objects import JsonObject
+from cloudfoundry_client.v3.entities import EntityManager
+
+if TYPE_CHECKING:
+    from cloudfoundry_client.client import CloudFoundryClient
+
+
+class AppManager(EntityManager):
+    def __init__(self, target_endpoint: str, client: "CloudFoundryClient"):
+        super(AppManager, self).__init__(target_endpoint, client, "/v3/apps")
+
+    def restart(self, application_guid: str):
+        return super(AppManager, self)._post("%s%s/%s/actions/restart" % (self.target_endpoint,
+                                                                          self.entity_uri,
+                                                                          application_guid))
+
+    def remove(self, application_guid: str, asynchronous: bool = True) -> Optional[str]:
+        return super(AppManager, self)._remove(application_guid, asynchronous)
+
+    def get_env(self, application_guid: str) -> JsonObject:
+        return super(AppManager, self)._get("%s%s/%s/env" % (self.target_endpoint, self.entity_uri, application_guid))
+
+    def get_routes(self, application_guid: str) -> JsonObject:
+        return super(AppManager, self)._get("%s%s/%s/routes" % (self.target_endpoint, self.entity_uri, application_guid))
+
+    def get_manifest(self, application_guid: str) -> str:
+        return self.client.get(url="%s%s/%s/manifest" % (self.target_endpoint, self.entity_uri, application_guid)).text
