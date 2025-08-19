f83eadb3d1333988210bb75393f5c83861b42d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:06:01 +0000

    Add package jeanmemory/1.9.0/jeanmemory-1.9.0-py3-none-any.whl

---
+++ packages/jeanmemory/jeanmemory-1.9.0-py3-none-any.whl/jeanmemory.py
@@ -0,0 +1,7 @@
+"""
+jeanmemory module - Top-level module for documentation compatibility
+Allows imports like: from jeanmemory import JeanClient
+"""
+
+# Import everything from jean_memory for compatibility
+from jean_memory import *
