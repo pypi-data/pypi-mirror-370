e2bbaf22fd647db22411349ec88b98097e4b0c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:05:32 +0000

    Add package cloudoll/3.0.6/cloudoll-3.0.6.tar.gz

---
+++ packages/cloudoll/cloudoll-3.0.6.tar.gz/cloudoll-3.0.6/cloudoll/orm/mysql.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+from cloudoll.orm import Mysql
+
+# for config folder conf.{env}.yaml
+
+database:
+  mysql:
+    type: mysql  # important ,default mysql
+    host: 127.0.0.1
+    port: 3306
+    user: root
+    password: 123456
+    db: test
+    charset:utf8mb4
+    pool_size:5
+
+# to get yaml config
+config = ....(database.mysql)
+
+pool = await Mysql().create_engine(**config)
+result = await pool.query("sql")
+"""
+__author__ = "Qiu / smallerqiu@gmail.com"
+
+from aiomysql import create_pool, DictCursor
+
+# from aiomysql.cursors import Cursor
+# from aiomysql.connection import Connection
+from cloudoll.logging import error
+
+# from inspect import isclass, isfunction
+from cloudoll.orm.base import MeteBase
+
+
+class AttrDict(dict):
+    """Dict that can get attribute by dot, and doesn't raise KeyError"""
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            return None
+
+
+class AttrDictCursor(DictCursor):
+    dict_type = AttrDict
+
+
+class Mysql(MeteBase):
+    # def __init__(self):
+        # self.pool: Optional[Pool] = None
+        # self.cursor: Cursor = None
+        # self.conn: Connection = None
+        # self.__MODELS__ = []
+
+    # def __call__(cls, *args: Any, **kwds: Any) -> Any:
+    #     cls.__init__(*args, **kwds)
+    #     return cls()
+
+    # @classmethod
+    async def create_engine(self, loop=None, **kw):
+        try:
+            self.pool = await create_pool(
+                host=kw.get("host", "localhost"),
+                port=kw.get("port", 3306),
+                user=kw.get("user"),
+                password=str(kw.get("password", "")),
+                db=kw.get("db"),
+                echo=kw.get("echo", False),
+                charset=kw.get("charset", "utf8"),
+                autocommit=False,  # kw.get("autocommit", False),
+                maxsize=kw.get("maxsize", 10),
+                minsize=kw.get("pool_size", 5),
+                cursorclass=AttrDictCursor,
+                connect_timeout=kw.get("connect_timeout", 10),
+                loop=loop,
+            )
+        except Exception as e:
+            error(f"Database connection failed,the instance : {kw.get('db')}")
+        return self
---
+++ packages/cloudoll/cloudoll-3.0.6.tar.gz/cloudoll-3.0.6/cloudoll/orm/postgres.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+from cloudoll.orm.postgres import Postgres
+
+# for config folder conf.{env}.yaml
+
+database:
+  pg:
+    type: postgres  # important ,default mysql
+    host: 127.0.0.1
+    port: 3306
+    user: root
+    password: 123456
+    db: test
+    echo: True
+    timeout: 60
+    pool_size:5
+
+# to get yaml config
+config = ....(database.postgres)
+
+pool = await Postgres().create_engine(**config)
+result = await pool.query("sql")
+"""
+__author__ = "Qiu / smallerqiu@gmail.com"
+
+from aiopg import create_pool
+from cloudoll.logging import error
+from cloudoll.orm.base import MeteBase
+
+
+class Postgres(MeteBase):
+    # def __init__(self):
+    #     self.pool: Optional[Pool] = None
+        # self.cursor: Optional[Cursor] = None
+        # self.conn: Optional[Connection] = None
+        # self.__MODELS__ = []
+
+    # def __call__(cls, *args: Any, **kwds: Any) -> Any:
+    #     cls.__init__(*args, **kwds)
+    #     return cls()
+
+    @classmethod
+    async def create_engine(cls, **kw):
+        try:
+            host = (kw.get("host", "localhost"),)
+            port = (kw.get("port", 5432),)
+            user = (kw.get("user"),)
+            password = (str(kw.get("password", "")),)
+            db = (kw.get("db"),)
+            dsn = f"dbname={db[0]} user={user[0]} password={password[0]} host={host[0]} port={port[0]}"
+            cls.pool = await create_pool(
+                dsn=dsn,
+                timeout=kw.get("timeout", 10.0),
+                enable_json=kw.get("enable_json", False),
+                enable_hstore=kw.get("enable_hstore", False),
+                enable_uuid=kw.get("enable_uuid", False),
+                pool_recycle=kw.get("pool_recycle", 10),
+                echo=kw.get("echo", False),
+                minsize=kw.get("pool_size", 5),
+                maxsize=kw.get("pool_size", 10),
+            )
+        except Exception as e:
+            # error(e)
+            error(f"Database connection failed,the instance : {kw.get('db')}")
+
+        return cls
