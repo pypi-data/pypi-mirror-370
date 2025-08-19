bc2b6c9721e7e10ec6b29b24f2d7c973107ae948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:05:43 +0000

    Add package netmon/0.5.2/netmon-0.5.2.tar.gz

---
+++ packages/netmon/netmon-0.5.2.tar.gz/netmon-0.5.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import codecs
+
+try:
+    f = codecs.open("README.rst", encoding="utf-8")
+    long_description = f.read()
+    f.close()
+except:
+    long_description = "Network Monitor for Linux"
+
+setup(
+    name="netmon",
+    version="0.5.2",
+    author="Denis Khoshaba",
+    author_email="pypi@theden.sh",
+    scripts=["netmon"],
+    url="https://github.com/theden/netmon",
+    keywords=["network", "monitor", "linux"],
+    license="GPL-2.0",
+    description="network monitor for linux",
+    long_description=long_description,
+    install_requires=[
+        "ascii-graph",
+        "cursor",
+    ],
+)
