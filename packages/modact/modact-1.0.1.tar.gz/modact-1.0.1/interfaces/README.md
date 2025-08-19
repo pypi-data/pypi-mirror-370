38cf56176c2c4beb7af49d6274cf2a8c22efb535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 04:05:18 +0000

    Add package modact/1.0.1/modact-1.0.1.tar.gz

---
+++ packages/modact/modact-1.0.1.tar.gz/modact-1.0.1/interfaces/README.md
@@ -0,0 +1,9 @@
+# MODact interfaces
+
+Python interfaces are embedded in the package. Other interfaces are located
+here.
+
+* c++ wrapper (header-only) using [pybind11](https://github.com/pybind/pybind11)
+  * Code for [NOMAD](https://www.gerad.ca/nomad/) (optional parallelization through MPI)
+  * Code for [Borg](http://borgmoea.org) (parallelization through MPI)
+* Example problem definition for [PlatEMO](https://github.com/BIMK/PlatEMO) (MATLAB)
---
+++ packages/modact/modact-1.0.1.tar.gz/modact-1.0.1/modact/interfaces/pymoo.py
@@ -0,0 +1,36 @@
+import numpy as np
+from pymoo.core.problem import ElementwiseProblem
+
+import modact.problems as pb
+
+
+class PymopProblem(ElementwiseProblem):
+    def __init__(self, function, **kwargs):
+        if isinstance(function, pb.Problem):
+            self.fct = function
+        else:
+            self.fct = pb.get_problem(function)
+        lb, ub = self.fct.bounds()
+        n_var = len(lb)
+        n_obj = len(self.fct.weights)
+        n_constr = len(self.fct.c_weights)
+        xl = lb
+        xu = ub
+
+        self.weights = np.array(self.fct.weights)
+        self.c_weights = np.array(self.fct.c_weights)
+
+        super().__init__(
+            n_var=n_var,
+            n_obj=n_obj,
+            n_ieq_constr=n_constr,
+            xl=xl,
+            xu=xu,
+            vtype=np.double,
+            **kwargs,
+        )
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        f, g = self.fct(x)
+        out["F"] = np.array(f) * -1 * self.weights
+        out["G"] = np.array(g) * self.c_weights
