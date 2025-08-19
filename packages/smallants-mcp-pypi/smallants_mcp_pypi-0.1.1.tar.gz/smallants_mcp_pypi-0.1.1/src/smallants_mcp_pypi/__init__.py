491bcb7d22e4b9e9f4118be4abe42dd748826781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:08 +0000

    Add package smallants-mcp-pypi/0.1.1/smallants_mcp_pypi-0.1.1.tar.gz

---
+++ packages/smallants-mcp-pypi/smallants_mcp_pypi-0.1.1.tar.gz/smallants_mcp_pypi-0.1.1/src/smallants_mcp_pypi/__init__.py
@@ -0,0 +1,21 @@
+"""
+FastMCP quickstart example.
+
+cd to the `examples/snippets/clients` directory and run:
+    uv run server fastmcp_quickstart stdio
+"""
+
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+def main() -> None:
+    mcp.run(transport='stdio')
