296a8ed04ac3680403412c6061f0d37503705bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 04:04:24 +0000

    Add package dltype/0.3.1/dltype-0.3.1.tar.gz

---
+++ packages/dltype/dltype-0.3.1.tar.gz/dltype-0.3.1/dltype/__init__.py
@@ -0,0 +1,111 @@
+from dltype._lib._core import (
+    DLTypeScopeProvider,
+    dltyped,
+    dltyped_namedtuple,
+    dltyped_dataclass,
+)
+
+from dltype._lib._constants import (
+    DEBUG_MODE,
+    MAX_ACCEPTABLE_EVALUATION_TIME_NS,
+)
+
+from dltype._lib._tensor_type_base import (
+    SUPPORTED_TENSOR_TYPES,
+    TensorTypeBase,
+)
+
+from dltype._lib._errors import (
+    DLTypeError,
+    DLTypeShapeError,
+    DLTypeScopeProviderError,
+    DLTypeInvalidReferenceError,
+    DLTypeDuplicateError,
+    DLTypeDtypeError,
+)
+from dltype._lib._dependency_utilities import (
+    is_torch_available,
+    is_numpy_available,
+    raise_for_missing_dependency,
+)
+
+if is_torch_available() and is_numpy_available():
+    from dltype._lib._universal_tensors import (
+        Int8Tensor,
+        Int16Tensor,
+        Int32Tensor,
+        Int64Tensor,
+        IntTensor,
+        Float16Tensor,
+        IEEE754HalfFloatTensor,
+        BFloat16Tensor,
+        Float32Tensor,
+        Float64Tensor,
+        FloatTensor,
+        BoolTensor,
+        DoubleTensor,
+    )
+elif is_numpy_available():
+    from dltype._lib._numpy_tensors import (
+        Int8Tensor,
+        Int16Tensor,
+        Int32Tensor,
+        Int64Tensor,
+        IntTensor,
+        Float16Tensor,
+        IEEE754HalfFloatTensor,
+        Float32Tensor,
+        Float64Tensor,
+        FloatTensor,
+        BoolTensor,
+        DoubleTensor,
+    )
+elif is_torch_available():
+    from dltype._lib._torch_tensors import (
+        Int8Tensor,
+        Int16Tensor,
+        Int32Tensor,
+        Int64Tensor,
+        IntTensor,
+        Float16Tensor,
+        IEEE754HalfFloatTensor,
+        BFloat16Tensor,
+        Float32Tensor,
+        Float64Tensor,
+        FloatTensor,
+        BoolTensor,
+        DoubleTensor,
+    )
+else:
+    raise_for_missing_dependency()
+
+
+__all__ = [
+    "DEBUG_MODE",
+    "MAX_ACCEPTABLE_EVALUATION_TIME_NS",
+    "SUPPORTED_TENSOR_TYPES",
+    "DLTypeError",
+    "DLTypeShapeError",
+    "DLTypeScopeProviderError",
+    "DLTypeInvalidReferenceError",
+    "DLTypeDuplicateError",
+    "DLTypeDtypeError",
+    "DLTypeScopeProvider",
+    "TensorTypeBase",
+    "dltyped",
+    "dltyped_namedtuple",
+    "dltyped_dataclass",
+    "Int8Tensor",
+    "Int16Tensor",
+    "Int32Tensor",
+    "Int64Tensor",
+    "IntTensor",
+    "Float16Tensor",
+    "IEEE754HalfFloatTensor",
+    "BFloat16Tensor",
+    "Float32Tensor",
+    "Float64Tensor",
+    "FloatTensor",
+    "BoolTensor",
+    "DoubleTensor",
+]
