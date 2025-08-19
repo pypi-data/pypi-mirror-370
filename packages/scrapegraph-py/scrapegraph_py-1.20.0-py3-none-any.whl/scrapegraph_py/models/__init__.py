b55d2f84cb8ff077a3ad1d2ca560797bbbcea15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 16:06:59 +0000

    Add package scrapegraph-py/1.20.0/scrapegraph_py-1.20.0-py3-none-any.whl

---
+++ packages/scrapegraph-py/scrapegraph_py-1.20.0-py3-none-any.whl/scrapegraph_py/models/__init__.py
@@ -0,0 +1,20 @@
+from .agenticscraper import AgenticScraperRequest, GetAgenticScraperRequest
+from .crawl import CrawlRequest, GetCrawlRequest
+from .feedback import FeedbackRequest
+from .markdownify import GetMarkdownifyRequest, MarkdownifyRequest
+from .searchscraper import GetSearchScraperRequest, SearchScraperRequest
+from .smartscraper import GetSmartScraperRequest, SmartScraperRequest
+
+__all__ = [
+    "AgenticScraperRequest",
+    "GetAgenticScraperRequest",
+    "CrawlRequest",
+    "GetCrawlRequest",
+    "FeedbackRequest",
+    "GetMarkdownifyRequest",
+    "MarkdownifyRequest",
+    "GetSearchScraperRequest",
+    "SearchScraperRequest",
+    "GetSmartScraperRequest",
+    "SmartScraperRequest",
+]
