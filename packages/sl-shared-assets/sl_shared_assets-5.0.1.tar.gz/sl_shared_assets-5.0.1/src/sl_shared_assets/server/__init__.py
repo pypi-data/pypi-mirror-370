4864f55460a1343fa658b9c5262cec9e67f6f54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 06:05:28 +0000

    Add package sl-shared-assets/5.0.1/sl_shared_assets-5.0.1.tar.gz

---
+++ packages/sl-shared-assets/sl_shared_assets-5.0.1.tar.gz/sl_shared_assets-5.0.1/src/sl_shared_assets/server/__init__.py
@@ -0,0 +1,28 @@
+"""This package provides the classes and methods used by all Sun lab libraries to work with the data stored on remote
+compute servers, such as the BioHPC server. It provides tools for submitting and monitoring jobs, running complex
+processing pipelines and interactively working with the data via a Jupyter lab server."""
+
+from .job import Job, JupyterJob
+from .server import Server, ServerCredentials, generate_server_credentials
+from .pipeline import (
+    ProcessingStatus,
+    TrackerFileNames,
+    ProcessingTracker,
+    ProcessingPipeline,
+    ProcessingPipelines,
+    generate_manager_id,
+)
+
+__all__ = [
+    "Job",
+    "JupyterJob",
+    "ProcessingPipeline",
+    "ProcessingPipelines",
+    "ProcessingStatus",
+    "ProcessingTracker",
+    "Server",
+    "ServerCredentials",
+    "TrackerFileNames",
+    "generate_manager_id",
+    "generate_server_credentials",
+]
