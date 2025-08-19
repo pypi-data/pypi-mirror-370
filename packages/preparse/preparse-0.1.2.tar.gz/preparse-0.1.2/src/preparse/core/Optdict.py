184d5e7e098546d0d937cd95df310f82db1e46ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:33 +0000

    Add package preparse/0.1.2/preparse-0.1.2.tar.gz

---
+++ packages/preparse/preparse-0.1.2.tar.gz/preparse-0.1.2/src/preparse/core/Optdict.py
@@ -0,0 +1,28 @@
+from typing import *
+
+import datahold
+
+from preparse._processing import *
+from preparse.core.Click import *
+from preparse.core.enums import *
+from preparse.core.warnings import *
+
+__all__ = ["Optdict"]
+
+
+class Optdict(datahold.OkayDict):
+    @property
+    def data(self: Self) -> dict:
+        return dict(self._data)
+
+    @data.setter
+    def data(self: Self, value: Any):
+        if value is None:
+            self._data = dict()
+            return
+        d: dict = dict()
+        k: Any
+        v: Any
+        for k, v in value.items():
+            d[str(k)] = Nargs(v)
+        self._data = d
