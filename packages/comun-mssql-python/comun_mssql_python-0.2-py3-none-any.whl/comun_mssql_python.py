9a1a73890c272770e13b8b61a904de4cae6e51b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:13 +0000

    Add package comun-mssql-python/0.2/comun_mssql_python-0.2-py3-none-any.whl

---
+++ packages/comun-mssql-python/comun_mssql_python-0.2-py3-none-any.whl/comun_mssql_python.py
@@ -0,0 +1,42 @@
+import mssql_python
+
+
+class Sql:
+    def __init__(self, driver, servidor, database, user, passw):
+        self.nombre = 'Server=' + servidor
+        self.nombre = self.nombre + ';Database=' + database
+        self.nombre = self.nombre + ';UID=' + user
+        self.nombre = self.nombre + ';PWD=' + passw
+        self.nombre = self.nombre + ';Encrypt=yes'
+        self.nombre = self.nombre + ';TrustServerCertificate=yes'
+        self.conexion = mssql_python.connect(self.nombre, autocommit=False)
+        self.cursor = self.conexion.cursor()
+
+    def cerrar_conexion(self):
+        self.conexion.close()
+
+    def ejecutar(self, texto, *parametros):
+        try:
+            if len(parametros):
+                if type(parametros[0]) == tuple:
+                    parametros = parametros[0]
+            self.cursor.execute(texto,parametros)
+            self.conexion.commit()
+        except Exception as e:
+            print(texto)
+            print(parametros)
+            print(e)
+            raise Exception(e)
+
+    def consultar(self, consulta, as_dict = True):
+        self.cursor.execute(consulta)
+        if as_dict:
+            columns = [column[0] for column in self.cursor.description]
+            results = []
+            for row in self.cursor.fetchall():
+                results.append(dict(zip(columns, row)))
+
+            return results
+        else:
+            return self.cursor.fetchall()
+
