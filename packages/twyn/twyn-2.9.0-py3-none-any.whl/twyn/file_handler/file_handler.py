3453f56e74b8130eac43b807e4ddf2d032f74162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 16:06:03 +0000

    Add package twyn/2.9.0/twyn-2.9.0-py3-none-any.whl

---
+++ packages/twyn/twyn-2.9.0-py3-none-any.whl/twyn/file_handler/file_handler.py
@@ -0,0 +1,52 @@
+import logging
+import os
+from pathlib import Path
+from typing import Protocol
+
+from twyn.base.exceptions import TwynError
+from twyn.file_handler.exceptions import PathIsNotFileError, PathNotFoundError
+
+logger = logging.getLogger("twyn")
+
+
+class BaseFileHandler(Protocol):
+    def read(self) -> str: ...
+    def file_exists(self) -> bool: ...
+    def write(self, data: str) -> None: ...
+
+
+class FileHandler(BaseFileHandler):
+    def __init__(self, file_path: str) -> None:
+        self.file_path = self._get_file_path(file_path)
+
+    def _get_file_path(self, file_path: str) -> Path:
+        return Path(os.path.abspath(os.path.join(os.getcwd(), file_path)))
+
+    def is_handler_of_file(self, name: str) -> bool:
+        return self._get_file_path(name) == self.file_path
+
+    def read(self) -> str:
+        self._raise_for_file_exists()
+
+        content = self.file_path.read_text()
+        logger.debug("Successfully read content from local dependencies file")
+
+        return content
+
+    def file_exists(self) -> bool:
+        try:
+            self._raise_for_file_exists()
+        except TwynError:
+            return False
+        return True
+
+    def _raise_for_file_exists(self) -> None:
+        if not self.file_path.exists():
+            raise PathNotFoundError
+
+        if not self.file_path.is_file():
+            raise PathIsNotFileError
+
+    def write(self, data: str) -> None:
+        self._raise_for_file_exists()
+        self.file_path.write_text(data)
---
+++ packages/twyn/twyn-2.9.0-py3-none-any.whl/twyn/trusted_packages/references.py
@@ -0,0 +1,59 @@
+import logging
+from abc import ABC, abstractmethod
+from typing import Any
+
+import requests
+
+from twyn.trusted_packages.exceptions import (
+    EmptyPackagesListError,
+    InvalidJSONError,
+    InvalidPyPiFormatError,
+)
+
+logger = logging.getLogger("twyn")
+
+
+class AbstractPackageReference(ABC):
+    """Represents a reference to retrieve trusted packages from."""
+
+    def __init__(self, source: str) -> None:
+        self.source = source
+
+    @abstractmethod
+    def get_packages(self) -> set[str]:
+        """Return the names of the trusted packages available in the reference."""
+
+
+class TopPyPiReference(AbstractPackageReference):
+    """Top PyPi packages retrieved from an online source."""
+
+    def get_packages(self) -> set[str]:
+        """Download and parse online source of top Python Package Index packages."""
+        packages_info = self._download()
+        return self._parse(packages_info)
+
+    def _download(self) -> dict[str, Any]:
+        packages = requests.get(self.source)
+        packages.raise_for_status()
+        try:
+            packages_json: dict[str, Any] = packages.json()
+        except requests.exceptions.JSONDecodeError as err:
+            raise InvalidJSONError from err
+
+        logger.debug(f"Successfully downloaded trusted packages list from {self.source}")
+
+        return packages_json
+
+    @staticmethod
+    def _parse(packages_info: dict[str, Any]) -> set[str]:
+        try:
+            names = {row["project"] for row in packages_info["rows"]}
+        except KeyError as err:
+            raise InvalidPyPiFormatError from err
+
+        if not names:
+            raise EmptyPackagesListError
+
+        logger.debug("Successfully parsed trusted packages list")
+
+        return names
