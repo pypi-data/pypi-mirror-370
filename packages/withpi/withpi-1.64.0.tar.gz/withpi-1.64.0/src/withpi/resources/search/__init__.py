ab2f10cbe0ae34f43437986a80c9b790bc613e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 06:04:38 +0000

    Add package withpi/1.64.0/withpi-1.64.0.tar.gz

---
+++ packages/withpi/withpi-1.64.0.tar.gz/withpi-1.64.0/src/withpi/resources/search/__init__.py
@@ -0,0 +1,61 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .search import (
+    SearchResource,
+    AsyncSearchResource,
+    SearchResourceWithRawResponse,
+    AsyncSearchResourceWithRawResponse,
+    SearchResourceWithStreamingResponse,
+    AsyncSearchResourceWithStreamingResponse,
+)
+from .groundedness import (
+    GroundednessResource,
+    AsyncGroundednessResource,
+    GroundednessResourceWithRawResponse,
+    AsyncGroundednessResourceWithRawResponse,
+    GroundednessResourceWithStreamingResponse,
+    AsyncGroundednessResourceWithStreamingResponse,
+)
+from .query_classifier import (
+    QueryClassifierResource,
+    AsyncQueryClassifierResource,
+    QueryClassifierResourceWithRawResponse,
+    AsyncQueryClassifierResourceWithRawResponse,
+    QueryClassifierResourceWithStreamingResponse,
+    AsyncQueryClassifierResourceWithStreamingResponse,
+)
+from .query_to_passage import (
+    QueryToPassageResource,
+    AsyncQueryToPassageResource,
+    QueryToPassageResourceWithRawResponse,
+    AsyncQueryToPassageResourceWithRawResponse,
+    QueryToPassageResourceWithStreamingResponse,
+    AsyncQueryToPassageResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "QueryClassifierResource",
+    "AsyncQueryClassifierResource",
+    "QueryClassifierResourceWithRawResponse",
+    "AsyncQueryClassifierResourceWithRawResponse",
+    "QueryClassifierResourceWithStreamingResponse",
+    "AsyncQueryClassifierResourceWithStreamingResponse",
+    "GroundednessResource",
+    "AsyncGroundednessResource",
+    "GroundednessResourceWithRawResponse",
+    "AsyncGroundednessResourceWithRawResponse",
+    "GroundednessResourceWithStreamingResponse",
+    "AsyncGroundednessResourceWithStreamingResponse",
+    "QueryToPassageResource",
+    "AsyncQueryToPassageResource",
+    "QueryToPassageResourceWithRawResponse",
+    "AsyncQueryToPassageResourceWithRawResponse",
+    "QueryToPassageResourceWithStreamingResponse",
+    "AsyncQueryToPassageResourceWithStreamingResponse",
+    "SearchResource",
+    "AsyncSearchResource",
+    "SearchResourceWithRawResponse",
+    "AsyncSearchResourceWithRawResponse",
+    "SearchResourceWithStreamingResponse",
+    "AsyncSearchResourceWithStreamingResponse",
+]
---
+++ packages/withpi/withpi-1.64.0.tar.gz/withpi-1.64.0/src/withpi/types/__init__.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import (
+    Example as Example,
+    Question as Question,
+    ExplorationMode as ExplorationMode,
+    SyntheticDataStatus as SyntheticDataStatus,
+    DataGenerationStatus as DataGenerationStatus,
+    ScoringSystemMetrics as ScoringSystemMetrics,
+    QueryClassifierResult as QueryClassifierResult,
+    ScoringSpecCalibrationStatus as ScoringSpecCalibrationStatus,
+)
+from .scoring_system_score_params import ScoringSystemScoreParams as ScoringSystemScoreParams
+from .search_embed_documents_params import SearchEmbedDocumentsParams as SearchEmbedDocumentsParams
+from .search_embed_documents_response import SearchEmbedDocumentsResponse as SearchEmbedDocumentsResponse
+from .scoring_system_import_spec_params import ScoringSystemImportSpecParams as ScoringSystemImportSpecParams
+from .scoring_system_import_spec_response import ScoringSystemImportSpecResponse as ScoringSystemImportSpecResponse
+from .scoring_system_upload_to_huggingface_params import (
+    ScoringSystemUploadToHuggingfaceParams as ScoringSystemUploadToHuggingfaceParams,
+)
+from .scoring_system_upload_to_huggingface_response import (
+    ScoringSystemUploadToHuggingfaceResponse as ScoringSystemUploadToHuggingfaceResponse,
+)
