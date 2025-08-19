a4fe8689ab97032bb704232f41fc039fc4fcdaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 04:04:30 +0000

    Add package bluer-ugv/6.674.1/bluer_ugv-6.674.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.674.1.tar.gz/bluer_ugv-6.674.1/bluer_ugv/README/items.py
@@ -0,0 +1,43 @@
+from bluer_objects import README
+
+
+items = README.Items(
+    [
+        {
+            "name": "bluer_swallow",
+            "marquee": "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250701_2206342_1.gif?raw=true",
+            "description": "based on power wheels.",
+            "url": "./bluer_ugv/docs/bluer_swallow",
+        },
+        {
+            "name": "bluer_sparrow",
+            "marquee": "https://github.com/kamangir/assets2/raw/main/bluer-sparrow/20250722_174115-2.jpg?raw=true",
+            "description": "bluer_swallow's little sister.",
+            "url": "./bluer_ugv/docs/bluer_sparrow",
+        },
+        {
+            "name": "bluer_robin",
+            "marquee": "https://github.com/kamangir/assets2/raw/main/bluer-robin/20250723_095155~2_1.gif?raw=true",
+            "description": "remote control car kit for teenagers.",
+            "url": "./bluer_ugv/docs/bluer_robin",
+        },
+        {
+            "name": "bluer_eagle",
+            "marquee": "https://github.com/kamangir/assets2/raw/main/bluer-eagle/file_0000000007986246b45343b0c06325dd.png?raw=true",
+            "description": "a remotely controlled ballon.",
+            "url": "./bluer_ugv/docs/bluer_eagle",
+        },
+        {
+            "name": "bluer-fire",
+            "marquee": "https://github.com/kamangir/assets/blob/main/bluer-ugv/bluer-fire.png?raw=true",
+            "description": "based on a used car.",
+            "url": "./bluer_ugv/docs/bluer_fire",
+        },
+        {
+            "name": "bluer-beast",
+            "marquee": "https://github.com/waveshareteam/ugv_rpi/raw/main/media/UGV-Rover-details-23.jpg",
+            "description": "based on [UGV Beast PI ROS2](https://www.waveshare.com/wiki/UGV_Beast_PI_ROS2).",
+            "url": "./bluer_ugv/docs/bluer_beast",
+        },
+    ]
+)
