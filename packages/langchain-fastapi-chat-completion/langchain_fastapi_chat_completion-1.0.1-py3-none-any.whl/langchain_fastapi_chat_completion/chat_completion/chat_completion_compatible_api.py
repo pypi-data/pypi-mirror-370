51b3e518bebac107b0e3dfc0f94bbe2b50f88c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 16:06:42 +0000

    Add package langchain-fastapi-chat-completion/1.0.1/langchain_fastapi_chat_completion-1.0.1-py3-none-any.whl

---
+++ packages/langchain-fastapi-chat-completion/langchain_fastapi_chat_completion-1.0.1-py3-none-any.whl/langchain_fastapi_chat_completion/chat_completion/chat_completion_compatible_api.py
@@ -0,0 +1,83 @@
+from typing import AsyncContextManager, AsyncIterator, List, Optional
+
+from langchain_core.runnables import Runnable
+from langgraph.graph.state import CompiledStateGraph
+from openai.types.chat import ChatCompletionMessage
+
+from langchain_fastapi_chat_completion.chat_completion.langchain_invoke_adapter import (
+    LangchainInvokeAdapter,
+)
+from langchain_fastapi_chat_completion.chat_completion.langchain_stream_adapter import (
+    LangchainStreamAdapter,
+)
+from langchain_fastapi_chat_completion.core.utils.pydantic_async_iterator import (
+    ato_dict,
+)
+
+
+class ChatCompletionCompatibleAPI:
+
+    @staticmethod
+    def from_agent(
+        agent: AsyncContextManager[Runnable],
+        llm_model: str,
+        system_fingerprint: Optional[str] = "",
+        event_adapter: callable = lambda event: None,
+    ):
+        return ChatCompletionCompatibleAPI(
+            LangchainStreamAdapter(llm_model, system_fingerprint),
+            LangchainInvokeAdapter(llm_model, system_fingerprint),
+            agent,
+            event_adapter,
+        )
+
+    def __init__(
+        self,
+        stream_adapter: LangchainStreamAdapter,
+        invoke_adapter: LangchainInvokeAdapter,
+        agent: AsyncContextManager[Runnable],
+        event_adapter: callable = lambda event: None,
+    ) -> None:
+        self.stream_adapter = stream_adapter
+        self.invoke_adapter = invoke_adapter
+        self.agent = agent
+        self.event_adapter = event_adapter
+
+    async def astream(
+        self, messages: List[ChatCompletionMessage]
+    ) -> AsyncIterator[dict]:
+        async with self.agent as runnable:
+            input = self.__to_input(runnable, messages)
+            astream_event = runnable.astream_events(
+                input=input,
+                version="v2",
+            )
+            async for it in ato_dict(
+                self.stream_adapter.ato_chat_completion_chunk_stream(
+                    astream_event, event_adapter=self.event_adapter
+                )
+            ):
+                yield it
+
+    async def ainvoke(self, messages: List[ChatCompletionMessage]) -> dict:
+        async with self.agent as runnable:
+            input = self.__to_input(runnable, messages)
+            result = await runnable.ainvoke(
+                input=input,
+            )
+
+        return self.invoke_adapter.to_chat_completion_object(result).model_dump()
+
+    def __to_input(self, runnable: Runnable, messages: List[ChatCompletionMessage]):
+        if isinstance(runnable, CompiledStateGraph):
+            return self.__to_react_agent_input(messages)
+        else:
+            return self.__to_chat_model_input(messages)
+
+    def __to_react_agent_input(self, messages: List[ChatCompletionMessage]):
+        return {
+            "messages": [message for message in messages],
+        }
+
+    def __to_chat_model_input(self, messages: List[ChatCompletionMessage]):
+        return [message for message in messages]
---
+++ packages/langchain-fastapi-chat-completion/langchain_fastapi_chat_completion-1.0.1-py3-none-any.whl/langchain_fastapi_chat_completion/core/create_agent_dto.py
@@ -0,0 +1,25 @@
+from typing import Annotated, Optional
+
+from fastapi import Depends, Header
+from pydantic import BaseModel
+
+from langchain_fastapi_chat_completion.core.types.openai import (
+    OpenAIChatCompletionRequest,
+)
+from langchain_fastapi_chat_completion.fastapi.token_getter import get_bearer_token
+
+
+class _CreateAgentDto(BaseModel):
+    request: OpenAIChatCompletionRequest
+    api_key: Optional[str] = None
+
+
+async def get_dto(
+    chat_completion_request: OpenAIChatCompletionRequest,
+    authorization: str = Header(None),
+):
+    api_key = get_bearer_token(authorization)
+    return CreateAgentDto(request=chat_completion_request, api_key=api_key)
+
+
+CreateAgentDto = Annotated[_CreateAgentDto, Depends(get_dto)]
