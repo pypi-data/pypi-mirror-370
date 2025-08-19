af0a9a3623701cc0280ab6d9838ceb25247906d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 03:22:27 +0000

    Add package votingai/1.0.1/votingai-1.0.1-py3-none-any.whl

---
+++ packages/votingai/votingai-1.0.1-py3-none-any.whl/votingai/__init__.py
@@ -0,0 +1,6 @@
+"""VotingAI - Democratic Multi-Agent Systems Built on AutoGen."""
+
+from .voting_group_chat import VoteType, VotingGroupChat, VotingMethod, VotingPhase
+
+__version__ = "1.0.1"
+__all__ = ["VotingGroupChat", "VotingMethod", "VoteType", "VotingPhase"]
