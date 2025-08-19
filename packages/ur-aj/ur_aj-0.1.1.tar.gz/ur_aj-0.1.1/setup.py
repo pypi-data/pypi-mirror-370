469d13503fe64f393b371f6a4cc7ccea36885b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 03:22:47 +0000

    Add package ur-aj/0.1.1/ur_aj-0.1.1.tar.gz

---
+++ packages/ur-aj/ur_aj-0.1.1.tar.gz/ur_aj-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Read README.md for long description
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="ur-aj",
+    version="0.1.1",
+    description="Python library for controlling Universal Robots via sockets",
+    long_description=long_description,                # <-- Added
+    long_description_content_type="text/markdown",    # <-- Important for PyPI
+    author="Akshay Ajit Bhawar",
+    author_email="akshayfusion1@gmail.com",
+    url="https://github.com/Akshaybhawarkiti/UR-aj",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
