636656e570224b37a133500fd933c8ae19b55636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:13 +0000

    Add package tallyfy/1.0.6/tallyfy-1.0.6.tar.gz

---
+++ packages/tallyfy/tallyfy-1.0.6.tar.gz/tallyfy-1.0.6/tallyfy/__init__.py
@@ -0,0 +1,24 @@
+"""
+Tallyfy SDK - A modular Python SDK for Tallyfy API
+"""
+
+from .core import TallyfySDK, TallyfyError
+from .models import *
+from .user_management import UserManager, UserManagement
+from .task_management import TaskManager, TaskManagement
+from .template_management import TemplateManager, TemplateManagement
+from .form_fields_management import FormFieldManager, FormFieldManagement
+
+__version__ = "1.0.0"
+__all__ = [
+    "TallyfySDK",
+    "TallyfyError",
+    "UserManager",
+    "UserManagement", 
+    "TaskManager",
+    "TaskManagement",
+    "TemplateManager",
+    "TemplateManagement",
+    "FormFieldManager",
+    "FormFieldManagement"
+]
