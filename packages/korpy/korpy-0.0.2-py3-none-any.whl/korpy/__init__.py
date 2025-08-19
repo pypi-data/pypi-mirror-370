72083d68b004bbe5bdb32c534b2199d14e5bfa4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:05:30 +0000

    Add package korpy/0.0.2/korpy-0.0.2-py3-none-any.whl

---
+++ packages/korpy/korpy-0.0.2-py3-none-any.whl/korpy/__init__.py
@@ -0,0 +1,3 @@
+from .core import vowels, conostants, extend, combine, group, finals, is_korean, get_sound
+
+__all__ = ["vowels", "conostants", "extend", "combine", "group", "finals", "is_korean", "get_sound"]
