9338b67ad1fafd42c8d9d224b0f03154ce50d964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:06:31 +0000

    Add package superlinked/33.7.8/superlinked-33.7.8-py3-none-any.whl

---
+++ packages/superlinked/superlinked-33.7.8-py3-none-any.whl/superlinked/framework/common/schema/schema_factory.py
@@ -0,0 +1,122 @@
+# Copyright 2024 Superlinked, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import inspect
+from dataclasses import dataclass
+from types import NoneType, UnionType
+
+from beartype.typing import Sequence, Union, get_args
+
+from superlinked.framework.common.exception import InvalidStateException
+from superlinked.framework.common.schema.event_schema_object import (
+    CreatedAtField,
+    EventSchemaObject,
+)
+from superlinked.framework.common.schema.general_type import T
+from superlinked.framework.common.schema.id_field import IdField
+from superlinked.framework.common.schema.id_schema_object import IdSchemaObject
+from superlinked.framework.common.schema.schema_field_descriptor import (
+    SchemaFieldDescriptor,
+)
+from superlinked.framework.common.schema.schema_object import SchemaField
+from superlinked.framework.common.schema.schema_type import SchemaType
+from superlinked.framework.common.schema.schema_validator import SchemaValidator
+from superlinked.framework.common.util.generic_class_util import GenericClassUtil
+
+
+@dataclass(frozen=True)
+class SchemaInformation:
+    id_field_name: str
+    schema_field_descriptors: list[SchemaFieldDescriptor]
+    base_class: type[IdSchemaObject | EventSchemaObject]
+
+
+class SchemaFactory:
+    @staticmethod
+    def calculate_schema_information(schema_cls: type[T], schema_type: SchemaType) -> SchemaInformation:
+        schema_validator = SchemaValidator(schema_type)
+        base_class = IdSchemaObject if schema_type == SchemaType.SCHEMA else EventSchemaObject
+        schema_field_descriptors = [
+            SchemaFactory.compile_annotation(name, type_annotation)
+            for name, type_annotation in schema_cls.__annotations__.items()
+        ]
+        schema_validator.validate_class_attributes(schema_field_descriptors)
+        schema_field_descriptors = SchemaFactory.filter_schema_field_descriptors(schema_field_descriptors, base_class)
+        return SchemaInformation(
+            SchemaFactory.get_field_name_or_raise(schema_cls, IdField), schema_field_descriptors, base_class
+        )
+
+    @staticmethod
+    def compile_annotation(name: str, type_annotation: type) -> SchemaFieldDescriptor:
+        type_ = GenericClassUtil.if_not_class_get_origin(type_annotation)
+        arg_types = get_args(type_annotation)
+        if type_ is UnionType or type_ is Union:
+            non_none_arg_types = [arg_type for arg_type in arg_types if arg_type is not NoneType]
+            if len(non_none_arg_types) != 1:
+                raise InvalidStateException(
+                    "An attribute of a schema can only have one optional or mandatory type-annotation.",
+                    attribute=name,
+                    annotation_details=[arg_type.__name__ for arg_type in non_none_arg_types],
+                )
+            return SchemaFieldDescriptor(
+                name=name,
+                type_=next(iter(non_none_arg_types), None.__class__),
+                nullable=len(arg_types) != 1,
+                arg_types=arg_types,
+            )
+        return SchemaFieldDescriptor(name=name, type_=type_, nullable=False, arg_types=arg_types)
+
+    @staticmethod
+    def filter_schema_field_descriptors(
+        descriptors: Sequence[SchemaFieldDescriptor], base_class: type[IdSchemaObject | EventSchemaObject]
+    ) -> list[SchemaFieldDescriptor]:
+        return [
+            descriptor
+            for descriptor in descriptors
+            if issubclass(descriptor.type_, get_args(base_class.get_schema_field_type()))
+        ]
+
+    @staticmethod
+    def get_field_name_or_raise(schema_cls: type[T], field_type: type) -> str:
+        return next(
+            name
+            for name, type_ in schema_cls.__annotations__.items()
+            if inspect.isclass(type_) and issubclass(type_, field_type)
+        )
+
+    @staticmethod
+    def decorate(schema_cls: type[T], schema_type: SchemaType) -> type[IdSchemaObject | EventSchemaObject]:
+        SchemaValidator(schema_type).check_unannotated_members(schema_cls)
+        schema_information = SchemaFactory.calculate_schema_information(schema_cls, schema_type)
+
+        class Decorated(schema_information.base_class):  # type: ignore
+            def __init__(self) -> None:
+                if schema_information.base_class is EventSchemaObject:
+                    created_at_field_name = SchemaFactory.get_field_name_or_raise(schema_cls, CreatedAtField)
+                    super().__init__(
+                        schema_cls,
+                        schema_information.id_field_name,
+                        created_at_field_name,
+                    )
+                else:
+                    super().__init__(schema_cls, schema_information.id_field_name)
+
+            def _init_schema_fields(self) -> Sequence[SchemaField]:
+                """Returns all declared SchemaFields. Does not include the mandatory "id" field."""
+                return [
+                    self._init_field(schema_field_descriptor)
+                    for schema_field_descriptor in schema_information.schema_field_descriptors
+                ]
+
+        return Decorated
---
+++ packages/superlinked/superlinked-33.7.8-py3-none-any.whl/superlinked/framework/common/space/embedding/model_based/embedding_engine_manager.py
@@ -0,0 +1,154 @@
+# Copyright 2024 Superlinked, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import structlog
+from beartype.typing import Mapping, Sequence
+
+from superlinked.framework.common.data_types import Vector
+from superlinked.framework.common.exception import NotImplementedException
+from superlinked.framework.common.settings import settings
+from superlinked.framework.common.space.embedding.model_based.embedding_input import (
+    ModelEmbeddingInputT,
+)
+from superlinked.framework.common.space.embedding.model_based.engine.embedding_engine import (
+    EmbeddingEngine,
+)
+from superlinked.framework.common.space.embedding.model_based.engine.embedding_engine_config import (
+    EmbeddingEngineConfig,
+)
+from superlinked.framework.common.space.embedding.model_based.engine.modal_engine import (
+    ModalEngine,
+)
+from superlinked.framework.common.space.embedding.model_based.engine.open_clip_engine import (
+    HF_HUB_PREFIX,
+    OpenCLIPEngine,
+)
+from superlinked.framework.common.space.embedding.model_based.engine.sentence_transformers_engine import (
+    SentenceTransformersEngine,
+)
+from superlinked.framework.common.space.embedding.model_based.model_dimension_cache import (
+    MODEL_DIMENSION_BY_NAME,
+)
+from superlinked.framework.common.space.embedding.model_based.model_handler import (
+    ModelHandler,
+    ModelHandlerType,
+    TextModelHandler,
+)
+from superlinked.framework.common.telemetry.telemetry_registry import telemetry
+
+ENGINE_BY_HANDLER: Mapping[ModelHandlerType, type[EmbeddingEngine]] = {
+    TextModelHandler.SENTENCE_TRANSFORMERS: SentenceTransformersEngine,
+    TextModelHandler.MODAL: ModalEngine,
+    ModelHandler.SENTENCE_TRANSFORMERS: SentenceTransformersEngine,
+    ModelHandler.OPEN_CLIP: OpenCLIPEngine,
+    ModelHandler.MODAL: ModalEngine,
+}
+
+logger = structlog.getLogger()
+
+
+class EmbeddingEngineManager:
+    def __init__(self) -> None:
+        self._engines: dict[str, EmbeddingEngine] = {}
+
+    def get_engine(
+        self,
+        model_handler: ModelHandlerType,
+        model_name: str,
+        model_cache_dir: Path | None,
+        config: EmbeddingEngineConfig,
+    ) -> EmbeddingEngine:
+        """
+        Get or create an embedding engine based on the provided config.
+        Returns existing engine if one with matching parameters exists.
+        """
+        engine_key = self.calculate_engine_key(model_handler, model_name, model_cache_dir, config)
+        if engine_key in self._engines:
+            return self._engines[engine_key]
+        if engine_type := ENGINE_BY_HANDLER.get(model_handler):
+            engine = engine_type(model_name, model_cache_dir, config)
+            self._engines[engine_key] = engine
+            return engine
+        raise NotImplementedException("Unsupported model handler.", model_handler=model_handler.value)
+
+    @classmethod
+    def calculate_engine_key(
+        cls,
+        model_handler: ModelHandlerType,
+        model_name: str,
+        model_cache_dir: Path | None,
+        config: EmbeddingEngineConfig,
+    ) -> str:
+        clean_model_name = cls._get_clean_model_name(model_handler, model_name)
+        parts = [model_handler.value, clean_model_name, config, model_cache_dir]
+        engine_key = ":".join(str(part) for part in parts if part is not None)
+        return engine_key
+
+    @classmethod
+    def _get_clean_model_name(cls, model_handler: ModelHandlerType, model_name: str) -> str:
+        if cls._is_sentence_transformers_model_without_prefix(model_handler, model_name):
+            return f"sentence-transformers/{model_name}"
+        return model_name.replace(HF_HUB_PREFIX, "")
+
+    @classmethod
+    def _is_sentence_transformers_model_without_prefix(cls, model_handler: ModelHandlerType, model_name: str) -> bool:
+        return (
+            model_handler in [TextModelHandler.SENTENCE_TRANSFORMERS, ModelHandler.SENTENCE_TRANSFORMERS]
+            and "/" not in model_name
+        )
+
+    def clear_engines(self) -> None:
+        self._engines.clear()
+
+    async def embed(  # pylint: disable=too-many-arguments
+        self,
+        model_handler: ModelHandlerType,
+        model_name: str,
+        inputs: Sequence[ModelEmbeddingInputT],
+        is_query_context: bool,
+        model_cache_dir: Path | None,
+        config: EmbeddingEngineConfig,
+    ) -> list[Vector]:
+        if not inputs:
+            return []
+        engine = self.get_engine(model_handler, model_name, model_cache_dir, config)
+        labels = {
+            "model_name": model_name,
+            "handler": model_handler.value,
+            "is_query_context": str(is_query_context),
+            "precision": config.precision.value,
+        }
+        telemetry.record_metric("engine.embed.count", len(inputs), labels=labels)
+        with telemetry.span("engine.embed", attributes=labels):
+            embeddings = await engine.embed(inputs, is_query_context)
+        return [Vector(embedding) for embedding in embeddings]
+
+    async def calculate_length(
+        self,
+        model_handler: ModelHandlerType,
+        model_name: str,
+        model_cache_dir: Path | None,
+        config: EmbeddingEngineConfig,
+    ) -> int:
+        clean_model_name = self._get_clean_model_name(model_handler, model_name)
+        if not settings.MODEL_WARMUP and clean_model_name in MODEL_DIMENSION_BY_NAME:
+            length = MODEL_DIMENSION_BY_NAME[clean_model_name]
+            return length
+        length = await self.get_engine(model_handler, model_name, model_cache_dir, config).length
+        logger.info("Consider caching model dimension.", model_name=clean_model_name, dimension=length)
+        return length
