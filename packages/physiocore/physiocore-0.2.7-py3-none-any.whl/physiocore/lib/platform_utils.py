5c77d75b98b8d3ab6d4e10549986a416e8ba298e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:28 +0000

    Add package physiocore/0.2.7/physiocore-0.2.7-py3-none-any.whl

---
+++ packages/physiocore/physiocore-0.2.7-py3-none-any.whl/physiocore/lib/platform_utils.py
@@ -0,0 +1,16 @@
+# Have platform specific things go here
+import platform
+
+import cv2
+
+save_video_codec_mac = cv2.VideoWriter_fourcc('M','J','P','G')
+save_video_codec_win = cv2.VideoWriter_fourcc(*'MP4V')
+
+save_video_codec = None
+
+if platform.system() == 'Darwin':
+	save_video_codec = save_video_codec_mac
+elif platform.system() == 'Windows':
+	save_video_codec = save_video_codec_win
+else:
+	raise Exception('Unsupported platform currently')
