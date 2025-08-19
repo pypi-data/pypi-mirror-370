93cc447f9a9d5c8a1d77b9b1cc68d5fadc21cbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:37 +0000

    Add package catt/0.13.0/catt-0.13.0-py3-none-any.whl

---
+++ packages/catt/catt-0.13.0-py3-none-any.whl/catt/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import sys
+
+if sys.version_info.major < 3:
+    print("This program requires Python 3 and above to run.")
+    sys.exit(1)
+
+__codename__ = "Zaniest Zapper"
