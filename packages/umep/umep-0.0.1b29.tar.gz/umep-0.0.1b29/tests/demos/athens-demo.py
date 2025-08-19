bbf9d09df734b619fbe84a160b2ba47f0972c90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 04:04:22 +0000

    Add package umep/0.0.1b29/umep-0.0.1b29.tar.gz

---
+++ packages/umep/umep-0.0.1b29.tar.gz/umep-0.0.1b29/tests/demos/athens-demo.py
@@ -0,0 +1,49 @@
+# %%
+from pathlib import Path
+
+from umep import (
+    skyviewfactor_algorithm,
+    wall_heightaspect_algorithm,
+)
+from umep.functions.SOLWEIGpython import solweig_runner_core
+
+# working folder
+working_folder = "tests/data/athens"
+working_path = Path(working_folder).absolute()
+working_path.mkdir(parents=True, exist_ok=True)
+working_path_str = str(working_path)
+# output folder
+output_folder = "temp/athens"
+output_folder_path = Path(output_folder).absolute()
+output_folder_path.mkdir(parents=True, exist_ok=True)
+output_folder_path_str = str(output_folder_path)
+# extents
+total_extents = [476800, 4205850, 477200, 4206250]
+
+# %%
+# wall info for SOLWEIG
+if not Path(output_folder_path_str + "/walls/").exists():
+    wall_heightaspect_algorithm.generate_wall_hts(
+        dsm_path=working_path_str + "/DSM.tif",
+        bbox=total_extents,
+        out_dir=output_folder_path_str + "/walls",
+    )
+
+# %%
+# skyview factor for SOLWEIG
+if not Path(output_folder_path_str + "/svf/").exists():
+    skyviewfactor_algorithm.generate_svf(
+        dsm_path=working_path_str + "/DSM.tif",
+        bbox=total_extents,
+        out_dir=output_folder_path_str + "/svf",
+        cdsm_path=working_path_str + "/CDSM.tif",
+        trans_veg_perc=3,
+    )
+
+# %%
+
+SRC = solweig_runner_core.SolweigRunCore(
+    "tests/data/athens/configsolweig.ini",
+    "tests/data/athens/parametersforsolweig.json",
+)
+SRC.run()
