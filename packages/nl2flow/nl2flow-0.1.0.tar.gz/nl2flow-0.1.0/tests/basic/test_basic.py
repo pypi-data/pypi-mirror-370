a398b665a3ef65205d1cc67a912f0e22dd3228e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:06:35 +0000

    Add package nl2flow/0.1.0/nl2flow-0.1.0.tar.gz

---
+++ packages/nl2flow/nl2flow-0.1.0.tar.gz/nl2flow-0.1.0/tests/basic/test_basic.py
@@ -0,0 +1,31 @@
+from nl2flow.compile.flow import Flow
+from nl2flow.compile.operators import ClassicalOperator as Operator
+from nl2flow.plan.planners.kstar import Kstar
+from nl2flow.compile.schemas import SignatureItem, GoalItem, GoalItems
+from nl2flow.printers.codelike import CodeLikePrint
+
+PLANNER = Kstar()
+
+
+def test_basic() -> None:
+    new_flow = Flow("Basic Test")
+
+    find_errors_api = Operator("Find Errors")
+    find_errors_api.add_input(SignatureItem(parameters=["database link"]))
+    find_errors_api.add_output(SignatureItem(parameters=["list of errors"]))
+
+    fix_errors_api = Operator("Fix Errors")
+    fix_errors_api.add_input(SignatureItem(parameters=["list of errors"]))
+
+    new_flow.add([find_errors_api, fix_errors_api])
+
+    goal = GoalItems(goals=GoalItem(goal_name="Fix Errors"))
+    new_flow.add(goal)
+
+    pddl, _ = new_flow.compile_to_pddl()
+    _ = pddl  # explore the pddl
+
+    plans = new_flow.plan_it(PLANNER)
+    print(CodeLikePrint.pretty_print(plans))
+
+    assert plans.list_of_plans, "There should be plans."
