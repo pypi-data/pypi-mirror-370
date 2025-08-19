4006ddc9e40b7171f8d7690e68b0f713239c62cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:05:30 +0000

    Add package agtools/1.0.0/agtools-1.0.0-py3-none-any.whl

---
+++ packages/agtools/agtools-1.0.0-py3-none-any.whl/agtools/__init__.py
@@ -0,0 +1,10 @@
+"""agtools: Tools for manipulating assembly graphs"""
+
+__author__ = "Vijini Mallawaarachchi"
+__copyright__ = "Copyright 2025, agtools Project"
+__credits__ = ["Vijini Mallawaarachchi"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Vijini Mallawaarachchi"
+__email__ = "viji.mallawaarachchi@gmail.com"
+__status__ = "Production"
