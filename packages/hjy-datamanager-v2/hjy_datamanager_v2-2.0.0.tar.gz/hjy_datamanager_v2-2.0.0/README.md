225306591ab34ae070e7570d33ccf3cc9eea6494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:06:02 +0000

    Add package hjy-datamanager-v2/2.0.0/hjy_datamanager_v2-2.0.0.tar.gz

---
+++ packages/hjy-datamanager-v2/hjy_datamanager_v2-2.0.0.tar.gz/hjy_datamanager_v2-2.0.0/README.md
@@ -0,0 +1,16 @@
+# hjy-datamanager-v2
+
+Hjy datamanager package version 2.0.0.
+
+## Installation
+
+```bash
+pip install hjy-datamanager-v2
+```
+
+## Usage
+
+```python
+import hjy_datamanager_v2
+print(hjy_datamanager_v2.__version__)
+```
---
+++ packages/hjy-datamanager-v2/hjy_datamanager_v2-2.0.0.tar.gz/hjy_datamanager_v2-2.0.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="hjy-datamanager-v2",
+    version="2.0.0",
+    description="Hjy datamanager package",
+    author="Hjy",
+    author_email="hjy@example.com",
+    packages=find_packages(),
+    install_requires=[
+        "loguru>=0.7.0",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
