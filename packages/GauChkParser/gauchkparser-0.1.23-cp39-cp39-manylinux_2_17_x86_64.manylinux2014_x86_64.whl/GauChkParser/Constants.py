902c5f774d4171ec8b82f32b0c93424cdaf0f7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 05:05:48 +0000

    Add package GauChkParser/0.1.23/gauchkparser-0.1.23-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/GauChkParser/gauchkparser-0.1.23-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/GauChkParser/Constants.py
@@ -0,0 +1,23 @@
+Bohr2Ang = 0.529177249
+""" Bohr to Angstrom constant in 1986, the value used in GaussView 6.0
+
+https://doi.org/10.1103/RevModPhys.59.1121
+"""
+
+Bohr2Ang1998 = 0.5291772083
+""" Borh to Angstrom constant in 1998, the value used in Gaussian 03
+
+https://physics.nist.gov/cuu/pdf/all_1998.pdf
+"""
+
+Bohr2Ang2006 = 0.52917720859
+""" Borh to Angstrom constant in 2006, the value used in Gaussian 09
+
+https://physics.nist.gov/cuu/pdf/all_2006.pdf
+"""
+
+Bohr2Ang2010 = 0.52917721092
+""" Borh to Angstrom constant in 2010, the value used in Gaussian 16
+
+https://physics.nist.gov/cuu/Constants/Preprints/lsa2010.pdf
+"""
