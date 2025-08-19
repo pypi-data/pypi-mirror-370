64bcb21e97dbc1ed6b77f5462003670cc9b55a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 07:04:29 +0000

    Add package hypha/0.21.8/hypha-0.21.8-py3-none-any.whl

---
+++ packages/hypha/hypha-0.21.8-py3-none-any.whl/hypha/startup.py
@@ -0,0 +1,46 @@
+import asyncio
+import logging
+import os
+import sys
+
+
+LOGLEVEL = os.environ.get("HYPHA_LOGLEVEL", "WARNING").upper()
+logging.basicConfig(level=LOGLEVEL, stream=sys.stdout)
+logger = logging.getLogger("services")
+logger.setLevel(LOGLEVEL)
+
+import importlib.util
+from importlib import import_module
+
+
+def _load_function(module_path, entrypoint):
+    if module_path.endswith(".py"):
+        spec = importlib.util.spec_from_file_location("user_module", module_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+    else:
+        module = import_module(module_path)
+
+    return getattr(module, entrypoint)
+
+
+async def run_startup_function(store: any, startup_function_uri: str):
+    parts = startup_function_uri.split(":")
+    module_path = parts[0]
+    assert not module_path.endswith(".py") or os.path.exists(
+        module_path
+    ), f"Module {module_path} does not exist"
+    entrypoint = parts[1] if len(parts) > 1 else None
+    assert (
+        entrypoint
+    ), f"Entrypoint is required for {startup_function_uri}, please use {startup_function_uri}:entrypoint_function"
+
+    # load the python module and get the entrypoint
+    load_func = _load_function(module_path, entrypoint)
+    # make sure the load_func is a coroutine
+    assert asyncio.iscoroutinefunction(
+        load_func
+    ), f"Entrypoint {entrypoint} is not a coroutine"
+    server = await store.get_public_api()
+    await load_func(server)
+    logger.info(f"Successfully executed the startup function: {startup_function_uri}")
