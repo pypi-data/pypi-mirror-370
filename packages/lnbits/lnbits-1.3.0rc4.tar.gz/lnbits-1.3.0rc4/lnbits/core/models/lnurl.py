aa411db7335c9bfdd44ebcc76eb921bfa262702d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 17:05:07 +0000

    Add package lnbits/1.3.0rc4/lnbits-1.3.0rc4.tar.gz

---
+++ packages/lnbits/lnbits-1.3.0rc4.tar.gz/lnbits-1.3.0rc4/lnbits/core/models/lnurl.py
@@ -0,0 +1,32 @@
+from time import time
+from typing import Optional
+
+from lnurl import LnAddress, Lnurl, LnurlPayResponse
+from pydantic import BaseModel, Field
+
+
+class CreateLnurlPayment(BaseModel):
+    res: LnurlPayResponse | None = None
+    lnurl: Lnurl | LnAddress | None = None
+    amount: int
+    comment: Optional[str] = None
+    unit: Optional[str] = None
+    internal_memo: Optional[str] = None
+
+
+class CreateLnurlWithdraw(BaseModel):
+    lnurl_w: Lnurl
+
+
+class LnurlScan(BaseModel):
+    lnurl: Lnurl | LnAddress
+
+
+class StoredPayLink(BaseModel):
+    lnurl: str
+    label: str
+    last_used: int = Field(default_factory=lambda: int(time()))
+
+
+class StoredPayLinks(BaseModel):
+    links: list[StoredPayLink] = []
