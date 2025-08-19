6137a5db6481fdff074de96954ea0eadc9e20e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 16:07:01 +0000

    Add package anclient.py3/0.1.3/anclient_py3-0.1.3.tar.gz

---
+++ packages/anclient.py3/anclient_py3-0.1.3.tar.gz/anclient_py3-0.1.3/test/t_02_ping.py
@@ -0,0 +1,31 @@
+import sys
+import unittest
+
+from anson.io.odysz.anson import Anson
+
+from src.anclient.io.odysz.jclient import Clients
+from src.io.odysz.semantic.jprotocol import AnsonResp, MsgCode
+
+
+class AnclientTest(unittest.TestCase):
+    def testPing(self):
+        Anson.java_src('src')
+        # err = OnError(lambda c, e, args: print(c, e.format(args), file=sys.stderr) and self.fail(e))
+        def err_ctx (c: MsgCode, e: str, *args: str) -> None:
+            print(c, e.format(args), file=sys.stderr)
+            self.fail(e)
+
+        Clients.servRt = 'http://127.0.0.1:8964/jserv-album'
+        resp = Clients.pingLess('Anson.py3/test', err_ctx)
+        self.assertIsNotNone(resp)
+
+        print(Clients.servRt, '<echo>', resp.toBlock())
+        self.assertEqual(type(resp.body[0]), AnsonResp)
+        self.assertEqual('ok', resp.code, resp.body[0].msg()) # TODO MsgCode.ok
+
+
+if __name__ == '__main__':
+    unittest.main()
+    t = AnclientTest()
+    t.testPing()
+
