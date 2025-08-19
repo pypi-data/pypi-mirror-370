c1350be9e5e770ec2512f7c18727d49f8efd5373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:05:40 +0000

    Add package pytest-modern/0.7.4/pytest_modern-0.7.4.tar.gz

---
+++ packages/pytest-modern/pytest_modern-0.7.4.tar.gz/pytest_modern-0.7.4/README.md
@@ -0,0 +1,12 @@
+# Modern Pytest
+
+[![uv](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/uv/main/assets/badge/v0.json)](https://github.com/astral-sh/uv)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+![GitHub License](https://img.shields.io/github/license/zen-xu/pytest-modern)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pytest-modern)
+![PyPI - Version](https://img.shields.io/pypi/v/pytest-modern)
+
+This plugin incorporates ideas from the implementation of [pytest-rich](https://github.com/nicoddemus/pytest-rich) and takes inspiration from the output style of [nextest](https://nexte.st).
+
+With the plugin currently outputs this:
+<a href="https://asciinema.org/a/728361" target="_blank"><img src="https://asciinema.org/a/728361.svg" /></a>
---
+++ packages/pytest-modern/pytest_modern-0.7.4.tar.gz/pytest_modern-0.7.4/tests/test_example.py
@@ -0,0 +1,81 @@
+import warnings
+
+import pytest
+
+
+class ChoiceError(Exception): ...
+
+
+def test_passed():
+    a = 1
+    b = 2
+    assert a + b == 3
+
+
+def test_failed():
+    raise RuntimeError("something wrong")
+
+
+@pytest.mark.skip(reason="skipped")
+def test_skipped():
+    a = 1
+    b = 2
+    assert a + b == 2
+
+
+@pytest.mark.xfail(reason="expected to fail but passed")
+def test_xfail():
+    a = 1
+    b = 2
+    assert a + b == 4
+
+
+@pytest.mark.xfail(reason="expected to fail but passed", strict=True)
+def test_xfail_strict():
+    a = 1
+    b = 2
+    assert a + b == 3
+
+
+@pytest.mark.xfail
+def test_xpass():
+    pass
+
+
+def test_warning():
+    warnings.warn("this is a warning", UserWarning, stacklevel=1)
+
+
+@pytest.mark.timeout(1)
+def test_timeout():
+    import time
+
+    time.sleep(3)
+
+
+def test_timeout_from_config():
+    import time
+
+    time.sleep(3)
+
+
+@pytest.mark.flaky(reruns=5)
+def test_rerun():
+    import random
+
+    if not random.choice([False, False, False, True]):
+        raise ChoiceError("choice is not False")
+
+
+def test_rerun_from_config():
+    import random
+
+    if not random.choice([False, False, False, True]):
+        raise ChoiceError("choice is not False")
+
+
+class TestGroup:
+    def test_ok(self): ...
+    def test_fail(self):
+        a = 1
+        assert a == 2
