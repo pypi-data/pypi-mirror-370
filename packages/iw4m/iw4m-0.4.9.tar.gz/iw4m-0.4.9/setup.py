48ce11078298844e8f5110b1142c831573d87bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 03:22:30 +0000

    Add package iw4m/0.4.9/iw4m-0.4.9.tar.gz

---
+++ packages/iw4m/iw4m-0.4.9.tar.gz/iw4m-0.4.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iw4m",
+    version="0.4.9",
+    author="budiworld",
+    author_email="budi.world@yahoo.com",
+    description="A Python wrapper for the IW4M-Admin API",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Yallamaztar/iw4m", 
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "aiohttp",
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
