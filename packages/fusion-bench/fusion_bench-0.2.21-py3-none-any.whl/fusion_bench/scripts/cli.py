3d5d53885365c99591030928564e8d301cc4e43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 03:22:49 +0000

    Add package fusion-bench/0.2.21/fusion_bench-0.2.21-py3-none-any.whl

---
+++ packages/fusion-bench/fusion_bench-0.2.21-py3-none-any.whl/fusion_bench/scripts/cli.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+This is the CLI script that is executed when the user runs the `fusion_bench` command.
+The script is responsible for parsing the command-line arguments, loading the configuration file, and running the fusion algorithm.
+"""
+
+import importlib
+import importlib.resources
+import logging
+import os
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+from fusion_bench.programs import BaseHydraProgram
+from fusion_bench.utils import instantiate
+from fusion_bench.constants import PROJECT_ROOT_PATH
+
+log = logging.getLogger(__name__)
+
+
+def _get_default_config_path():
+    for config_dir in ["fusion_bench_config", "config"]:
+        for config_path_root in [os.getcwd(), PROJECT_ROOT_PATH]:
+            config_path = os.path.join(config_path_root, config_dir)
+            if os.path.exists(config_path) and os.path.isdir(config_path):
+                return os.path.abspath(config_path)
+    return None
+
+
+@hydra.main(
+    config_path=_get_default_config_path(),
+    config_name="fabric_model_fusion",
+    version_base=None,
+)
+def main(cfg: DictConfig) -> None:
+    OmegaConf.resolve(cfg)
+    program: BaseHydraProgram = instantiate(cfg)
+    program.run()
+
+
+if __name__ == "__main__":
+    main()
