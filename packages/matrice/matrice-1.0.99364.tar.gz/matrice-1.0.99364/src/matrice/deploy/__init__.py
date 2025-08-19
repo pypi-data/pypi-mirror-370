c9349c55e366171383708a225945c2fb6da03108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:05:30 +0000

    Add package matrice/1.0.99364/matrice-1.0.99364.tar.gz

---
+++ packages/matrice/matrice-1.0.99364.tar.gz/matrice-1.0.99364/src/matrice/deploy/__init__.py
@@ -0,0 +1,42 @@
+"""Module providing __init__ functionality."""
+
+from matrice.utils import dependencies_check
+
+dependencies_check(
+    [
+        "httpx",
+        "fastapi",
+        "uvicorn",
+        "pillow",
+        "confluent_kafka[snappy]",
+        "aiokafka",
+        "filterpy",
+        "scipy",
+        "scikit-learn",
+        "matplotlib",
+        "scikit-image",
+        "python-snappy",
+        "pyyaml",
+        "imagehash",
+        "easyocr",
+        "torch",
+        "opencv-python"
+    ]
+)
+
+if not dependencies_check(["opencv-python"]):
+    dependencies_check(["opencv-python-headless"])
+
+from matrice.deploy.server.server import MatriceDeployServer  # noqa: E402
+from matrice.deploy.server.server import MatriceDeployServer as MatriceDeploy  # noqa: E402 # Keep this for backwards compatibility
+from matrice.deploy.server.inference.inference_interface import InferenceInterface  # noqa: E402
+from matrice.deploy.server.proxy.proxy_interface import MatriceProxyInterface  # noqa: E402
+from matrice.deploy.client import MatriceDeployClient  # noqa: E402
+
+__all__ = [
+    "MatriceDeploy",
+    "MatriceDeployServer",
+    "InferenceInterface",
+    "MatriceProxyInterface",
+    "MatriceDeployClient"
+]
