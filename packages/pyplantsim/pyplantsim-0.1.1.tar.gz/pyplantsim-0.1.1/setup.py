2301263dd3e22c5306c8e6e41f8fe90d9bff649a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 05:05:20 +0000

    Add package pyplantsim/0.1.1/pyplantsim-0.1.1.tar.gz

---
+++ packages/pyplantsim/pyplantsim-0.1.1.tar.gz/pyplantsim-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyplantsim",
+    version="0.1.1",
+    description="A Python wrapper to Siemens Tecnomatix Plant Simulation COM Interface",
+    keywords=["plant", "siemens", "simulation", "COM"],
+    author="Luca Bernstiel",
+    author_email="bernstiel@gmx.de",
+    packages=find_packages(),
+    install_requires=[],
+)
