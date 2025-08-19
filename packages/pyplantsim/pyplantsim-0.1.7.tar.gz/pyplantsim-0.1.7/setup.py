3cb48672bbdfe120aed82e57dc45fafa1b78635f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 16:06:08 +0000

    Add package pyplantsim/0.1.7/pyplantsim-0.1.7.tar.gz

---
+++ packages/pyplantsim/pyplantsim-0.1.7.tar.gz/pyplantsim-0.1.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="pyplantsim",
+    version="0.1.7",
+    description="A Python wrapper for Siemens Tecnomatix Plant Simulation COM Interface",
+    keywords=["plant", "siemens", "simulation", "COM"],
+    url="https://github.com/malun22/pyplantsim",
+    author="Luca Bernstiel",
+    author_email="bernstiel@gmx.de",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "colorama>=0.4.6",
+        "loguru>=0.7.3",
+        "pywin32>=311; platform_system=='Windows'",
+        "setuptools>=80.9.0",
+        "win32_setctime>=1.2.0; platform_system=='Windows'",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    package_data={
+        "pyplantsim": ["sim_talk_scripts/*.st"],
+    },
+)
