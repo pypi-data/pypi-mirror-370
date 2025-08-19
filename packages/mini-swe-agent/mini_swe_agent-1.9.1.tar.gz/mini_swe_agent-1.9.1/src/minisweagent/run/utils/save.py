b97342868cca08c068a9bd7fab53255c21f6ef6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:23 +0000

    Add package mini-swe-agent/1.9.1/mini_swe_agent-1.9.1.tar.gz

---
+++ packages/mini-swe-agent/mini_swe_agent-1.9.1.tar.gz/mini_swe_agent-1.9.1/src/minisweagent/run/utils/save.py
@@ -0,0 +1,54 @@
+import json
+from collections.abc import Callable
+from pathlib import Path
+
+from minisweagent import Agent, __version__
+
+
+def save_traj(
+    agent: Agent | None,
+    path: Path,
+    *,
+    print_path: bool = True,
+    exit_status: str | None = None,
+    result: str | None = None,
+    extra_info: dict | None = None,
+    print_fct: Callable = print,
+    **kwargs,
+):
+    """Save the trajectory of the agent to a file.
+
+    Args:
+        agent: The agent to save the trajectory of.
+        path: The path to save the trajectory to.
+        print_path: Whether to print confirmation of path to the terminal.
+        exit_status: The exit status of the agent.
+        result: The result/submission of the agent.
+        extra_info: Extra information to save (will be merged into the info dict).
+        **kwargs: Additional information to save (will be merged into top level)
+
+    """
+    data = {
+        "info": {
+            "exit_status": exit_status,
+            "submission": result,
+            "model_stats": {
+                "instance_cost": 0.0,
+                "api_calls": 0,
+            },
+            "mini_version": __version__,
+        },
+        "messages": [],
+        "trajectory_format": "mini-swe-agent-1",
+    } | kwargs
+    if agent is not None:
+        data["info"]["model_stats"]["instance_cost"] = agent.model.cost
+        data["info"]["model_stats"]["api_calls"] = agent.model.n_calls
+        data["messages"] = agent.messages
+    if extra_info:
+        data["info"].update(extra_info)
+
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(json.dumps(data, indent=2))
+    if print_path:
+        print_fct(f"Saved trajectory to '{path}'")
