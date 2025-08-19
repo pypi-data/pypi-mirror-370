df40ebe78e0e1a966470aeecc6f9ccce851897b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 05:04:54 +0000

    Add package cuda-bindings/12.9.2/cuda_bindings-12.9.2-cp39-cp39-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/cuda-bindings/cuda_bindings-12.9.2-cp39-cp39-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/cuda/bindings/_internal/nvvm_windows.pyx
@@ -0,0 +1,343 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+#
+# This code was automatically generated across versions from 11.0.3 to 12.9.0. Do not modify it directly.
+
+from libc.stdint cimport intptr_t
+import threading
+
+from .utils import FunctionNotFoundError, NotSupportedError
+
+from cuda.pathfinder import load_nvidia_dynamic_lib
+
+import win32api
+
+
+###############################################################################
+# Wrapper init
+###############################################################################
+
+LOAD_LIBRARY_SEARCH_SYSTEM32     = 0x00000800
+LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000
+LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100
+cdef object __symbol_lock = threading.Lock()
+cdef bint __py_nvvm_init = False
+cdef void* __cuDriverGetVersion = NULL
+
+cdef void* __nvvmGetErrorString = NULL
+cdef void* __nvvmVersion = NULL
+cdef void* __nvvmIRVersion = NULL
+cdef void* __nvvmCreateProgram = NULL
+cdef void* __nvvmDestroyProgram = NULL
+cdef void* __nvvmAddModuleToProgram = NULL
+cdef void* __nvvmLazyAddModuleToProgram = NULL
+cdef void* __nvvmCompileProgram = NULL
+cdef void* __nvvmVerifyProgram = NULL
+cdef void* __nvvmGetCompiledResultSize = NULL
+cdef void* __nvvmGetCompiledResult = NULL
+cdef void* __nvvmGetProgramLogSize = NULL
+cdef void* __nvvmGetProgramLog = NULL
+
+
+cdef int _check_or_init_nvvm() except -1 nogil:
+    global __py_nvvm_init
+    if __py_nvvm_init:
+        return 0
+
+    cdef int err, driver_ver = 0
+
+    with gil, __symbol_lock:
+        # Load driver to check version
+        try:
+            handle = win32api.LoadLibraryEx("nvcuda.dll", 0, LOAD_LIBRARY_SEARCH_SYSTEM32)
+        except Exception as e:
+            raise NotSupportedError(f'CUDA driver is not found ({e})')
+        global __cuDriverGetVersion
+        if __cuDriverGetVersion == NULL:
+            __cuDriverGetVersion = <void*><intptr_t>win32api.GetProcAddress(handle, 'cuDriverGetVersion')
+            if __cuDriverGetVersion == NULL:
+                raise RuntimeError('something went wrong')
+        err = (<int (*)(int*) noexcept nogil>__cuDriverGetVersion)(&driver_ver)
+        if err != 0:
+            raise RuntimeError('something went wrong')
+
+        # Load library
+        handle = load_nvidia_dynamic_lib("nvvm")._handle_uint
+
+        # Load function
+        global __nvvmGetErrorString
+        try:
+            __nvvmGetErrorString = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmGetErrorString')
+        except:
+            pass
+
+        global __nvvmVersion
+        try:
+            __nvvmVersion = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmVersion')
+        except:
+            pass
+
+        global __nvvmIRVersion
+        try:
+            __nvvmIRVersion = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmIRVersion')
+        except:
+            pass
+
+        global __nvvmCreateProgram
+        try:
+            __nvvmCreateProgram = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmCreateProgram')
+        except:
+            pass
+
+        global __nvvmDestroyProgram
+        try:
+            __nvvmDestroyProgram = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmDestroyProgram')
+        except:
+            pass
+
+        global __nvvmAddModuleToProgram
+        try:
+            __nvvmAddModuleToProgram = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmAddModuleToProgram')
+        except:
+            pass
+
+        global __nvvmLazyAddModuleToProgram
+        try:
+            __nvvmLazyAddModuleToProgram = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmLazyAddModuleToProgram')
+        except:
+            pass
+
+        global __nvvmCompileProgram
+        try:
+            __nvvmCompileProgram = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmCompileProgram')
+        except:
+            pass
+
+        global __nvvmVerifyProgram
+        try:
+            __nvvmVerifyProgram = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmVerifyProgram')
+        except:
+            pass
+
+        global __nvvmGetCompiledResultSize
+        try:
+            __nvvmGetCompiledResultSize = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmGetCompiledResultSize')
+        except:
+            pass
+
+        global __nvvmGetCompiledResult
+        try:
+            __nvvmGetCompiledResult = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmGetCompiledResult')
+        except:
+            pass
+
+        global __nvvmGetProgramLogSize
+        try:
+            __nvvmGetProgramLogSize = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmGetProgramLogSize')
+        except:
+            pass
+
+        global __nvvmGetProgramLog
+        try:
+            __nvvmGetProgramLog = <void*><intptr_t>win32api.GetProcAddress(handle, 'nvvmGetProgramLog')
+        except:
+            pass
+
+        __py_nvvm_init = True
+        return 0
+
+
+cdef dict func_ptrs = None
+
+
+cpdef dict _inspect_function_pointers():
+    global func_ptrs
+    if func_ptrs is not None:
+        return func_ptrs
+
+    _check_or_init_nvvm()
+    cdef dict data = {}
+
+    global __nvvmGetErrorString
+    data["__nvvmGetErrorString"] = <intptr_t>__nvvmGetErrorString
+
+    global __nvvmVersion
+    data["__nvvmVersion"] = <intptr_t>__nvvmVersion
+
+    global __nvvmIRVersion
+    data["__nvvmIRVersion"] = <intptr_t>__nvvmIRVersion
+
+    global __nvvmCreateProgram
+    data["__nvvmCreateProgram"] = <intptr_t>__nvvmCreateProgram
+
+    global __nvvmDestroyProgram
+    data["__nvvmDestroyProgram"] = <intptr_t>__nvvmDestroyProgram
+
+    global __nvvmAddModuleToProgram
+    data["__nvvmAddModuleToProgram"] = <intptr_t>__nvvmAddModuleToProgram
+
+    global __nvvmLazyAddModuleToProgram
+    data["__nvvmLazyAddModuleToProgram"] = <intptr_t>__nvvmLazyAddModuleToProgram
+
+    global __nvvmCompileProgram
+    data["__nvvmCompileProgram"] = <intptr_t>__nvvmCompileProgram
+
+    global __nvvmVerifyProgram
+    data["__nvvmVerifyProgram"] = <intptr_t>__nvvmVerifyProgram
+
+    global __nvvmGetCompiledResultSize
+    data["__nvvmGetCompiledResultSize"] = <intptr_t>__nvvmGetCompiledResultSize
+
+    global __nvvmGetCompiledResult
+    data["__nvvmGetCompiledResult"] = <intptr_t>__nvvmGetCompiledResult
+
+    global __nvvmGetProgramLogSize
+    data["__nvvmGetProgramLogSize"] = <intptr_t>__nvvmGetProgramLogSize
+
+    global __nvvmGetProgramLog
+    data["__nvvmGetProgramLog"] = <intptr_t>__nvvmGetProgramLog
+
+    func_ptrs = data
+    return data
+
+
+cpdef _inspect_function_pointer(str name):
+    global func_ptrs
+    if func_ptrs is None:
+        func_ptrs = _inspect_function_pointers()
+    return func_ptrs[name]
+
+
+###############################################################################
+# Wrapper functions
+###############################################################################
+
+cdef const char* _nvvmGetErrorString(nvvmResult result) except?NULL nogil:
+    global __nvvmGetErrorString
+    _check_or_init_nvvm()
+    if __nvvmGetErrorString == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmGetErrorString is not found")
+    return (<const char* (*)(nvvmResult) noexcept nogil>__nvvmGetErrorString)(
+        result)
+
+
+cdef nvvmResult _nvvmVersion(int* major, int* minor) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmVersion
+    _check_or_init_nvvm()
+    if __nvvmVersion == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmVersion is not found")
+    return (<nvvmResult (*)(int*, int*) noexcept nogil>__nvvmVersion)(
+        major, minor)
+
+
+cdef nvvmResult _nvvmIRVersion(int* majorIR, int* minorIR, int* majorDbg, int* minorDbg) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmIRVersion
+    _check_or_init_nvvm()
+    if __nvvmIRVersion == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmIRVersion is not found")
+    return (<nvvmResult (*)(int*, int*, int*, int*) noexcept nogil>__nvvmIRVersion)(
+        majorIR, minorIR, majorDbg, minorDbg)
+
+
+cdef nvvmResult _nvvmCreateProgram(nvvmProgram* prog) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmCreateProgram
+    _check_or_init_nvvm()
+    if __nvvmCreateProgram == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmCreateProgram is not found")
+    return (<nvvmResult (*)(nvvmProgram*) noexcept nogil>__nvvmCreateProgram)(
+        prog)
+
+
+cdef nvvmResult _nvvmDestroyProgram(nvvmProgram* prog) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmDestroyProgram
+    _check_or_init_nvvm()
+    if __nvvmDestroyProgram == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmDestroyProgram is not found")
+    return (<nvvmResult (*)(nvvmProgram*) noexcept nogil>__nvvmDestroyProgram)(
+        prog)
+
+
+cdef nvvmResult _nvvmAddModuleToProgram(nvvmProgram prog, const char* buffer, size_t size, const char* name) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmAddModuleToProgram
+    _check_or_init_nvvm()
+    if __nvvmAddModuleToProgram == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmAddModuleToProgram is not found")
+    return (<nvvmResult (*)(nvvmProgram, const char*, size_t, const char*) noexcept nogil>__nvvmAddModuleToProgram)(
+        prog, buffer, size, name)
+
+
+cdef nvvmResult _nvvmLazyAddModuleToProgram(nvvmProgram prog, const char* buffer, size_t size, const char* name) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmLazyAddModuleToProgram
+    _check_or_init_nvvm()
+    if __nvvmLazyAddModuleToProgram == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmLazyAddModuleToProgram is not found")
+    return (<nvvmResult (*)(nvvmProgram, const char*, size_t, const char*) noexcept nogil>__nvvmLazyAddModuleToProgram)(
+        prog, buffer, size, name)
+
+
+cdef nvvmResult _nvvmCompileProgram(nvvmProgram prog, int numOptions, const char** options) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmCompileProgram
+    _check_or_init_nvvm()
+    if __nvvmCompileProgram == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmCompileProgram is not found")
+    return (<nvvmResult (*)(nvvmProgram, int, const char**) noexcept nogil>__nvvmCompileProgram)(
+        prog, numOptions, options)
+
+
+cdef nvvmResult _nvvmVerifyProgram(nvvmProgram prog, int numOptions, const char** options) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmVerifyProgram
+    _check_or_init_nvvm()
+    if __nvvmVerifyProgram == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmVerifyProgram is not found")
+    return (<nvvmResult (*)(nvvmProgram, int, const char**) noexcept nogil>__nvvmVerifyProgram)(
+        prog, numOptions, options)
+
+
+cdef nvvmResult _nvvmGetCompiledResultSize(nvvmProgram prog, size_t* bufferSizeRet) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmGetCompiledResultSize
+    _check_or_init_nvvm()
+    if __nvvmGetCompiledResultSize == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmGetCompiledResultSize is not found")
+    return (<nvvmResult (*)(nvvmProgram, size_t*) noexcept nogil>__nvvmGetCompiledResultSize)(
+        prog, bufferSizeRet)
+
+
+cdef nvvmResult _nvvmGetCompiledResult(nvvmProgram prog, char* buffer) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmGetCompiledResult
+    _check_or_init_nvvm()
+    if __nvvmGetCompiledResult == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmGetCompiledResult is not found")
+    return (<nvvmResult (*)(nvvmProgram, char*) noexcept nogil>__nvvmGetCompiledResult)(
+        prog, buffer)
+
+
+cdef nvvmResult _nvvmGetProgramLogSize(nvvmProgram prog, size_t* bufferSizeRet) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmGetProgramLogSize
+    _check_or_init_nvvm()
+    if __nvvmGetProgramLogSize == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmGetProgramLogSize is not found")
+    return (<nvvmResult (*)(nvvmProgram, size_t*) noexcept nogil>__nvvmGetProgramLogSize)(
+        prog, bufferSizeRet)
+
+
+cdef nvvmResult _nvvmGetProgramLog(nvvmProgram prog, char* buffer) except?_NVVMRESULT_INTERNAL_LOADING_ERROR nogil:
+    global __nvvmGetProgramLog
+    _check_or_init_nvvm()
+    if __nvvmGetProgramLog == NULL:
+        with gil:
+            raise FunctionNotFoundError("function nvvmGetProgramLog is not found")
+    return (<nvvmResult (*)(nvvmProgram, char*) noexcept nogil>__nvvmGetProgramLog)(
+        prog, buffer)
