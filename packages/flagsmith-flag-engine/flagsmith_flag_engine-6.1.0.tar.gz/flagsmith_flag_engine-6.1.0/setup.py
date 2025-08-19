47dde87d10f28d34a45a7b12c5b1eb541f23dfe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 05:05:09 +0000

    Add package flagsmith-flag-engine/6.1.0/flagsmith_flag_engine-6.1.0.tar.gz

---
+++ packages/flagsmith-flag-engine/flagsmith_flag_engine-6.1.0.tar.gz/flagsmith_flag_engine-6.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="flagsmith-flag-engine",
+    version="6.1.0",
+    author="Flagsmith",
+    author_email="support@flagsmith.com",
+    packages=find_packages(include=["flag_engine", "flag_engine.*"]),
+    package_data={"flag_engine": ["py.typed"]},
+    url="https://github.com/Flagsmith/flagsmith-engine",
+    license="BSD3",
+    description="Flag engine for the Flagsmith API.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "pydantic>=2.3.0,<3",
+        "pydantic-collections>=0.5.1,<1",
+        "semver>=3.0.1",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
