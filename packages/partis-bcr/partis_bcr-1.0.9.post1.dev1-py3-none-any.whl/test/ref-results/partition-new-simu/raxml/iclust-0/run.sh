2ba84c80fc036aca3d152fda1d5f876c790a970b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 04:04:50 +0000

    Add package partis-bcr/1.0.9.post1.dev1/partis_bcr-1.0.9.post1.dev1-py3-none-any.whl

---
+++ packages/partis-bcr/partis_bcr-1.0.9.post1.dev1-py3-none-any.whl/test/ref-results/partition-new-simu/raxml/iclust-0/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/runner/work/partis/partis/bin/raxml-ng-linux --model GTR+G --msa /home/runner/work/partis/partis/test/ref-results/partition-new-simu/raxml/iclust-0/input-seqs.fa --msa-format FASTA
+/home/runner/work/partis/partis/bin/raxml-ng-linux --model GTR+G --msa /home/runner/work/partis/partis/test/ref-results/partition-new-simu/raxml/iclust-0/input-seqs.fa --msa-format FASTA --ancestral --tree /home/runner/work/partis/partis/test/ref-results/partition-new-simu/raxml/iclust-0/input-seqs.fa.raxml.ancestralTree
