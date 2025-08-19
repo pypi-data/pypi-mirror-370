5f313eb64e5bfda561bab07d4108d4d43549c6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 05:04:52 +0000

    Add package plex2mix/1.0.1/plex2mix-1.0.1.tar.gz

---
+++ packages/plex2mix/plex2mix-1.0.1.tar.gz/plex2mix-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="plex2mix",
+    version="1.0.1",
+    description="Download Plex playlists and export them to various formats",
+    packages=find_packages(),
+    author='Anatosun',
+    author_email='z4jyol8l@duck.com',
+    url='https://github.com/anatosun/plex2mix',
+    include_package_data=True,
+    install_requires=[
+        "click>=8.0",
+        "plexapi>=4.9",
+        "pyyaml>=6.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'plex2mix=plex2mix.main:cli',
+        ],
+    },
+    python_requires=">=3.8",
+)
