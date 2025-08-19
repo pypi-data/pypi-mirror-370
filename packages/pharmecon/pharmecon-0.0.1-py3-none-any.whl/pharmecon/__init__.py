cf3a02fe2dc56c368b7f1a3c09c199fdb77f6389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 03:22:43 +0000

    Add package pharmecon/0.0.1/pharmecon-0.0.1-py3-none-any.whl

---
+++ packages/pharmecon/pharmecon-0.0.1-py3-none-any.whl/pharmecon/__init__.py
@@ -0,0 +1,12 @@
+"""
+Pharmecon - A Python package for pharmaceutical economics analysis.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Pharmecon Team"
+__email__ = "team@pharmecon.org"
+__description__ = "Pharmecon test package"
+
+from .core import PharmeconAnalyzer
+
+__all__ = ["PharmeconAnalyzer"]
