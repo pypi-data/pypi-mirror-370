76bf054b3ba49b0bb67d8983a5535f56baef83a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 15:06:34 +0000

    Add package second-module/0.1/second_module-0.1.tar.gz

---
+++ packages/second-module/second_module-0.1.tar.gz/second_module-0.1/secondModule/Second_module.py
@@ -0,0 +1,6 @@
+class second:
+    def second_module(self):
+        print("Hello from second module")
+
+obj=second()
+obj.second_module()
---
+++ packages/second-module/second_module-0.1.tar.gz/second_module-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,Extension,find_packages
+
+setup(
+    name="second_module",
+    version="0.1",
+    packages=find_packages(),
+    description="My second module for practice",
+)
