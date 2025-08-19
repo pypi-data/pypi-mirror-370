f61843cbc0bfd0e9810803ca1ddc7decce321973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:44 +0000

    Add package microsoft-agents-hosting-core/0.3.0.dev0/microsoft_agents_hosting_core-0.3.0.dev0-py3-none-any.whl

---
+++ packages/microsoft-agents-hosting-core/microsoft_agents_hosting_core-0.3.0.dev0-py3-none-any.whl/microsoft/agents/hosting/core/app/oauth/__init__.py
@@ -0,0 +1,8 @@
+from .authorization import (
+    Authorization,
+    AuthorizationHandlers,
+    AuthHandler,
+    SignInState,
+)
+
+__all__ = ["Authorization", "AuthorizationHandlers", "AuthHandler", "SignInState"]
