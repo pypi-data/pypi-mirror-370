e95d59cebed0ad117dabe85134f239008bfdadd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:51 +0000

    Add package pydantic-extension/0.0.1/pydantic_extension-0.0.1.tar.gz

---
+++ packages/pydantic-extension/pydantic_extension-0.0.1.tar.gz/pydantic_extension-0.0.1/src/pydantic_extension/model_slicing/__init__.py
@@ -0,0 +1,12 @@
+from .mixin import (ModeSlicingMixin, NotMode, FrontendField, BackendField, LLMField,
+                DtoType,
+                BackendType,
+                FrontendType,
+                LLMType,
+                use_mode)
+
+__all__ = ["ModeSlicingMixin", "NotMode", "FrontendField", "BackendField", "LLMField",
+                "DtoType",
+                "BackendType",
+                "FrontendType",
+                "LLMType", "use_mode"]
