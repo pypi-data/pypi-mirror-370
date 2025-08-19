15f2507ee714312d050ef5bdef098a1c57f873f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 16:06:28 +0000

    Add package xdsl/0.49.0/xdsl-0.49.0.tar.gz

---
+++ packages/xdsl/xdsl-0.49.0.tar.gz/xdsl-0.49.0/tests/filecheck/frontend/dialects/func.py
@@ -0,0 +1,45 @@
+# RUN: python %s | filecheck %s
+
+from xdsl.dialects import bigint
+from xdsl.frontend.pyast.context import PyASTContext
+
+ctx = PyASTContext(post_transforms=[])
+ctx.register_type(int, bigint.bigint)
+
+
+# CHECK:      func.func @f1(%{{.*}} : !bigint.bigint) {
+# CHECK-NEXT:   symref.declare "x"
+# CHECK-NEXT:   symref.update @x = %{{.*}} : !bigint.bigint
+# CHECK-NEXT:   func.return
+# CHECK-NEXT: }
+@ctx.parse_program
+def f1(x: int):
+    return
+
+
+print(f1.module)
+
+
+# CHECK:      func.func @f2() {
+# CHECK-NEXT:   func.return
+# CHECK-NEXT: }
+@ctx.parse_program
+def f2():
+    return
+
+
+print(f2.module)
+
+
+# CHECK:      func.func @f3(%{{.*}} : !bigint.bigint) -> !bigint.bigint {
+# CHECK-NEXT:   symref.declare "x"
+# CHECK-NEXT:   symref.update @x = %{{.*}} : !bigint.bigint
+# CHECK-NEXT:   %{{.*}} = symref.fetch @x : !bigint.bigint
+# CHECK-NEXT:   func.return %{{.*}} : !bigint.bigint
+# CHECK-NEXT: }
+@ctx.parse_program
+def f3(x: int) -> int:
+    return x
+
+
+print(f3.module)
