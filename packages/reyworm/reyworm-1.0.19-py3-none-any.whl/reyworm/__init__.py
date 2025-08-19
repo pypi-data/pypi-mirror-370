b6f80f199544b809794a63d5254d7dad5277cba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 03:22:40 +0000

    Add package reyworm/1.0.19/reyworm-1.0.19-py3-none-any.whl

---
+++ packages/reyworm/reyworm-1.0.19-py3-none-any.whl/reyworm/__init__.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-19 18:59:26
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Worm method set.
+
+Modules
+-------
+rall : All methods.
+rbrowser : Browser methods.
+rcalendar : Calendar methods.
+rnews : News methods.
+rsecurity : Security methods.
+rtranslate : Translate methods.
+"""
