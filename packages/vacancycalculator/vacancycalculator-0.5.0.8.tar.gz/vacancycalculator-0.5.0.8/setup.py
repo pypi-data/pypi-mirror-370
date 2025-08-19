109fd5885159ad57ef2564e5d1fd542aade7a82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 06:05:18 +0000

    Add package vacancycalculator/0.5.0.8/vacancycalculator-0.5.0.8.tar.gz

---
+++ packages/vacancycalculator/vacancycalculator-0.5.0.8.tar.gz/vacancycalculator-0.5.0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vacancycalculator',
+    version='0.5.0.8',
+    author='E.Bringa-S.Bergamin-SiMaF',
+    author_email='santiagobergamin@gmail.com',
+    license='MIT',
+    description='Defect analysis and vacancy calculation for materials science',
+    url='https://github.com/TiagoBe0/VFScript-SiMaF',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+    'numpy',
+    'scipy',
+    'pandas',
+    'scikit-learn',
+    'xgboost',
+    'ase',
+    'ovito',
+    'pyvista',     
+    'pyvistaqt',  
+    'vtk',
+    'tensorflow'         
+],
+
+    entry_points={
+        "console_scripts": [
+            "vacancyfinder = vfscript.vfs_win:main"
+        ]}
+,
+    include_package_data=True,
+)
