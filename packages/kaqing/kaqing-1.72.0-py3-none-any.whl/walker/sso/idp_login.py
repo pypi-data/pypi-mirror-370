fc274c221fa05964f64799cf933bc0b1d531aac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-19 14:05:25 +0000

    Add package kaqing/1.72.0/kaqing-1.72.0-py3-none-any.whl

---
+++ packages/kaqing/kaqing-1.72.0-py3-none-any.whl/walker/sso/idp_login.py
@@ -0,0 +1,23 @@
+import base64
+import json
+import requests
+
+class IdpLogin:
+    def __init__(self, app_login_url: str, id: str, state: str, user: str = None, session: requests.Session = None):
+        self.app_login_url = app_login_url
+        self.id = id
+        self.state = state
+        self.user = user
+        self.session = session
+
+    def deser(idp_token: str):
+        j = json.loads(base64.b64decode(idp_token.encode('utf-8')))
+
+        return IdpLogin(j['r'], j['id'], j['state'])
+
+    def ser(self):
+        return base64.b64encode(json.dumps({
+            'r': self.app_login_url,
+            'id': self.id,
+            'state': self.state
+        }).encode('utf-8')).decode('utf-8')
