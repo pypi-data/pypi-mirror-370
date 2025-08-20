af920c41c7bb8780bcf41f57320c2384164c7d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:21:51 +0000

    Add package uncertainty-engine-types/0.7.0/uncertainty_engine_types-0.7.0.tar.gz

---
+++ packages/uncertainty-engine-types/uncertainty_engine_types-0.7.0.tar.gz/uncertainty_engine_types-0.7.0/uncertainty_engine_types/context.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+from .node_info import NodeInfo
+
+
+class UserContext(BaseModel):
+    email: str
+    project_id: str
+    cost_code: str
+
+
+class Context(BaseModel):
+    sync: bool
+    job_id: str
+    queue_url: str
+    cache_url: str
+    timeout: int
+    nodes: dict[str, NodeInfo]
+    user: UserContext
