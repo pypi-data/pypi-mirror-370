6f76192d3690bfed8aa3136299500889d7d2def6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:02 +0000

    Add package universal-mcp/0.1.24rc4/universal_mcp-0.1.24rc4.tar.gz

---
+++ packages/universal-mcp/universal_mcp-0.1.24rc4.tar.gz/universal_mcp-0.1.24rc4/src/universal_mcp/agents/__init__.py
@@ -0,0 +1,6 @@
+from .auto import AutoAgent
+from .base import BaseAgent
+from .react import ReactAgent
+from .simple import SimpleAgent
+
+__all__ = ["BaseAgent", "ReactAgent", "SimpleAgent", "AutoAgent"]
