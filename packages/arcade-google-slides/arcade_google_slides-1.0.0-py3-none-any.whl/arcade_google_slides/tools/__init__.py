dd9a7e6900f809192cac94f46fb9cd8763e27b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 05:05:30 +0000

    Add package arcade-google-slides/1.0.0/arcade_google_slides-1.0.0-py3-none-any.whl

---
+++ packages/arcade-google-slides/arcade_google_slides-1.0.0-py3-none-any.whl/arcade_google_slides/tools/__init__.py
@@ -0,0 +1,20 @@
+from arcade_google_slides.tools.comment import (
+    comment_on_presentation,
+    list_presentation_comments,
+)
+from arcade_google_slides.tools.create import create_presentation, create_slide
+from arcade_google_slides.tools.file_picker import generate_google_file_picker_url
+from arcade_google_slides.tools.get import get_presentation_as_markdown
+from arcade_google_slides.tools.search import (
+    search_presentations,
+)
+
+__all__ = [
+    "create_presentation",
+    "create_slide",
+    "get_presentation_as_markdown",
+    "search_presentations",
+    "comment_on_presentation",
+    "list_presentation_comments",
+    "generate_google_file_picker_url",
+]
