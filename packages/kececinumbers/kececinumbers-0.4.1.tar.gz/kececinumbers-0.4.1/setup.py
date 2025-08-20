3d1e6cab6794c92590a6d844e292023213761afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 07:05:19 +0000

    Add package kececinumbers/0.4.1/kececinumbers-0.4.1.tar.gz

---
+++ packages/kececinumbers/kececinumbers-0.4.1.tar.gz/kececinumbers-0.4.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="kececinumbers",
+    version="0.4.1",
+    description="Keçeci Numbers: An Exploration of a Dynamic Sequence Across Diverse Number Sets",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Mehmet Keçeci",
+    maintainer="Mehmet Keçeci",
+    author_email="mkececi@yaani.com",
+    maintainer_email="mkececi@yaani.com",
+    url="https://github.com/WhiteSymmetry/kececinumbers",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "numpy-quaternion",
+        "sympy",
+    ],
+    extras_require={
+
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.9',
+    license="MIT",
+)
