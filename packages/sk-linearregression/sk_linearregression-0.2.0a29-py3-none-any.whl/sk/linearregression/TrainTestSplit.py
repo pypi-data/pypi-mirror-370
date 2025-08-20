f6b93084399c58ce4ff9b3032598578bd837597d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 05:05:39 +0000

    Add package sk-linearregression/0.2.0a29/sk_linearregression-0.2.0a29-py3-none-any.whl

---
+++ packages/sk-linearregression/sk_linearregression-0.2.0a29-py3-none-any.whl/sk/linearregression/TrainTestSplit.py
@@ -0,0 +1,6 @@
+def TrainTestSplit(X, Y, test_size=0.2):
+    X_train = X[:int(len(X) * test_size)]
+    X_test = X[int(len(X) * test_size):]
+    Y_train = Y[:int(len(Y) * test_size)]
+    Y_test = Y[int(len(Y) * test_size):]
+    return X_train, X_test, Y_train, Y_test
