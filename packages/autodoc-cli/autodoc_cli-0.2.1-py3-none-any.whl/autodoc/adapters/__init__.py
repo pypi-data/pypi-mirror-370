152ba3cb5bdf694758ae17d5ab42bb0af3650bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:06:18 +0000

    Add package autodoc-cli/0.2.1/autodoc_cli-0.2.1-py3-none-any.whl

---
+++ packages/autodoc-cli/autodoc_cli-0.2.1-py3-none-any.whl/autodoc/adapters/__init__.py
@@ -0,0 +1,10 @@
+from .base import FunctionInfo, LanguageAdapter
+from .c_adapter import CAdapter
+from .python_adapter import PythonAdapter
+
+__all__ = [
+    "FunctionInfo",
+    "LanguageAdapter",
+    "CAdapter",
+    "PythonAdapter",
+]
