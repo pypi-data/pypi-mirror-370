e168e50c5df260dc82703896521611a2664d65f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:05:58 +0000

    Add package spei-python/1.0.6/spei_python-1.0.6-py3-none-any.whl

---
+++ packages/spei-python/spei_python-1.0.6-py3-none-any.whl/spei/responses/__init__.py
@@ -0,0 +1,9 @@
+from spei.responses.acuse import AcuseResponse
+from spei.responses.orden import OrdenResponse
+from spei.responses.respuesta import RespuestaResponse
+
+__all__ = [  # noqa: WPS410
+    'AcuseResponse',
+    'OrdenResponse',
+    'RespuestaResponse',
+]
