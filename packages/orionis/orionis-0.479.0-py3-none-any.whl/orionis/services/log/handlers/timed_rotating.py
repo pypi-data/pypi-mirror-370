d71987b4e8dfe72a21540aafa3fa9154dda331c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:51 +0000

    Add package orionis/0.479.0/orionis-0.479.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.479.0-py3-none-any.whl/orionis/services/log/handlers/timed_rotating.py
@@ -0,0 +1,29 @@
+from logging.handlers import TimedRotatingFileHandler
+from orionis.services.log.handlers.filename import FileNameLogger
+
+class PrefixedTimedRotatingFileHandler(TimedRotatingFileHandler):
+
+    def rotation_filename(self, default_name) -> str:
+        """
+        Generates a rotated log filename by prefixing the original filename with a timestamp.
+
+        Parameters
+        ----------
+        default_name : str
+            The original file path that is subject to rotation.
+
+        Returns
+        -------
+        str
+            The new file path as a string, where the base name is prefixed with a timestamp
+            in the format 'YYYYMMDD_HHMMSS'. This ensures each rotated log file is uniquely
+            identified by its creation time.
+
+        Notes
+        -----
+        This method utilizes the FileNameLogger class to construct the new filename.
+        The timestamp prefix helps in organizing and distinguishing rotated log files.
+        """
+
+        # Use FileNameLogger to generate a new filename with a timestamp prefix
+        return FileNameLogger(default_name).generate()
