21962a55c9787913bb167a5a8ce5f95d01de0540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:28 +0000

    Add package services-pulp-file-client/20250819.1/services_pulp_file_client-20250819.1.tar.gz

---
+++ packages/services-pulp-file-client/services_pulp_file_client-20250819.1.tar.gz/services_pulp_file_client-20250819.1/services-pulp_file-client/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from services-pulp_file-client.api.acs_file_api import AcsFileApi
+from services-pulp_file-client.api.content_files_api import ContentFilesApi
+from services-pulp_file-client.api.distributions_file_api import DistributionsFileApi
+from services-pulp_file-client.api.publications_file_api import PublicationsFileApi
+from services-pulp_file-client.api.remotes_file_api import RemotesFileApi
+from services-pulp_file-client.api.repositories_file_api import RepositoriesFileApi
+from services-pulp_file-client.api.repositories_file_versions_api import RepositoriesFileVersionsApi
+
