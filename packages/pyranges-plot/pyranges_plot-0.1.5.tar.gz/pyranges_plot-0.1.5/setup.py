07514888b64914551b903b5d34b2686899855ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:06:11 +0000

    Add package pyranges-plot/0.1.5/pyranges_plot-0.1.5.tar.gz

---
+++ packages/pyranges-plot/pyranges_plot-0.1.5.tar.gz/pyranges_plot-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pyranges_plot",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,  # Ensure package data is included
+    package_data={
+        "pyranges_plot": ["data/*"],  # Specify the path to include data folder contents
+    },
+)
