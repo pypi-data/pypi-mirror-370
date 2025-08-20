87d23f4a6931a02bc40af1aed751466ee653abdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:22:02 +0000

    Add package glider-client/2024.4.1/glider_client-2024.4.1.tar.gz

---
+++ packages/glider-client/glider_client-2024.4.1.tar.gz/glider_client-2024.4.1/glider_client/utils/glider.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created by chiesa
+
+Copyright Alpes Lasers SA, Switzerland
+"""
+__author__ = 'chiesa'
+__copyright__ = "Copyright Alpes Lasers SA"
+
+import requests
+
+
+def get_glider_status(hostname):
+    rsp = requests.get('http://{}:5000/api/status'.format(hostname))
+    rsp.raise_for_status()
+    return rsp.json()
+
+
+def get_glider_configuration(hostname):
+    status = get_glider_status(hostname)
+    return status['config']
