9b89f801e18272f4e1fab636ff2ff77d00c4e9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:39 +0000

    Add package fabrial/0.2.0/fabrial-0.2.0-py3-none-any.whl

---
+++ packages/fabrial/fabrial-0.2.0-py3-none-any.whl/fabrial/custom_widgets/augmented/__init__.py
@@ -0,0 +1,16 @@
+from .button import BiggerButton, Button, FixedButton
+from .combo_box import ComboBox
+from .dialog import (
+    Dialog,
+    DontShowAgainDialog,
+    OkCancelDialog,
+    OkDialog,
+    YesCancelDialog,
+    YesNoDialog,
+)
+from .groupbox import GroupBox
+from .label import IconLabel, Label
+from .progressbar import ProgressBar
+from .spin_box import DoubleSpinBox, SpinBox
+from .tab_widget import TabWidget
+from .widget import FixedWidget, SignalWidget, Widget
