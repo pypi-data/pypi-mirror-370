2850572b5c5001c8bcbf5811bb74888691f26b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:21:44 +0000

    Add package facade-message-bot-shared-models/0.12.0/facade_message_bot_shared_models-0.12.0.tar.gz

---
+++ packages/facade-message-bot-shared-models/facade_message_bot_shared_models-0.12.0.tar.gz/facade_message_bot_shared_models-0.12.0/src/shared_models/messaging/queues/manual_moderator.py
@@ -0,0 +1,47 @@
+from faststream.rabbit import RabbitQueue, QueueType
+from faststream.rabbit.schemas.queue import QuorumQueueArgs
+
+
+manual_moderator_queue = RabbitQueue(
+    "moderator.manual",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+    arguments=QuorumQueueArgs(
+        {
+            "x-dead-letter-exchange": "dlx",
+            "x-dead-letter-routing-key": "dlx.moderator.manual",
+            "x-dead-letter-strategy": "at-least-once",
+            "x-overflow": "reject-publish",
+        }
+    ),
+)
+
+manual_moderator_dlx_queue = RabbitQueue(
+    "dlx.moderator.manual",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+)
+
+second_manual_moderator_queue = RabbitQueue(
+    "moderator.manual.second",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+    arguments=QuorumQueueArgs(
+        {
+            "x-dead-letter-exchange": "dlx",
+            "x-dead-letter-routing-key": "dlx.moderator.manual.second",
+            "x-dead-letter-strategy": "at-least-once",
+            "x-overflow": "reject-publish",
+        }
+    ),
+)
+
+second_manual_moderator_dlx_queue = RabbitQueue(
+    "dlx.moderator.manual.second",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+)
