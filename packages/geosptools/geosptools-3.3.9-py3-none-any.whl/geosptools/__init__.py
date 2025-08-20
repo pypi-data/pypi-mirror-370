3bb1f52afaf5a893eb0208ba8227a7bd2f49c481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:22:20 +0000

    Add package geosptools/3.3.9/geosptools-3.3.9-py3-none-any.whl

---
+++ packages/geosptools/geosptools-3.3.9-py3-none-any.whl/geosptools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# geosptools/__init__.py
+
+__version__ = "3.3.9"
+
+# Define what should be available when using 'from geosptools import *'
+__all__ = [
+    'raster_tools'
+]
