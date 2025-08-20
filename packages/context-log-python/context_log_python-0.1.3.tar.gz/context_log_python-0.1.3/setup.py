ce9247a94381716c7c85001be25bccc050ae3bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:09 +0000

    Add package context-log-python/0.1.3/context_log_python-0.1.3.tar.gz

---
+++ packages/context-log-python/context_log_python-0.1.3.tar.gz/context_log_python-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='context-log-python',
+    version='0.1.3',
+    packages=find_packages(),
+    description='A contextual logging framework for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ramakrishna Bapathu',
+    author_email='ramkrishna9493@gmail.com',
+    url='https://github.com/Ramakrishna9493/context_logger',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+)
