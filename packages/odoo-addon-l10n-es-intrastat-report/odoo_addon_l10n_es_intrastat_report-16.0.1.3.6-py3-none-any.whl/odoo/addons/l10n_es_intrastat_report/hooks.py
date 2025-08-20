3eddf9a6a425e6474a7e9015bd37700b8bca8826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:06:12 +0000

    Add package odoo-addon-l10n-es-intrastat-report/16.0.1.3.6/odoo_addon_l10n_es_intrastat_report-16.0.1.3.6-py3-none-any.whl

---
+++ packages/odoo-addon-l10n-es-intrastat-report/odoo_addon_l10n_es_intrastat_report-16.0.1.3.6-py3-none-any.whl/odoo/addons/l10n_es_intrastat_report/hooks.py
@@ -0,0 +1,32 @@
+# Copyright 2020 ACSONE SA/NV
+# Copyright 2025 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import SUPERUSER_ID, api
+
+
+def post_init_hook(cr, registry):
+    """Set the intrastat field of the private fiscal position records.
+    This is necessary for the intrastat report generation
+    """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    b2b_items = env["ir.model.data"].search(
+        [
+            ("model", "=", "account.fiscal.position"),
+            ("name", "like", "%_fp_intra"),
+            ("module", "=", "l10n_es"),
+        ]
+    )
+    b2c_items = env["ir.model.data"].search(
+        [
+            ("model", "=", "account.fiscal.position"),
+            ("name", "like", "%_fp_intra_private"),
+            ("module", "=", "l10n_es"),
+        ]
+    )
+    env["account.fiscal.position"].browse(b2b_items.mapped("res_id")).write(
+        {"intrastat": "b2b", "vat_required": True}
+    )
+    env["account.fiscal.position"].browse(b2c_items.mapped("res_id")).write(
+        {"intrastat": "b2c", "vat_required": False}
+    )
