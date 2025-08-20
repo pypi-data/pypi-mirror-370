e3dd1711261ddf5a0000d986d1055826d968d9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 17:05:50 +0000

    Add package adaup/0.1.1/adaup-0.1.1.tar.gz

---
+++ packages/adaup/adaup-0.1.1.tar.gz/adaup-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adaup',
+    version='0.1.1',
+    packages=['adaup'],
+    package_dir={'': 'src'},
+    install_requires=[
+        'tqdm',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cardano=adaup:main',
+        ],
+    },
+    author='Sudip Bhattarai',
+    author_email='sudip@bhattarai.me',
+    description='A Python package for interacting with Cardano.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dquadrant/kuber',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
