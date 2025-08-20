e3ae754d894cb631f0b120ce5d63a1e9624217ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:28 +0000

    Add package services-pulp-gem-client/20250819.1/services_pulp_gem_client-20250819.1.tar.gz

---
+++ packages/services-pulp-gem-client/services_pulp_gem_client-20250819.1.tar.gz/services_pulp_gem_client-20250819.1/services-pulp_gem-client/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from services-pulp_gem-client.api.content_gem_api import ContentGemApi
+from services-pulp_gem-client.api.distributions_gem_api import DistributionsGemApi
+from services-pulp_gem-client.api.publications_gem_api import PublicationsGemApi
+from services-pulp_gem-client.api.remotes_gem_api import RemotesGemApi
+from services-pulp_gem-client.api.repositories_gem_api import RepositoriesGemApi
+from services-pulp_gem-client.api.repositories_gem_versions_api import RepositoriesGemVersionsApi
+
