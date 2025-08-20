71d9b193d694f703829d435680cf250ab6b486b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:06:22 +0000

    Add package langchain-fastapi-chat-completion/1.0.2/langchain_fastapi_chat_completion-1.0.2-py3-none-any.whl

---
+++ packages/langchain-fastapi-chat-completion/langchain_fastapi_chat_completion-1.0.2-py3-none-any.whl/langchain_fastapi_chat_completion/chat_completion/chat_completion_chunk_choice_adapter.py
@@ -0,0 +1,68 @@
+from typing import Optional
+
+from langchain_core.runnables.schema import StreamEvent
+from openai.types.chat.chat_completion_chunk import (
+    ChatCompletionChunk,
+    Choice,
+    ChoiceDelta,
+    ChoiceDeltaFunctionCall,
+)
+
+from langchain_fastapi_chat_completion.chat_completion.chat_completion_chunk_object_factory import (
+    create_chat_completion_chunk_object,
+)
+
+
+def to_openai_chat_message(
+    event: StreamEvent,
+    role: str = "assistant",
+) -> ChoiceDelta:
+    if getattr(event["data"]["chunk"], "tool_call_chunks", None):
+        function_call = ChoiceDeltaFunctionCall(
+            name=event["data"]["chunk"].tool_call_chunks[0]["name"],
+            arguments=event["data"]["chunk"].tool_call_chunks[0]["args"],
+        )
+    else:
+        function_call = None
+
+    return ChoiceDelta(
+        content=event["data"]["chunk"].content,
+        role=role,
+        function_call=function_call,
+    )
+
+
+def to_openai_chat_completion_chunk_choice(
+    event: StreamEvent,
+    index: int = 0,
+    role: Optional[str] = None,
+    finish_reason: Optional[str] = None,
+) -> Choice:
+    message = to_openai_chat_message(event, role)
+
+    return Choice(
+        index=index,
+        delta=message,
+        finish_reason=finish_reason,
+    )
+
+
+def to_openai_chat_completion_chunk_object(
+    event: StreamEvent,
+    id: str = "",
+    model: str = "",
+    system_fingerprint: Optional[str] = None,
+    role: Optional[str] = None,
+    finish_reason: Optional[str] = None,
+) -> ChatCompletionChunk:
+
+    choice1 = to_openai_chat_completion_chunk_choice(
+        event, index=0, role=role, finish_reason=finish_reason
+    )
+
+    return create_chat_completion_chunk_object(
+        id=id,
+        model=model,
+        system_fingerprint=system_fingerprint,
+        choices=[choice1],
+    )
