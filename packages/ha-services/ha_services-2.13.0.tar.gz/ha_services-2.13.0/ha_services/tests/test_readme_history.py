0bff7438dcba35472dc9e024502c6a7879012a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:53 +0000

    Add package ha-services/2.13.0/ha_services-2.13.0.tar.gz

---
+++ packages/ha-services/ha_services-2.13.0.tar.gz/ha_services-2.13.0/ha_services/tests/test_readme_history.py
@@ -0,0 +1,17 @@
+import logging
+from unittest import TestCase, skipIf
+
+from cli_base.cli_tools.constants import GITHUB_ACTION
+from cli_base.cli_tools.git_history import update_readme_history
+
+
+class ReadmeHistoryTestCase(TestCase):
+    @skipIf(
+        # After a release the history may be "changed" because of version bump.
+        # We should not block merge requests because of this.
+        GITHUB_ACTION,
+        reason='Skip on github actions',
+    )
+    def test_readme_history(self):
+        with self.assertNoLogs(level=logging.WARNING):
+            update_readme_history(raise_update_error=True)
