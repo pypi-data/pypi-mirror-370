0e0aa14b64340985c62b9dbe2df2e83e17a57047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:22:20 +0000

    Add package LinguaLab/3.5.10/lingualab-3.5.10-py3-none-any.whl

---
+++ packages/LinguaLab/lingualab-3.5.10-py3-none-any.whl/LinguaLab/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# LinguaLab/__init__.py
+
+__version__ = "3.5.10"
+
+# Define what should be available when using 'from LinguaLab import *'
+__all__ = [
+    'text_translations',
+    'transcribe_video_files'
+]
