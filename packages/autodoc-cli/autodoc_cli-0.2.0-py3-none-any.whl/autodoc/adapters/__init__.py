c85ba4ab542165731c56bdfbeea4b587032abb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:05:49 +0000

    Add package autodoc-cli/0.2.0/autodoc_cli-0.2.0-py3-none-any.whl

---
+++ packages/autodoc-cli/autodoc_cli-0.2.0-py3-none-any.whl/autodoc/adapters/__init__.py
@@ -0,0 +1,8 @@
+from .base import FunctionInfo, LanguageAdapter
+from .c_adapter import CAdapter
+
+__all__ = [
+    "FunctionInfo",
+    "LanguageAdapter",
+    "CAdapter",
+]
---
+++ packages/autodoc-cli/autodoc_cli-0.2.0-py3-none-any.whl/autodoc/editing/__init__.py
@@ -0,0 +1,3 @@
+from .editor import PlanEdit, Editor
+
+__all__ = ["PlanEdit", "Editor"]
