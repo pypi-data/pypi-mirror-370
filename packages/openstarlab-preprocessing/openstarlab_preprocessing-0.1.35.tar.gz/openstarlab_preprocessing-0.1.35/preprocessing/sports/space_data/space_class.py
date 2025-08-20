dd516d1f4da029d883ebe1b33e4dc0aea01ce530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:06:36 +0000

    Add package openstarlab-preprocessing/0.1.35/openstarlab_preprocessing-0.1.35.tar.gz

---
+++ packages/openstarlab-preprocessing/openstarlab_preprocessing-0.1.35.tar.gz/openstarlab_preprocessing-0.1.35/preprocessing/sports/space_data/space_class.py
@@ -0,0 +1,13 @@
+
+class Space_data:
+    # Modified the sports list to only include fully supported providers
+    sports = ['SportVU_NBA']
+
+    def __new__(cls, data_provider, *args, **kwargs):
+        if data_provider in cls.sports:
+            from .basketball.basketball_space_class import Basketball_space_data
+            # If the data_provider is in the supported list, return an instance of Soccer_SAR_data
+            return Basketball_space_data(data_provider, *args, **kwargs)
+        else:
+            # If the data_provider is unrecognized, raise a ValueError
+            raise ValueError(f'Unknown data provider: {data_provider}')
