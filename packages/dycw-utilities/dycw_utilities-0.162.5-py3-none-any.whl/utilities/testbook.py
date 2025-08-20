e790fe2d1188c07d50041b4c9c8f371dfe0ce8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:05:29 +0000

    Add package dycw-utilities/0.162.5/dycw_utilities-0.162.5-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.162.5-py3-none-any.whl/utilities/testbook.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+from testbook import testbook
+
+from utilities.pytest import throttle
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from utilities.types import Delta, PathLike
+
+
+def build_notebook_tester(
+    path: PathLike, /, *, throttle: Delta | None = None, on_try: bool = False
+) -> object:
+    """Build the notebook test class."""
+    name = f"Test{Path(path).stem}"
+    notebooks = [
+        path_i
+        for path_i in Path(path).rglob("**/*.ipynb")
+        if all(p != ".ipynb_checkpoints" for p in path_i.parts)
+    ]
+    namespace = {
+        f"test_{p.stem}": _build_test_method(p, delta=throttle, on_try=on_try)
+        for p in notebooks
+    }
+    return type(name, (), namespace)
+
+
+def _build_test_method(
+    path: Path, /, *, delta: Delta | None = None, on_try: bool = False
+) -> Callable[..., Any]:
+    def method(self: Any, tb: Any) -> None:
+        _ = (self, tb)  # pragma: no cover
+
+    if delta is not None:
+        method = throttle(delta=delta, on_try=on_try)(method)
+
+    tbook = testbook(path, execute=True)
+    return tbook(method)
+
+
+__all__ = ["build_notebook_tester"]
