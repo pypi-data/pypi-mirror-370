45127c49dd7389de8cf4511a0f110697ee86dc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:05:49 +0000

    Add package terra_ui_components/0.0.63/terra_ui_components-0.0.63.tar.gz

---
+++ packages/terra_ui_components/terra_ui_components-0.0.63.tar.gz/terra_ui_components-0.0.63/src/components/time-average-map/time-average-map.styles.ts
@@ -0,0 +1,13 @@
+import { css } from 'lit'
+
+export default css`
+    :host {
+        display: block;
+        height: 600px;
+    }
+
+    #map {
+        width: 100%;
+        height: 100%;
+    }
+`
