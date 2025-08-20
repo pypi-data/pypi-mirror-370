fe9df0e6d64dc1894eb3566e051bc1b9e0597dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:22:01 +0000

    Add package glider-client/2024.2.1/glider_client-2024.2.1.tar.gz

---
+++ packages/glider-client/glider_client-2024.2.1.tar.gz/glider_client-2024.2.1/glider_client/utils/mcu_registers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created by chiesa
+
+Copyright Alpes Lasers SA, Switzerland
+"""
+__author__ = 'chiesa'
+__copyright__ = "Copyright Alpes Lasers SA"
+
+
+ADC_SAMPLING_TIMES = [9, 16, 53, 103, 222, 403, 2422, 5066]
