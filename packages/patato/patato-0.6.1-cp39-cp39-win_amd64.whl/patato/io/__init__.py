dda1cb1e4ba50943e6e3fa6c96316c00f9a1168e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:21:59 +0000

    Add package patato/0.6.1/patato-0.6.1-cp39-cp39-win_amd64.whl

---
+++ packages/patato/patato-0.6.1-cp39-cp39-win_amd64.whl/patato/io/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) Thomas Else 2023-25.
+#  License: MIT
+
+"""
+Input/output module
+==========================
+
+I/O module for patato. Provides interfaces to HDF5 format, simpa and iThera.
+"""
