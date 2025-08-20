ab9ad0487ada4a452234b309e0626f6613503e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:06:10 +0000

    Add package pyfusion/2.0.17/pyfusion-2.0.17-cp311-cp311-manylinux_2_17_ppc64.manylinux2014_ppc64.whl

---
+++ packages/pyfusion/pyfusion-2.0.17-cp311-cp311-manylinux_2_17_ppc64.manylinux2014_ppc64.whl/fusion/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for fusion."""
+
+__author__ = """Fusion Devs"""
+__email__ = "fusion_developers@jpmorgan.com"
+__version__ = "2.0.17"
+
+from fusion._fusion import FusionCredentials
+from fusion.fs_sync import fsync
+from fusion.fusion import Fusion
+
+from ._fusion import *  # noqa: F403
+
+__all__ = ["Fusion", "FusionCredentials", "fsync", "rust_ok"]  # noqa: F405
