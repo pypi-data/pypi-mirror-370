76a80818292420164788212432e5c780c0a7a136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 05:05:24 +0000

    Add package sagea/0.2.3a3/sagea-0.2.3a3.tar.gz

---
+++ packages/sagea/sagea-0.2.3a3.tar.gz/sagea-0.2.3a3/sagea/__init__.py
@@ -0,0 +1,47 @@
+#!/use/bin/env python
+# coding=utf-8
+# @Author  : Shuhao Liu
+# @Time    : 2025/6/13 10:28 
+# @File    : __init__.py.py
+
+from ._version import __version__
+import os
+
+
+def check_version():
+    os.system("pip index versions sagea --pre")
+
+
+# from sagea.pysrc.data_class.__SHC__ import SHC
+# from sagea.pysrc.data_class.__GRD__ import GRD
+#
+# from sagea.pysrc.error_assessment.tch.TCH import tch
+#
+# from sagea.pysrc.load_file.LoadL2SH import load_SHC
+# from sagea.pysrc.load_file.LoadCov import load_CovMatrix as load_SHCov
+# from sagea.pysrc.load_file.LoadL2LowDeg import load_low_degs as load_SHLowDegs
+# from sagea.pysrc.load_file.LoadSHP import load_shp
+#
+# from sagea.pysrc.auxiliary.MathTool import MathTool
+# from sagea.pysrc.auxiliary.TimeTool import TimeTool
+# from sagea.pysrc.auxiliary.FileTool import FileTool
+# import sagea.pysrc.auxiliary.Preference as Preference
+#
+# from sagea.pysrc.data_collection.collect_auxiliary import collect_auxiliary as collect_auxiliary_data
+
+from .pysrc.data_class.__SHC__ import SHC
+from .pysrc.data_class.__GRD__ import GRD
+
+from .pysrc.error_assessment.tch.TCH import tch
+
+from .pysrc.load_file.LoadL2SH import load_SHC
+from .pysrc.load_file.LoadCov import load_CovMatrix as load_SHCov
+from .pysrc.load_file.LoadL2LowDeg import load_low_degs as load_SHLowDegs
+from .pysrc.load_file.LoadSHP import load_shp
+
+from .pysrc.auxiliary.MathTool import MathTool
+from .pysrc.auxiliary.TimeTool import TimeTool
+from .pysrc.auxiliary.FileTool import FileTool
+from .pysrc.auxiliary import Preference
+
+from .pysrc.data_collection.collect_auxiliary import collect_auxiliary as collect_auxiliary_data
