f07a6bfc57ec93923f3285d54ed440b52283ed21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:01 +0000

    Add package ndice/0.1.7/ndice-0.1.7.tar.gz

---
+++ packages/ndice/ndice-0.1.7.tar.gz/ndice-0.1.7/src/ndice/op.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from functools import total_ordering
+from typing import Self, Callable
+
+
+type BinaryFunction = Callable[[int, int], int]
+
+
+@total_ordering
+class Op(Enum):
+    PLUS = '+', int.__add__
+    MINUS = '-', int.__sub__
+    TIMES = 'x', int.__mul__
+
+    def __init__(self, symbol: str, f: BinaryFunction):
+        self.symbol = symbol
+        self.f = f
+
+    def __repr__(self) -> str:
+        return f'<{self.__class__.__name__}.{self.name}>'
+
+    def __str__(self) -> str:
+        return self.symbol
+
+    def __lt__(self, other: Self) -> bool:
+        assert isinstance(other, Op)
+        return self.symbol < other.symbol
+
+    def __call__(self, a: int, b: int) -> int:
+        return self.f(a, b)
