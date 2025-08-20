eaeae225c4e760d6dda6e306ed005ce90a61d1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:05:59 +0000

    Add package HaplotagLR/1.1.13/HaplotagLR-1.1.13-py3-none-any.whl

---
+++ packages/HaplotagLR/HaplotagLR-1.1.13-py3-none-any.whl/LRphase/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '1.1.3'
+try:
+    from ._LRphase import longest  # noqa
+except ImportError:
+    def longest(args):
+        """
+        Args:
+            args:
+        """
+        return max(args, key=len)
