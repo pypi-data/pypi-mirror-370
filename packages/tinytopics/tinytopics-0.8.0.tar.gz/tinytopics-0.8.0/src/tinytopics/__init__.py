3830324ef62c74e496b75c46749e47919cf97f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 07:05:55 +0000

    Add package tinytopics/0.8.0/tinytopics-0.8.0.tar.gz

---
+++ packages/tinytopics/tinytopics-0.8.0.tar.gz/tinytopics-0.8.0/src/tinytopics/__init__.py
@@ -0,0 +1,17 @@
+"""
+Topic modeling via sum-to-one constrained neural Poisson NMF.
+"""
+
+from .colors import pal_tinytopics, scale_color_tinytopics
+from .data import NumpyDiskDataset, TorchDiskDataset
+from .fit import fit_model
+from .fit_distributed import fit_model_distributed
+from .loss import poisson_nmf_loss
+from .models import NeuralPoissonNMF
+from .plot import plot_loss, plot_structure, plot_top_terms
+from .utils import (
+    align_topics,
+    generate_synthetic_data,
+    set_random_seed,
+    sort_documents,
+)
