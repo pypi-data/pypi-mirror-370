af316bb87442fe34b7f104a2d8dae81741703db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 07:05:30 +0000

    Add package ap-cfg/1.1.0.20250819/ap_cfg-1.1.0.20250819.tar.gz

---
+++ packages/ap-cfg/ap_cfg-1.1.0.20250819.tar.gz/ap_cfg-1.1.0.20250819/src/ap_cfg/task.py
@@ -0,0 +1,47 @@
+"""
+This module provides task scheduling classes for the management of OmniTracker
+SRR (NHRR) processing for Department UMH.
+    SRR: Sustainability Risk Rating
+    NHRR: Nachhaltigkeits Risiko Rating
+"""
+from collections.abc import Callable
+
+import ut_dic.doc.Doc as DoC
+import ap_cfg.setup.Setup as Setup
+
+from typing import Any
+TyArr = list[Any]
+TyAoStr = list[str]
+TyBool = bool
+TyCallable = Callable[..., Any]
+TyDic = dict[Any, Any]
+TyAoA = list[TyArr]
+TyAoD = list[TyDic]
+TyCmd = str
+TyDoAoD = dict[Any, TyAoD]
+TyStr = str
+TyTup = tuple[Any]
+TyTask = Any
+TyDoC = dict[str, TyCallable]
+
+TnDic = None | TyDic
+
+
+class Task:
+    """
+    General Task class
+    """
+    """
+    Dictionary of callables of class Setup or Email
+    """
+    doc: TyDoC = {
+        'setup': Setup.setup,
+    }
+
+    @classmethod
+    def do(cls, kwargs: TyDic) -> None:
+        """
+        Select the task method from the task command table for the given
+        command (value of 'cmd' in kwargs) and execute the selected method.
+        """
+        DoC.ex_cmd(cls.doc, kwargs)
