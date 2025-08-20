cfa41c872c2348a6f5b18f865eac084ff65e578b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:05:33 +0000

    Add package synalinks/0.3.28/synalinks-0.3.28.tar.gz

---
+++ packages/synalinks/synalinks-0.3.28.tar.gz/synalinks-0.3.28/synalinks/src/datasets/arcagi/dsl/constants.py
@@ -0,0 +1,54 @@
+# MIT License
+
+# Copyright (c) 2023 Michael Hodel
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+ZERO = 0
+ONE = 1
+TWO = 2
+THREE = 3
+FOUR = 4
+FIVE = 5
+SIX = 6
+SEVEN = 7
+EIGHT = 8
+NINE = 9
+TEN = 10
+F = False
+T = True
+
+NEG_ONE = -1
+
+ORIGIN = (0, 0)
+UNITY = (1, 1)
+DOWN = (1, 0)
+RIGHT = (0, 1)
+UP = (-1, 0)
+LEFT = (0, -1)
+
+NEG_TWO = -2
+NEG_UNITY = (-1, -1)
+UP_RIGHT = (-1, 1)
+DOWN_LEFT = (1, -1)
+
+ZERO_BY_TWO = (0, 2)
+TWO_BY_ZERO = (2, 0)
+TWO_BY_TWO = (2, 2)
+THREE_BY_THREE = (3, 3)
