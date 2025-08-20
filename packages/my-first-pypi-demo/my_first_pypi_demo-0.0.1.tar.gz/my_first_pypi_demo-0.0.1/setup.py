2aed9f499f01c15d07797f8e10ccfd9d86b579d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 17:05:41 +0000

    Add package my-first-pypi-demo/0.0.1/my_first_pypi_demo-0.0.1.tar.gz

---
+++ packages/my-first-pypi-demo/my_first_pypi_demo-0.0.1.tar.gz/my_first_pypi_demo-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-first-pypi-demo',
+    version='0.0.1',
+    author='Your Name',
+    author_email='you@example.com',
+    description='A demo package to learn PyPI publishing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my-first-pypi-demo',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
