c9cdccbdd36701cda9e693d241bc7287bef23382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:06:15 +0000

    Add package key-stroke/1.3.1/key_stroke-1.3.1-py3-none-any.whl

---
+++ packages/key-stroke/key_stroke-1.3.1-py3-none-any.whl/key_stroke/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+key_stroke - The python loop key pressed interupt module
+
+key_stroke is a small module to quit/terminate a loop of a console app
+by simply pressing a key. The beauty is that this module is running on Linux,
+Öpfel-Rechnern as well Fenster operated computers.
+
+Have fun while using the key_stroke module!
+Thomas
+"""
+
+__author__ = "Thomas"
+__copyright__ = "Copyright 2023, Chriesibaum GmbH"
+__version__ = "1.3.1"
+
+from .key_stroke import KeyStroke     # noqa: F401
