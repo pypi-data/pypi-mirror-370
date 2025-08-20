69d22e3310a4897b73b848e716fb637c0022ad84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:39 +0000

    Add package fabrial/0.2.0/fabrial-0.2.0.tar.gz

---
+++ packages/fabrial/fabrial-0.2.0.tar.gz/fabrial-0.2.0/fabrial/constants/__init__.py
@@ -0,0 +1,6 @@
+"""Constants for the application."""
+
+from . import paths, tree_item
+from .entry_points import PLUGIN_ENTRY_POINT
+from .name import APP_NAME, PACKAGE_NAME
+from .paths import icons
