4f2dae3d8a30b6bd39e87eb1da1826b3a2001c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:10 +0000

    Add package abstract-gui/0.1.63.62/abstract_gui-0.1.63.62-py3-none-any.whl

---
+++ packages/abstract-gui/abstract_gui-0.1.63.62-py3-none-any.whl/abstract_gui/QT6/managers/windowManagerConsole/imports/imports.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from abstract_gui.QT6.imports import (
+    os, re, subprocess, sys, time, List, Tuple, Qt, QTimer,QColor,
+    QBrush, QApplication, QComboBox, QFileDialog, QHBoxLayout,
+    QLineEdit, QMainWindow, QMessageBox, QPushButton, QStatusBar,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget,
+    QAbstractItemView
+)
