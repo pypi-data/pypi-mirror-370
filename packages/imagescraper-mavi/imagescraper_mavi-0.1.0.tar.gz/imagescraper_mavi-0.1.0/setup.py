25a9822fe918874a2ffd49a2e7e1e3a177b28c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:05:47 +0000

    Add package imagescraper-mavi/0.1.0/imagescraper_mavi-0.1.0.tar.gz

---
+++ packages/imagescraper-mavi/imagescraper_mavi-0.1.0.tar.gz/imagescraper_mavi-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="imagescraper-mavi",
+    version="0.1.0",
+    author="MaviMods",
+    author_email="your-email@example.com",
+    description="A simple Python package to scrape images from Google, Bing, and Yahoo",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaviMods/imagescraper",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pillow"
+    ],
+    entry_points={
+        "console_scripts": [
+            "imagescraper=imagescraper.scraper:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+    ],
+    python_requires=">=3.7",
+)
