1511e1fddd51580f3bb321e28a6f0dd2ca97314e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:40 +0000

    Add package k7eel/0.1.2/k7eel-0.1.2.tar.gz

---
+++ packages/k7eel/k7eel-0.1.2.tar.gz/k7eel-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="k7eel",
+    version="0.1.2",  # غيّر رقم النسخة هنا
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "k7eel=k7eel.main:main",
+        ],
+    },
+    author="اسمك",
+    description="مكتبة لتحميل وتشغيل ملف exe من رابط",
+    python_requires=">=3.6",
+)
