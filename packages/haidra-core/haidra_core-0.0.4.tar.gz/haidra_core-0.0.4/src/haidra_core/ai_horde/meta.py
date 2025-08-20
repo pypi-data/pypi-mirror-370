aa30c3973eb101cbc931cbdc578fec4f3c6a1232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:06:17 +0000

    Add package haidra-core/0.0.4/haidra_core-0.0.4.tar.gz

---
+++ packages/haidra-core/haidra_core-0.0.4.tar.gz/haidra_core-0.0.4/src/haidra_core/ai_horde/meta.py
@@ -0,0 +1,21 @@
+from pydantic import Field
+
+from haidra_core.meta import SharedCISettings
+
+from .settings import DEFAULT_MODELS_FOLDER
+
+
+class AIHordeCISettings(SharedCISettings):
+    """Settings for AI Horde CI/CD pipelines."""
+
+    hordelib_ci_ongoing: bool = Field(default=False)
+    """Indicates if the hordelib CI/CD pipeline is ongoing."""
+
+    horde_sdk_testing: bool = Field(default=False)
+    """Indicates if the AI Horde SDK is currently being tested."""
+
+    ai_horde_testing: bool = Field(default=False)
+    """Indicates if the AI Horde is currently being tested."""
+
+    aiworker_cache_home: str = DEFAULT_MODELS_FOLDER
+    """The folder where AI worker models are stored."""
