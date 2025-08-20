8865c5f5c31e44bf220e11743eac9d879b83eebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:53 +0000

    Add package tencent-wedata-feature-engineering/0.1.32.dev9/tencent_wedata_feature_engineering-0.1.32.dev9-py3-none-any.whl

---
+++ packages/tencent-wedata-feature-engineering/tencent_wedata_feature_engineering-0.1.32.dev9-py3-none-any.whl/wedata/feature_store/entities/mlflow_model.py
@@ -0,0 +1,8 @@
+import mlflow
+
+class ModelWrapper(mlflow.pyfunc.PythonModel):
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self, context, model_input):
+        return self.model.predict(model_input)
