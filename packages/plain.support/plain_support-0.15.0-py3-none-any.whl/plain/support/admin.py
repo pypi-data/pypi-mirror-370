064ab4919bd04320d84e0dc02f139764c43fb430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:05:44 +0000

    Add package plain.support/0.15.0/plain_support-0.15.0-py3-none-any.whl

---
+++ packages/plain.support/plain_support-0.15.0-py3-none-any.whl/plain/support/admin.py
@@ -0,0 +1,34 @@
+from plain.admin.cards import Card
+from plain.admin.views import (
+    AdminModelDetailView,
+    AdminModelListView,
+    AdminViewset,
+    register_viewset,
+)
+
+from .models import SupportFormEntry
+
+
+@register_viewset
+class SupportFormEntryAdmin(AdminViewset):
+    class ListView(AdminModelListView):
+        model = SupportFormEntry
+        nav_section = "Support"
+        title = "Form entries"
+        nav_icon = "headset"
+        fields = ["user", "email", "name", "form_slug", "created_at"]
+
+    class DetailView(AdminModelDetailView):
+        model = SupportFormEntry
+
+
+class UserSupportFormEntriesCard(Card):
+    title = "Recent support"
+    template_name = "support/card.html"
+
+    def get_template_context(self):
+        context = super().get_template_context()
+
+        context["entries"] = SupportFormEntry.objects.filter(user=self.view.object)
+
+        return context
