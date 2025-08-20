661261fa837313ebc21ac738bda23a6ca1b81055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:06:25 +0000

    Add package nillanet/1.0.2/nillanet-1.0.2-py3-none-any.whl

---
+++ packages/nillanet/nillanet-1.0.2-py3-none-any.whl/nillanet/loss.py
@@ -0,0 +1,29 @@
+import cupy as cp
+
+class Loss(object):
+  """Loss functions for NN class"""
+
+  def __init__(self):
+    pass
+
+  def mse(self,yhat,y):
+    return cp.mean((yhat - y)**2)
+
+  def mse_derivative(self,yhat,y):
+    if yhat.shape == y.shape:
+        return yhat - y
+    return yhat - cp.reshape(y,yhat.shape)
+
+  def mse_alt(y,yhat):
+    return cp.mean((y - yhat)**2)
+
+  def mse_alt_derivative(y,yhat):
+    if y.shape == yhat.shape:
+        return y - yhat
+    return cp.reshape(y,yhat.shape) - yhat
+
+  def binary_crossentropy(self,y,yhat):
+    return -(y * cp.log(yhat) + (1 - y) * cp.log(1 - yhat))
+
+  def binary_crossentropy_derivative(self,y,yhat):
+    return -(y / yhat) + (1 - y) / (1 - yhat)
