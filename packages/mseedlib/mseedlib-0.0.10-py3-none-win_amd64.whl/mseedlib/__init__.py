4dcba76570a543b3bb883b6c54991fc3a806def5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 07:05:41 +0000

    Add package mseedlib/0.0.10/mseedlib-0.0.10-py3-none-win_amd64.whl

---
+++ packages/mseedlib/mseedlib-0.0.10-py3-none-win_amd64.whl/mseedlib/__init__.py
@@ -0,0 +1,51 @@
+import warnings
+from .__version__ import __version__, libmseed_version
+from .definitions import (
+    NSTERROR,
+    NSTUNSET,
+    NSTMODULUS,
+    DataEncoding,
+    TimeFormat,
+    SubSecond,
+)
+from .exceptions import MseedLibError
+from .msrecord import MS3Record
+from .msrecord_reader import MS3RecordReader
+from .msrecord_buffer_reader import MS3RecordBufferReader
+from .mstracelist import MSTraceList
+from .util import (
+    nstime2timestr,
+    timestr2nstime,
+    sourceid2nslc,
+    nslc2sourceid,
+    sampletime,
+)
+
+# Issue deprecation warning when the package is imported
+warnings.warn(
+    "mseedlib is deprecated and no longer maintained. Please use pymseed instead: https://pypi.org/project/pymseed/",
+    DeprecationWarning,
+    stacklevel=2
+)
+
+
+__all__ = [
+    "__version__",
+    "libmseed_version",
+    "NSTERROR",
+    "NSTUNSET",
+    "NSTMODULUS",
+    "DataEncoding",
+    "TimeFormat",
+    "SubSecond",
+    "MseedLibError",
+    "MS3Record",
+    "MS3RecordReader",
+    "MS3RecordBufferReader",
+    "MSTraceList",
+    "nstime2timestr",
+    "timestr2nstime",
+    "sourceid2nslc",
+    "nslc2sourceid",
+    "sampletime",
+]
