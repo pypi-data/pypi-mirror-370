2589dfd97d49f132809cfc4afab769ceb83c9898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:40 +0000

    Add package dashlab/0.1.40/dashlab-0.1.40.tar.gz

---
+++ packages/dashlab/dashlab-0.1.40.tar.gz/dashlab-0.1.40/dashlab/__init__.py
@@ -0,0 +1,7 @@
+"""
+Dashlab - A Python package for dashboard and data visualization tools.
+"""
+
+__version__ = "0.1.40" # This line is automatically updated at build time, do not edit manually.
+
+__all__ = ["__version__"]
