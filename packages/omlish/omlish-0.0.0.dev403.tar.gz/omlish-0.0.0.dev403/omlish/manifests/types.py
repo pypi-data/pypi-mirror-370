3d46328255b6ef26bf9024849a455a5daf595c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 07:05:57 +0000

    Add package omlish/0.0.0.dev403/omlish-0.0.0.dev403.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev403.tar.gz/omlish-0.0.0.dev403/omlish/manifests/types.py
@@ -0,0 +1,21 @@
+# ruff: noqa: UP006 UP007 UP045
+# @omlish-lite
+import dataclasses as dc
+import typing as ta
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class ManifestOrigin:
+    module: str
+    attr: ta.Optional[str]  # None if inline
+
+    file: str
+    line: int
+
+
+@dc.dataclass(frozen=True)
+class Manifest(ManifestOrigin):
+    value: ta.Any
