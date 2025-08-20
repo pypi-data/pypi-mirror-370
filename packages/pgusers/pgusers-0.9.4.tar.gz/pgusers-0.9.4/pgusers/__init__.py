eedc3416934ff112bb59cae5caf49fceddb6b3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:07:09 +0000

    Add package pgusers/0.9.4/pgusers-0.9.4.tar.gz

---
+++ packages/pgusers/pgusers-0.9.4.tar.gz/pgusers-0.9.4/pgusers/__init__.py
@@ -0,0 +1,10 @@
+from .pgusers import BadCallError, UserSpace, OK, NOT_FOUND, EXPIRED, REJECTED
+
+__all__ = [
+    "BadCallError",
+    "UserSpace",
+    "OK",
+    "NOT_FOUND",
+    "EXPIRED",
+    "REJECTED",
+]
