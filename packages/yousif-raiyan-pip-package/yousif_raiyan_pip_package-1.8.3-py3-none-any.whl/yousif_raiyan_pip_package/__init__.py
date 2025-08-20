072051abc38ac7d4b7b2e4ab16a68df8f3f3e578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 05:05:08 +0000

    Add package yousif-raiyan-pip-package/1.8.3/yousif_raiyan_pip_package-1.8.3-py3-none-any.whl

---
+++ packages/yousif-raiyan-pip-package/yousif_raiyan_pip_package-1.8.3-py3-none-any.whl/yousif_raiyan_pip_package/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "1.8.3"
+
+from .edf_loader import EDFLoader
+from .trigger_detector import TriggerDetector
+from .analyzer import Analyzer
+from .eeg_graph_representation import EEGGraphProcessor
+from .fooof_analyzer import FOOOFAnalyzer
+from .spectral_analyzer import SpectralAnalyzer
+from .connectivity_analyzer import ConnectivityAnalyzer
+
+__all__ = ["EEGGraphProcessor",
+           "EDFLoader", 
+           "TriggerDetector",
+           "Analyzer",
+           "FOOOFAnalyzer",
+           "SpectralAnalyzer",
+           "ConnectivityAnalyzer"]
