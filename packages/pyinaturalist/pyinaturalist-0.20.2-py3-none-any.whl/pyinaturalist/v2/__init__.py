b2339c35b012a16d86814f3c6adc487eb11327ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 07:05:29 +0000

    Add package pyinaturalist/0.20.2/pyinaturalist-0.20.2-py3-none-any.whl

---
+++ packages/pyinaturalist/pyinaturalist-0.20.2-py3-none-any.whl/pyinaturalist/v2/__init__.py
@@ -0,0 +1,7 @@
+"""
+Functions to access the iNaturalist API v2
+See: http://api.inaturalist.org/v2/docs/
+"""
+
+# ruff: noqa: F401, F403
+from pyinaturalist.v2.observations import get_observations
