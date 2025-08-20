bf3387e859abd7a882e794329385403e7749356f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:06:04 +0000

    Add package runloop-api-client/0.55.2/runloop_api_client-0.55.2-py3-none-any.whl

---
+++ packages/runloop-api-client/runloop_api_client-0.55.2-py3-none-any.whl/runloop_api_client/types/benchmark_start_run_params.py
@@ -0,0 +1,44 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, Optional
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["BenchmarkStartRunParams", "RunProfile"]
+
+
+class BenchmarkStartRunParams(TypedDict, total=False):
+    benchmark_id: Required[str]
+    """ID of the Benchmark to run."""
+
+    metadata: Optional[Dict[str, str]]
+    """User defined metadata to attach to the benchmark run for organization."""
+
+    run_name: Optional[str]
+    """Display name of the run."""
+
+    run_profile: Annotated[Optional[RunProfile], PropertyInfo(alias="runProfile")]
+    """Runtime configuration to use for this benchmark run"""
+
+
+class RunProfile(TypedDict, total=False):
+    env_vars: Annotated[Optional[Dict[str, str]], PropertyInfo(alias="envVars")]
+    """Mapping of Environment Variable to Value.
+
+    May be shown in devbox logging. Example: {"DB_PASS": "DATABASE_PASSWORD"} would
+    set the environment variable 'DB_PASS' to the value 'DATABASE_PASSWORD_VALUE'.
+    """
+
+    purpose: Optional[str]
+    """Purpose of the run."""
+
+    secrets: Optional[Dict[str, str]]
+    """Mapping of Environment Variable to User Secret Name.
+
+    Never shown in devbox logging. Example: {"DB_PASS": "DATABASE_PASSWORD"} would
+    set the environment variable 'DB_PASS' to the value of the secret
+    'DATABASE_PASSWORD'.
+    """
