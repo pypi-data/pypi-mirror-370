bb3a053de3cf633837b2188e0ce0a542848b324d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:06:11 +0000

    Add package atlas-aws-autodownload/1.0.0/atlas_aws_autodownload-1.0.0.tar.gz

---
+++ packages/atlas-aws-autodownload/atlas_aws_autodownload-1.0.0.tar.gz/atlas_aws_autodownload-1.0.0/atlas_aws_autodownload/__init__.py
@@ -0,0 +1,13 @@
+"""
+Atlas AWS Auto Download
+
+A Python tool for downloading files from AWS S3 bucket with configuration file support.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+from .core import aws_download
+
+__all__ = ["aws_download"]
