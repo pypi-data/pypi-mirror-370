e75673fe3c3e31ff34f99ed372bb04d03443cc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 17:05:49 +0000

    Add package ragraph/1.22.9/ragraph-1.22.9-py3-none-any.whl

---
+++ packages/ragraph/ragraph-1.22.9-py3-none-any.whl/ragraph/__init__.py
@@ -0,0 +1,11 @@
+"""RaGraph
+
+Ratio graph handling in Python.
+"""
+
+__version__ = "1.22.9"
+
+from logging import Logger, getLogger
+
+logger: Logger = getLogger(__name__)
+"""RaGraph's central logger instance."""
