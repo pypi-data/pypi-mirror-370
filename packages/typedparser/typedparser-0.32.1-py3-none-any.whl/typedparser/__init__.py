da0fd9f6290fc6600df0b78ec7ca28f63aa5209e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:06:49 +0000

    Add package typedparser/0.32.1/typedparser-0.32.1-py3-none-any.whl

---
+++ packages/typedparser/typedparser-0.32.1-py3-none-any.whl/typedparser/__init__.py
@@ -0,0 +1,28 @@
+from ._typedattr import definenumpy, attrs_from_dict, NamedTupleMixin
+from ._typedparser import (
+    add_argument,
+    TypedParser,
+    VerboseQuietArgs,
+    TaskSplitterArgs,
+    split_list_given_task_splitter_args,
+    split_list_for_processing,
+)
+from .custom_format import CustomArgparseFmt
+from .objects import get_attr_names
+
+__all__ = [
+    "definenumpy",
+    "attrs_from_dict",
+    "NamedTupleMixin",
+    "add_argument",
+    "TypedParser",
+    "VerboseQuietArgs",
+    "CustomArgparseFmt",
+    "get_attr_names",
+    "TaskSplitterArgs",
+    "split_list_given_task_splitter_args",
+    "split_list_for_processing",
+]
+
+
+__version__ = "0.32.1"
