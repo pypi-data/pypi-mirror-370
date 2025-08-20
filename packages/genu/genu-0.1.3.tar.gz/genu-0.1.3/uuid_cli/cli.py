6f68efe5ace45c17a7fd5db1cca723cc4e955ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:05:28 +0000

    Add package genu/0.1.3/genu-0.1.3.tar.gz

---
+++ packages/genu/genu-0.1.3.tar.gz/genu-0.1.3/uuid_cli/cli.py
@@ -0,0 +1,54 @@
+import uuid
+import argparse
+from typing import List
+
+try:
+    from uuid6 import uuid7
+    has_uuid7 = True
+except ImportError:
+    has_uuid7 = False
+
+
+def generate_uuid(uuid_version: int, count: int, namespace: str = None) -> List[uuid.UUID]:
+    uuids = []
+    for _ in range(count):
+        if uuid_version == 1:
+            uuids.append(uuid.uuid1())
+        elif uuid_version == 3:
+            if not namespace:
+                raise ValueError("Namespace is required for UUID version 3.")
+            uuids.append(uuid.uuid3(uuid.NAMESPACE_URL, namespace))
+        elif uuid_version == 4:
+            uuids.append(uuid.uuid4())
+        elif uuid_version == 5:
+            if not namespace:
+                raise ValueError("Namespace is required for UUID version 5.")
+            uuids.append(uuid.uuid5(uuid.NAMESPACE_URL, namespace))
+        elif uuid_version == 7 and has_uuid7:
+            uuids.append(uuid7())
+        else:
+            raise ValueError("Unsupported UUID version. Use 1, 3, 4, 5, or 7.")
+    return uuids
+
+
+def main() -> None:
+    version = '0.1.3'
+
+    parser = argparse.ArgumentParser(description="Generate UUIDs of different types.")
+    parser.add_argument("-u", "--uuid-version", type=int, choices=[1, 3, 4, 5, 7], help="UUID version to generate (1, 3, 4, 5, 7)")
+    parser.add_argument("-c", "--count", type=int, default=1, help="Number of UUIDs to generate")
+    parser.add_argument("--namespace", type=str, help="Namespace URL for UUID version 3 or 5")
+    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s {version}")
+    args = parser.parse_args()
+
+    try:
+        uuid_version = args.uuid_version if args.uuid_version else 4
+        uuids = generate_uuid(uuid_version, args.count, args.namespace)
+        for u in uuids:
+            print(u)
+    except ValueError as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
