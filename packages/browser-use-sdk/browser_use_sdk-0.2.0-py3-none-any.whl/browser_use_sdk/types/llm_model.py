8e07da6ffa58047c7f34193f708ded28668a1fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:04:57 +0000

    Add package browser-use-sdk/0.2.0/browser_use_sdk-0.2.0-py3-none-any.whl

---
+++ packages/browser-use-sdk/browser_use_sdk-0.2.0-py3-none-any.whl/browser_use_sdk/types/llm_model.py
@@ -0,0 +1,22 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing_extensions import Literal, TypeAlias
+
+__all__ = ["LlmModel"]
+
+LlmModel: TypeAlias = Literal[
+    "gpt-4o",
+    "gpt-4o-mini",
+    "gpt-4.1",
+    "gpt-4.1-mini",
+    "o4-mini",
+    "o3",
+    "gemini-2.0-flash",
+    "gemini-2.0-flash-lite",
+    "gemini-2.5-flash-preview-04-17",
+    "gemini-2.5-flash",
+    "gemini-2.5-pro",
+    "claude-3-7-sonnet-20250219",
+    "claude-sonnet-4-20250514",
+    "llama-4-maverick-17b-128e-instruct",
+]
