04cb93c20abdc585ad522d7ddc9f079451eb80b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 05:05:28 +0000

    Add package sk-linearregression/0.2.0a22/sk_linearregression-0.2.0a22-py3-none-any.whl

---
+++ packages/sk-linearregression/sk_linearregression-0.2.0a22-py3-none-any.whl/sk/linearregression/HandleMissingValue.py
@@ -0,0 +1,23 @@
+import pandas as pd
+def HandleMissingValue(X):
+    # Calculate missing percentage per column
+    null_percentage = X.isnull().sum() / len(X) * 100
+
+    # Split columns by missing percentage
+    null_percentage_most = null_percentage[null_percentage > 40].index.tolist()
+    null_percentage_less = null_percentage[null_percentage < 40].index.tolist()
+
+    # Drop high-missing columns
+    X.drop(columns=null_percentage_most, inplace=True)
+
+    # Fill low-missing categorical columns with mode
+    for col in null_percentage_less:
+        if X[col].dtype == "object" or X[col].dtype.name == "category":
+            X[col] = X[col].fillna(X[col].mode()[0])
+        else:
+            X[col] = X[col].fillna(X[col].median())  # better for numeric
+
+
+    categorical_columns = X.select_dtypes(include=["object", "category"]).columns.tolist()
+    X_encoded = pd.get_dummies(X, columns=categorical_columns)
+    return X_encoded
