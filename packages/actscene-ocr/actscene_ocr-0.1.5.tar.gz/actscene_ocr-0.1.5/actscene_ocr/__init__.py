d2a7ba27b1d6d4921cf9aff6b4784d6e2360dcbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:31 +0000

    Add package actscene-ocr/0.1.5/actscene_ocr-0.1.5.tar.gz

---
+++ packages/actscene-ocr/actscene_ocr-0.1.5.tar.gz/actscene_ocr-0.1.5/actscene_ocr/__init__.py
@@ -0,0 +1,15 @@
+"""
+Actscene OCR
+
+以下の日本語文書に対応した包括的なOCRライブラリ:
+- 自動車検査証
+- 自動車查証記錄事項
+- リサイクル券
+- 印鑑証明書
+- 委任証明書
+- 自動車損害賠償責任保険証明書
+"""
+
+from .actscene_ocr import ActsceneOCR
+
+__all__ = ["ActsceneOCR"]
