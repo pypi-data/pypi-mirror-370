9a9de76f18a2e021de0429d9106e934d85ab1e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 17:05:45 +0000

    Add package energy-counters/1.2.5/energy_counters-1.2.5.tar.gz

---
+++ packages/energy-counters/energy_counters-1.2.5.tar.gz/energy_counters-1.2.5/energy_counters/lovato/dmg800.py
@@ -0,0 +1,15 @@
+"""
+Lovato DMG800 Counter Module
+
+This module will provide interface for the Lovato DMG800 energy meter.
+
+TODO: Implement the following:
+- Configuration classes
+- Communication protocol setup
+- Data collection functions
+- Error handling
+"""
+
+# TODO: Implement DMG800 counter functionality
+# class DMG800DataCollector:
+#     pass
