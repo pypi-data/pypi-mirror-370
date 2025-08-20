8e65b51ea2cc14d3a4bfb076a1f7c72d194f9650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 07:05:35 +0000

    Add package truss/0.9.120rc502/truss-0.9.120rc502-py3-none-any.whl

---
+++ packages/truss/truss-0.9.120rc502-py3-none-any.whl/truss_train/__init__.py
@@ -0,0 +1,17 @@
+from truss_train.definitions import (
+    Compute,
+    Runtime,
+    SecretReference,
+    TrainingJob,
+    TrainingProject,
+)
+from truss_train.public_api import push
+
+__all__ = [
+    "Compute",
+    "Runtime",
+    "SecretReference",
+    "TrainingJob",
+    "TrainingProject",
+    "push",
+]
