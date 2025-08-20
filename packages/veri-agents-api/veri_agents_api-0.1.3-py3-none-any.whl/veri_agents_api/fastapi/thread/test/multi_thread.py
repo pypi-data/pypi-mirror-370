3c55dffe160f80ce46f6c5453897b049f5bc5431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:05:47 +0000

    Add package veri-agents-api/0.1.3/veri_agents_api-0.1.3-py3-none-any.whl

---
+++ packages/veri-agents-api/veri_agents_api-0.1.3-py3-none-any.whl/veri_agents_api/fastapi/thread/test/multi_thread.py
@@ -0,0 +1,59 @@
+from typing import Annotated
+
+from langchain_aws import ChatBedrock
+from typing_extensions import TypedDict
+
+from fastapi import FastAPI
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+import uvicorn
+
+from veri_agents_api.fastapi.thread import create_thread_router
+
+if __name__ == "__main__":
+    class State(TypedDict):
+        messages: Annotated[list, add_messages]
+
+    graph_builder = StateGraph(State)
+
+    llm = ChatBedrock(model_id="us.anthropic.claude-3-7-sonnet-20250219-v1:0") # pyright: ignore[reportCallIssue]
+
+    def chatbot(state: State):
+        return {"messages": [llm.invoke(state["messages"])]}
+
+    # The first argument is the unique node name
+    # The second argument is the function or object that will be called whenever
+    # the node is used.
+    graph_builder.add_node("chatbot", chatbot)
+    graph_builder.set_entry_point("chatbot")
+    graph_builder.set_finish_point("chatbot")
+
+
+
+    # in-memory persistence
+    memory = MemorySaver()
+    graph = graph_builder.compile(checkpointer=memory)
+
+    # veri-agents convenience router
+    thread_router = create_thread_router(
+        # same graph for every request
+        get_graph=lambda req: graph,
+        # derive thread id from /thread/{thread_id} path param
+        get_thread_id=lambda req: req.path_params["thread_id"]
+    )
+
+    # root fastapi app
+    app = FastAPI()
+    app.include_router(thread_router, prefix="/threads/{thread_id}")
+
+    uvicorn.run(app, port=5000, log_level="info")
+    # you can now access:
+    #   GET  /openapi.json
+    #   GET  /threads
+    #   GET /thread/{thread_id}/info
+    #   POST /threads/{thread_id}/invoke
+    #   POST /threads/{thread_id}/stream
+    #   GET  /threads/{thread_id}/history
+    #   GET  /threads/{thread_id}/feedback
+    #   POST /threads/{thread_id}/feedback
