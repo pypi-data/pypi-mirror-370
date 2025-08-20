0a3545cc3356131f60bf324c21f69f59ca2b2879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:22:44 +0000

    Add package py3comtrade/4.0.2/py3comtrade-4.0.2.tar.gz

---
+++ packages/py3comtrade/py3comtrade-4.0.2.tar.gz/py3comtrade-4.0.2/py3comtrade/model/analog.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  Copyright (c) [2019] [name of copyright holder]
+#  [py3comtrade] is licensed under Mulan PSL v2.
+#  You can use this software according to the terms and conditions of the Mulan
+#  PSL v2.
+#  You may obtain a copy of Mulan PSL v2 at:
+#           http://license.coscl.org.cn/MulanPSL2
+#  THIS SOFTWARE IS PROVIDED ON CFGAN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+#  KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+#  NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+#  See the Mulan PSL v2 for more details.
+
+from pydantic import Field
+
+from .channel import Channel
+from .type import ElectricalUnit, PsType
+
+
+class Analog(Channel):
+    unit: ElectricalUnit = Field(default=ElectricalUnit.NONE, description="通道单位")
+    a: float = Field(default=1.0, description="通道增益系数")
+    b: float = Field(default=0.0, description="通道偏移系数")
+    skew: float = Field(default=0.0, description="通道时滞（us）")
+    min_val: float = Field(default=0.0, description="通道最小值")
+    max_val: float = Field(default=0.0, description="通道最大值")
+    primary: float = Field(default=1.0, description="通道互感器变比一次系数")
+    secondary: float = Field(default=1.0, description="通道互感器变比二次系数")
+    ps: PsType = Field(default=PsType.P, description="一次还是二次值标识")
+    ratio: float = Field(default=1.0, description="通道比率")
+    is_current: bool = Field(default=True, description="是否电流通道")
+    y: list[float] = Field(default_factory=list, description="通道数值")
+
+    def clear(self) -> None:
+        """清除模型中所有字段"""
+        super().clear()
+        for field in self.model_fields.keys():
+            setattr(self, field, None)
+
+    def __str__(self):
+        return (
+                super().__str__()
+                + f",{self.unit.code},{self.a},{self.b},{self.skew},{self.min_val},{self.max_val}"
+                + f",{self.primary},{self.secondary},{self.ps.code}"
+        )
