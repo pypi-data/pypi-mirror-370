7d243504b47fb978208bf5fdb28a15dae6b83286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:21:58 +0000

    Add package abstract-gui/0.1.63.50/abstract_gui-0.1.63.50.tar.gz

---
+++ packages/abstract-gui/abstract_gui-0.1.63.50.tar.gz/abstract_gui-0.1.63.50/src/abstract_gui/QT6/managers/WindowManagerApp/__init__.py
@@ -0,0 +1,6 @@
+from .main import WindowManagerApp
+from .imports import QApplication,sys
+def start_window_info_gui():
+    app = QApplication(sys.argv)
+    win = WindowManagerApp(); win.show()
+    sys.exit(app.exec_())
