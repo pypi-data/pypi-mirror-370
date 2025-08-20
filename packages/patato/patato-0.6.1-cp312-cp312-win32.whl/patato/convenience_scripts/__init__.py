22d4b08c4cf7d7db4469d14ad3a8728a626837cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:21:55 +0000

    Add package patato/0.6.1/patato-0.6.1-cp312-cp312-win32.whl

---
+++ packages/patato/patato-0.6.1-cp312-cp312-win32.whl/patato/convenience_scripts/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) Thomas Else 2023-25.
+#  License: MIT
+
+"""
+PATATO command line interfaces
+==============================
+
+A series of convenience scripts for running the patato package in the command line.
+"""
