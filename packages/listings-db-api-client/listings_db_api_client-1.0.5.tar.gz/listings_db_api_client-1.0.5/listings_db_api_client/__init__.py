9259ff4b904a39391dad1d771ed4091052e7616f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:06:26 +0000

    Add package listings-db-api-client/1.0.5/listings_db_api_client-1.0.5.tar.gz

---
+++ packages/listings-db-api-client/listings_db_api_client-1.0.5.tar.gz/listings_db_api_client-1.0.5/listings_db_api_client/__init__.py
@@ -0,0 +1,10 @@
+"""
+Listings DB API Client Package
+
+A Python client library for the Finder property listings database API.
+"""
+
+from .listings_db_api_client import ListingsDBAPIClient
+
+__version__ = "1.0.5"
+__all__ = ["ListingsDBAPIClient"]
