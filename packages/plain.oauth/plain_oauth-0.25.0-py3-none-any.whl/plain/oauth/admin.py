33db2ef2b0fc2ad46bcee7c8c4af7c4ce7f8b09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 04:05:43 +0000

    Add package plain.oauth/0.25.0/plain_oauth-0.25.0-py3-none-any.whl

---
+++ packages/plain.oauth/plain_oauth-0.25.0-py3-none-any.whl/plain/oauth/admin.py
@@ -0,0 +1,48 @@
+from plain.admin.cards import ChartCard
+from plain.admin.views import (
+    AdminModelDetailView,
+    AdminModelListView,
+    AdminViewset,
+    register_viewset,
+)
+from plain.models import Count
+
+from .models import OAuthConnection
+
+
+class ProvidersChartCard(ChartCard):
+    title = "Providers"
+
+    def get_chart_data(self) -> dict:
+        results = (
+            OAuthConnection.objects.all()
+            .values("provider_key")
+            .annotate(count=Count("id"))
+        )
+        return {
+            "type": "doughnut",
+            "data": {
+                "labels": [result["provider_key"] for result in results],
+                "datasets": [
+                    {
+                        "label": "Providers",
+                        "data": [result["count"] for result in results],
+                    }
+                ],
+            },
+        }
+
+
+@register_viewset
+class OAuthConnectionViewset(AdminViewset):
+    class ListView(AdminModelListView):
+        nav_section = "OAuth"
+        model = OAuthConnection
+        title = "Connections"
+        nav_icon = "link-45deg"
+        fields = ["id", "user", "provider_key", "provider_user_id"]
+        cards = [ProvidersChartCard]
+
+    class DetailView(AdminModelDetailView):
+        model = OAuthConnection
+        title = "OAuth connection"
