f6ecfcc6c4f3a9719df651f5351fb2b02b98a240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:17 +0000

    Add package fedfred/2.1.3/fedfred-2.1.3.tar.gz

---
+++ packages/fedfred/fedfred-2.1.3.tar.gz/fedfred-2.1.3/src/fedfred/__about__.py
@@ -0,0 +1,29 @@
+# filepath: /src/fedfred/__about__.py
+#
+# Copyright (c) 2025 Nikhil Sunder
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+This module contains metadata about the fedfred package.
+It includes the package name, description, version, author, license, and URL.
+This information is used for package distribution and documentation.
+"""
+
+__title__ = "fedfred"
+__description__ = "A feature-rich python package for interacting with the Federal Reserve Bank of St. Louis Economic Database: FRED"
+__version__ = "2.1.2"
+__copyright__ = "Copyright (c) 2025 Nikhil Sunder"
+__author__ = "Nikhil Sunder"
+__license__ = "AGPL-3.0-or-later"
+__url__ = "https://github.com/nikhilxsunder/fedfred"
