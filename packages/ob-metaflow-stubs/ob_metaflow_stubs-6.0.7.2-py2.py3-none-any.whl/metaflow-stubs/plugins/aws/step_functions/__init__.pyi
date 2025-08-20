d4950645a0865c5c3b5cac6cc716cee2b66f832e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:06:02 +0000

    Add package ob-metaflow-stubs/6.0.7.2/ob_metaflow_stubs-6.0.7.2-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.7.2-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/step_functions/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.17.1.0+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-19T23:54:56.026062                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import schedule_decorator as schedule_decorator
+from . import step_functions_deployer as step_functions_deployer
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.7.2-py2.py3-none-any.whl/metaflow-stubs/plugins/optuna/__init__.pyi
@@ -0,0 +1,24 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.17.1.0+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-19T23:54:55.931443                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def auth():
+    ...
+
+def get_deployment_db_access_endpoint(name: str):
+    ...
+
+def get_db_url(app_name: str):
+    """
+    Example usage:
+        >>> from metaflow.plugins.optuna import get_db_url
+        >>> s = optuna.create_study(..., storage=get_db_url("optuna-dashboard"))
+    """
+    ...
+
