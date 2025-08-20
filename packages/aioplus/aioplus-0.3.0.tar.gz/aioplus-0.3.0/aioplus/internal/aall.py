f712f9c7393255da47089ea300e8e86215d7edd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 16:06:22 +0000

    Add package aioplus/0.3.0/aioplus-0.3.0.tar.gz

---
+++ packages/aioplus/aioplus-0.3.0.tar.gz/aioplus-0.3.0/aioplus/internal/aall.py
@@ -0,0 +1,50 @@
+from collections.abc import AsyncIterable
+
+from aioplus.internal.coercions import to_async_iterable
+from aioplus.internal.typing import SupportsBool
+
+
+async def aall(aiterable: AsyncIterable[SupportsBool], /) -> bool:
+    """
+    Return :obj:`True` if all elements of the async iterable evaluate to :obj:`True`.
+
+    Parameters
+    ----------
+    aiterable : AsyncIterable of SupportsBool
+        An asynchronous iterable of objects supporting :meth:`object.__bool__`.
+
+    Returns
+    -------
+    :class:`bool`
+        :obj:`True` if all elements evaluate to :obj:`True`, or if the iterable is empty.
+        :obj:`False` if any element evaluates to :obj:`False`.
+
+    Examples
+    --------
+    >>> import asyncio
+    >>>
+    >>> from aioplus import aall, arange
+    >>>
+    >>> async def main() -> None:
+    >>>     '''Run the program.'''
+    >>>     aiterable = (num > 0 async for num in arange(2304))
+    >>>     flg = await aall(aiterable)
+    >>>
+    >>> if __name__ == '__main__':
+    >>>     asyncio.run(main())
+
+    Notes
+    -----
+    - Short-circuits on the first object that evaluates to :obj:`False`.
+
+    See Also
+    --------
+    :func:`all`
+    """
+    aiterable = to_async_iterable(aiterable, variable_name="aiterable")
+
+    async for value in aiterable:
+        if not value:
+            return False
+
+    return True
---
+++ packages/aioplus/aioplus-0.3.0.tar.gz/aioplus-0.3.0/aioplus/internal/aenumerate.py
@@ -0,0 +1,99 @@
+from collections.abc import AsyncIterable, AsyncIterator
+from dataclasses import dataclass
+from typing import Self, SupportsIndex, TypeVar
+
+from aioplus.internal.coercions import to_async_iterable, to_int
+
+
+T = TypeVar("T")
+
+
+def aenumerate(
+    aiterable: AsyncIterable[T],
+    /,
+    start: SupportsIndex = 0,
+) -> AsyncIterable[tuple[int, T]]:
+    """Return an enumerated iterator.
+
+    Parameters
+    ----------
+    aiterable : AsyncIterable of T
+        An asynchronous iterable of objects to enumerate.
+
+    start : int, default 0
+        The starting index. Must be an object supporting :meth:`object.__index__`.
+
+    Returns
+    -------
+    AsyncIterable of tuple[int, T]
+        An asynchronous iterable yielding pairs of the form ``(index, object)``.
+
+    Examples
+    --------
+    >>> import asyncio
+    >>>
+    >>> from aioplus import aenumerate, arange
+    >>>
+    >>> async def main() -> None:
+    >>>     '''Run the program.'''
+    >>>     async for index, num in aenumerate(arange(2304)):
+    >>>         print(index, num)
+    >>>
+    >>> if __name__ == '__main__':
+    >>>     asyncio.run(main())
+
+    See Also
+    --------
+    :func:`enumerate`
+    """
+    aiterable = to_async_iterable(aiterable, variable_name="aiterable")
+    start = to_int(start, variable_name="start")
+
+    return AenumerateIterable(aiterable, start)
+
+
+@dataclass
+class AenumerateIterable(AsyncIterable[tuple[int, T]]):
+    """An enumerated asynchronous iterable."""
+
+    aiterable: AsyncIterable[T]
+    start: int
+
+    def __aiter__(self) -> AsyncIterator[tuple[int, T]]:
+        """Return an asynchronous iterator."""
+        aiterator = aiter(self.aiterable)
+        return AenumerateIterator(aiterator, self.start)
+
+
+@dataclass
+class AenumerateIterator(AsyncIterator[tuple[int, T]]):
+    """An enumerated asynchronous iterator."""
+
+    aiterator: AsyncIterator[T]
+    start: int
+
+    def __post_init__(self) -> None:
+        """Initialize the object."""
+        self._next_count: int = self.start
+        self._finished_flg: bool = False
+
+    def __aiter__(self) -> Self:
+        """Return an asynchronous iterator."""
+        return self
+
+    async def __anext__(self) -> tuple[int, T]:
+        """Return the next value."""
+        if self._finished_flg:
+            raise StopAsyncIteration
+
+        try:
+            value = await anext(self.aiterator)
+
+        except Exception:
+            self._finished_flg = True
+            raise
+
+        count = self._next_count
+        self._next_count += 1
+
+        return (count, value)
