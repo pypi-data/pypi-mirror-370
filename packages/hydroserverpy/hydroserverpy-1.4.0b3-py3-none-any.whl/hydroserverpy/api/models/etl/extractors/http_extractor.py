bbdcdd8908176e7d3fccf8cc956fefb380bab337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:05:41 +0000

    Add package hydroserverpy/1.4.0b3/hydroserverpy-1.4.0b3-py3-none-any.whl

---
+++ packages/hydroserverpy/hydroserverpy-1.4.0b3-py3-none-any.whl/hydroserverpy/api/models/etl/extractors/http_extractor.py
@@ -0,0 +1,31 @@
+import logging
+import requests
+from io import BytesIO
+
+from ..etl_configuration import Payload
+from .base import Extractor, ExtractorConfig
+
+
+class HTTPExtractor(Extractor):
+    def __init__(self, settings: ExtractorConfig):
+        super().__init__(settings)
+
+    def extract(self, payload: Payload, loader=None):
+        """
+        Downloads the file from the HTTP/HTTPS server and returns a file-like object.
+        """
+        url = self.resolve_placeholder_variables(payload, loader)
+        logging.info(f"Requesting data from → {url}")
+
+        try:
+            response = requests.get(url)
+        except Exception as e:
+            logging.error(f"Failed to fetch {url}: {e}")
+            raise
+
+        data = BytesIO()
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                data.write(chunk)
+        data.seek(0)
+        return data
