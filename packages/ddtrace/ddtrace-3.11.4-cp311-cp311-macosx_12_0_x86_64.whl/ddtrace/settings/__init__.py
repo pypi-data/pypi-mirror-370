973db80142e69797e67f1c2d2766c17ba0b63e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 06:05:03 +0000

    Add package ddtrace/3.11.4/ddtrace-3.11.4-cp311-cp311-macosx_12_0_x86_64.whl

---
+++ packages/ddtrace/ddtrace-3.11.4-cp311-cp311-macosx_12_0_x86_64.whl/ddtrace/settings/__init__.py
@@ -0,0 +1,12 @@
+from .._hooks import Hooks
+from .exceptions import ConfigException
+from .http import HttpConfig
+from .integration import IntegrationConfig
+
+
+__all__ = [
+    "ConfigException",
+    "HttpConfig",
+    "Hooks",
+    "IntegrationConfig",
+]
