bcc5609e1d518dcc28916dce80305541241e1f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 15:06:09 +0000

    Add package otlmow-model/2.16.1.6/otlmow_model-2.16.1.6.tar.gz

---
+++ packages/otlmow-model/otlmow_model-2.16.1.6.tar.gz/otlmow_model-2.16.1.6/otlmow_model/OtlmowModel/Classes/Installatie/Tunnelverlichting.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from ...Classes.ImplementatieElement.NaampadObject import NaampadObject
+from otlmow_model.OtlmowModel.GeometrieTypes.LijnGeometrie import LijnGeometrie
+from otlmow_model.OtlmowModel.GeometrieTypes.VlakGeometrie import VlakGeometrie
+
+
+# Generated with OTLClassCreator. To modify: extend, do not edit
+class Tunnelverlichting(NaampadObject, LijnGeometrie, VlakGeometrie):
+    """Het systeem van verlichtingselementen en armaturen, typisch ontworpen en geïnstalleerd in een tunnel, om aangepaste lichtniveaus volgens verschillende regimes te bieden voor veilig verkeer en zichtbaarheid binnenin een koker."""
+
+    typeURI = 'https://wegenenverkeer.data.vlaanderen.be/ns/installatie#Tunnelverlichting'
+    """De URI van het object volgens https://www.w3.org/2001/XMLSchema#anyURI."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.add_valid_relation(relation='https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#HoortBij', target='https://wegenenverkeer.data.vlaanderen.be/ns/installatie#BasisverlichtingTunnel', direction='i')  # i = direction: incoming
+        self.add_valid_relation(relation='https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#HoortBij', target='https://wegenenverkeer.data.vlaanderen.be/ns/installatie#VersterkingsverlichtingTunnel', direction='i')  # i = direction: incoming
+        self.add_valid_relation(relation='https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#HoortBij', target='https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Lichtsensor', direction='i')  # i = direction: incoming
