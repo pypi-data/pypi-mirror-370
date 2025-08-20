7e58398bb4d522ac0ce801345cfa899580d3c36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 14:05:26 +0000

    Add package qti2txt/0.0.1/qti2txt-0.0.1.tar.gz

---
+++ packages/qti2txt/qti2txt-0.0.1.tar.gz/qti2txt-0.0.1/src/qti2txt/helpers.py
@@ -0,0 +1,36 @@
+import html2text
+import re
+
+
+# Clean up HTML
+def html_to_cleantext(html_text):
+    h = html2text.HTML2Text()
+    h.ignore_links = False
+    h.body_width = 0
+    h.single_line_break = True
+    clean_text = h.handle(html_text)  # convert
+    if clean_text is None:
+        return ""
+    clean_text = re.sub(r"\s+", " ", clean_text)  # remove extra spaces
+    return clean_text
+
+
+# Renumber text file
+def renumber_file(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+
+    new_lines = []
+    question_number = 1
+
+    for line in lines:
+        # Match lines that start with a number followed by a period and a space
+        if re.match(r"^\d+\.\s", line):
+            new_line = re.sub(r"^\d+\.\s", f"{question_number}. ", line)
+            new_lines.append(new_line)
+            question_number += 1
+        else:
+            new_lines.append(line)
+
+    with open(file_path, "w") as file:
+        file.writelines(new_lines)
