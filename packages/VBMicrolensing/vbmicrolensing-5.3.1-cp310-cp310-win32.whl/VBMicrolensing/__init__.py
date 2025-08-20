a58c613a02b0f143beb0635a07b0ea87c7fac09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 03:22:37 +0000

    Add package VBMicrolensing/5.3.1/vbmicrolensing-5.3.1-cp310-cp310-win32.whl

---
+++ packages/VBMicrolensing/vbmicrolensing-5.3.1-cp310-cp310-win32.whl/VBMicrolensing/__init__.py
@@ -0,0 +1,9 @@
+from .VBMicrolensing import VBMicrolensing
+
+import inspect
+import os
+
+_fil_in=inspect.getfile(VBMicrolensing)
+VBMicrolensing.SetESPLtablefile(os.path.join(os.path.join(os.path.dirname(_fil_in), 'data'), 'ESPL.tbl'))
+VBMicrolensing.SetSuntablefile(os.path.join(os.path.join(os.path.dirname(_fil_in), 'data'), 'SunEphemeris.txt'))
+
