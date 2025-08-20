890c6f240398054c7c1249e18277dafd49c2717b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 05:05:13 +0000

    Add package markitdown-pro/1.0.4/markitdown_pro-1.0.4.tar.gz

---
+++ packages/markitdown-pro/markitdown_pro-1.0.4.tar.gz/markitdown_pro-1.0.4/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="markitdown-pro",
+    version="1.0.4",
+    author="Developer",
+    description="A package that converts almost any file format to Markdown.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(
+        exclude=["tests", "tests/*", ".venv", ".venv/*", ".vscode", ".vscode/*"]
+    ),
+    python_requires=">=3.12.2",
+    install_requires=[
+        "pillow==11.1.0",
+        "requests",
+        "httpx",
+        "python-dotenv",
+        "azure-core",
+        "azure-ai-documentintelligence==1.0.2",
+        "azure-cognitiveservices-speech==1.42.0",
+        "azure-common==1.1.28",
+        "whisper",
+        "markitdown[all]",
+        "langchain==0.3.27",
+        "langchain-core==0.3.74",
+        "langchain-openai==0.3.29",
+        "langchain-text-splitters==0.3.9",
+        "langdetect==1.0.9",
+        "langsmith==0.4.13",
+        "openai==1.99.6",
+        "PyMuPDF",
+        "tabulate==0.9.0",
+        "chardet==5.2.0",
+        "unstructured[all-docs]",
+        "youtube-transcript-api",
+        "pyexiftool",
+        "pydub",
+        "nbformat",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
