f6de2bd6747f24d4f7b70582dc93f158327fea97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-20 17:05:44 +0000

    Add package llm-chat-term/0.2.10/llm_chat_term-0.2.10-py3-none-any.whl

---
+++ packages/llm-chat-term/llm_chat_term-0.2.10-py3-none-any.whl/llm_chat_term/llm/models.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class ModelConfig(BaseModel):
+    provider: str
+    name: str
+    temperature: float | None = None
+
+
+def get_models():
+    return [
+        ModelConfig(provider="google", name="gemini-2.5-pro"),
+        ModelConfig(provider="deepseek", name="deepseek-chat"),
+        ModelConfig(provider="openai", name="o3-mini"),
+        ModelConfig(provider="openai", name="gpt-4o"),
+        ModelConfig(
+            provider="anthropic",
+            name="claude-3-7-sonnet-20250219",
+        ),
+    ]
