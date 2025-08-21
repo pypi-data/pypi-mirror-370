7549d9744e85f01603e577ad26023723bc7feac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:08:20 +0000

    Add package pycof/1.5.15/pycof-1.5.15.tar.gz

---
+++ packages/pycof/pycof-1.5.15.tar.gz/pycof-1.5.15/pycof/init_template.py
@@ -0,0 +1,8 @@
+from .sql import *
+from .data import *
+from .format import *
+from .misc import _get_config as get_config
+from .misc import _pycof_folders as pycof_folders
+from .misc import setup_logging
+
+__version__ = '{version}'
