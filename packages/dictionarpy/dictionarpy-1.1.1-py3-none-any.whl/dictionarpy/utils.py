fad04bab6802bd2bae75103377047c2ee767fa35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:08:32 +0000

    Add package dictionarpy/1.1.1/dictionarpy-1.1.1-py3-none-any.whl

---
+++ packages/dictionarpy/dictionarpy-1.1.1-py3-none-any.whl/dictionarpy/utils.py
@@ -0,0 +1,13 @@
+class AnsiWrapperGenerator:
+    '''
+    Return a function to wrap around a string which will style the text
+    according to the passed ANSI escape codes
+    '''
+    def __init__(self, no_ansi: bool):
+        self.no_ansi = no_ansi
+        
+
+    def genwrapper(self, start_code: str, end_code: str):
+        if self.no_ansi:
+            return lambda string: string
+        return lambda string: start_code + string + end_code
