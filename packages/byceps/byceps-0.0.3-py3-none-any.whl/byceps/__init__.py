716e08c9af4d0ff7ac724a6657b4f40ed0aab70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:06:49 +0000

    Add package byceps/0.0.3/byceps-0.0.3-py3-none-any.whl

---
+++ packages/byceps/byceps-0.0.3-py3-none-any.whl/byceps/__init__.py
@@ -0,0 +1,19 @@
+TEXT = """
+Hey there!
+
+It seems you have tried to install BYCEPS from PyPI via `pip install byceps`.
+However, as of now, that is not the way to install BYCEPS (at least not yet).
+
+Please visit the homepage at https://byceps.nwsnet.de/ to learn how to
+actually install BYCEPS.
+
+Thanks a lot and have a great time!
+"""
+
+
+def main():
+    print(TEXT)
+
+
+if __name__ == "__main__":
+    main()
