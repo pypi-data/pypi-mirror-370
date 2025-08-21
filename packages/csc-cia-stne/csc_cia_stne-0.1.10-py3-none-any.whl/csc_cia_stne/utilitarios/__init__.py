78a4f7332f4336142b259fcf39fd77e7de6c58b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:06:09 +0000

    Add package csc-cia-stne/0.1.10/csc_cia_stne-0.1.10-py3-none-any.whl

---
+++ packages/csc-cia-stne/csc_cia_stne-0.1.10-py3-none-any.whl/csc_cia_stne/utilitarios/__init__.py
@@ -0,0 +1,11 @@
+from .functions import titulo, recriar_pasta, b64encode, b64decode, convert_bquery_result_to_json,get_config, get_secret, now_sp
+__all__ = [
+    "titulo", 
+    "recriar_pasta", 
+    "b64encode", 
+    "b64decode", 
+    "convert_bquery_result_to_json",
+    "get_config",
+    "get_secret",
+    "now_sp",
+    ]
