459cbd0082037360f1cc732a347ad61b0309322b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 15:05:36 +0000

    Add package scCAMUS/1.0.1/sccamus-1.0.1.tar.gz

---
+++ packages/scCAMUS/sccamus-1.0.1.tar.gz/sccamus-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import Command, find_packages, setup
+
+
+setup(
+    name = "scCAMUS",
+    version = "1.0.1",
+    description = "Cross-datasets annotation modeling with universal reference data and method selection",
+    url = "https://github.com/Zhanglabtools/CAMUS",
+    author = "Qunlun Shen",
+    author_email = "knotnet@foxmail.com",
+    license = "MIT",
+    packages = ['scCAMUS'],
+    install_requires = ["requests",],
+    zip_safe = False,
+    include_package_data = True,
+)
