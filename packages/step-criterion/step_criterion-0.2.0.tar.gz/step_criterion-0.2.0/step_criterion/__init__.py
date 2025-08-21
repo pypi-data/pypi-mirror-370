79b3ca5a6e47af7ecc733395d77ff428db56915c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 08:06:18 +0000

    Add package step-criterion/0.2.0/step_criterion-0.2.0.tar.gz

---
+++ packages/step-criterion/step_criterion-0.2.0.tar.gz/step_criterion-0.2.0/step_criterion/__init__.py
@@ -0,0 +1,10 @@
+from .stepwise import StepwiseResult, step_criterion, step_aic, step_bic, step_adjr2, step_pvalue
+
+__all__ = [
+    "StepwiseResult",
+    "step_criterion",
+    "step_aic",
+    "step_bic",
+    "step_adjr2",
+    "step_pvalue",
+]
