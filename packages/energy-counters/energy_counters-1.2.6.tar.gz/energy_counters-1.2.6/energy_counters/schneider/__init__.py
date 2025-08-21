94f3e549708b18b1d76ee260d887fc6a4344671a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:07:46 +0000

    Add package energy-counters/1.2.6/energy_counters-1.2.6.tar.gz

---
+++ packages/energy-counters/energy_counters-1.2.6.tar.gz/energy_counters-1.2.6/energy_counters/schneider/__init__.py
@@ -0,0 +1,15 @@
+"""
+Schneider Counter Modules
+
+This module provides interfaces for Schneider energy meters and counters.
+
+Available counters:
+- IEM3250: (To be implemented)
+- IEM3155: (To be implemented)
+"""
+
+# TODO: Implement Schneider counter classes and functions
+# from .iem3250 import IEM3250DataCollector
+# from .iem3155 import IEM3155DataCollector
+
+__all__ = []
