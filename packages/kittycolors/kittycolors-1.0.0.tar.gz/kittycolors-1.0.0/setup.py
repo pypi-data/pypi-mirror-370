d2b027fffba3a69679af3f65089b1cb66e7bc900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 15:05:56 +0000

    Add package kittycolors/1.0.0/kittycolors-1.0.0.tar.gz

---
+++ packages/kittycolors/kittycolors-1.0.0.tar.gz/kittycolors-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="kittycolors",
+    version="1.0.0",
+    description="color lib dats kewlio",
+    author="g4",
+    author_email="defnotg4@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6',
+)
+
