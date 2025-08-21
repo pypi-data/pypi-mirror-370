c53dbe6595bd15ae311a2e708677ef3000ba05dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:18 +0000

    Add package memory-fs/0.16.0/memory_fs-0.16.0.tar.gz

---
+++ packages/memory-fs/memory_fs-0.16.0.tar.gz/memory_fs-0.16.0/memory_fs/helpers/Memory_FS__Latest_Temporal.py
@@ -0,0 +1,12 @@
+from typing                                                         import List
+from osbot_utils.type_safe.primitives.safe_str.identifiers.Safe_Id  import Safe_Id
+from memory_fs.Memory_FS                                            import Memory_FS
+
+
+class Memory_FS__Latest_Temporal(Memory_FS):                                                # Latest + temporal pattern
+    def __init__(self, storage_fs = None         ,
+                       areas      : List[Safe_Id] = None,
+                       **kwargs):                                                           # params for Path__Handler
+        super().__init__(storage_fs=storage_fs)
+        self.add_handler__latest  (**kwargs)
+        self.add_handler__temporal(areas=areas, **kwargs)
