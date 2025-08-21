112865ee2f87a0424f143e2f33792abad87f1220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 15:05:29 +0000

    Add package otpcr/92/otpcr-92-py3-none-any.whl

---
+++ packages/otpcr/otpcr-92-py3-none-any.whl/otpcr/modules/__init__.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+
+
+"modules"
+
+
+from . import cmd, lst, thr
+from . import irc, rss
+from . import pth, req, slg
+from . import dbg, srv # noqa: F401
+from . import fnd
+
+
+__all__ = (
+    "cmd",
+    "fnd",
+    "irc",
+    "lst",
+    'pth',
+    "req",
+    "rss",
+    "slg",
+    "thr"
+)
+
+
+def __dir__():
+    return __all__
