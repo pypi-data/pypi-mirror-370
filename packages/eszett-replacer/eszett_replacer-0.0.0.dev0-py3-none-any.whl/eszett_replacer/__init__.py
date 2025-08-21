5821c0d00888aa0c8bf158c0dc3f9cf56b5820ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 08:05:39 +0000

    Add package eszett-replacer/0.0.0.dev0/eszett_replacer-0.0.0.dev0-py3-none-any.whl

---
+++ packages/eszett-replacer/eszett_replacer-0.0.0.dev0-py3-none-any.whl/eszett_replacer/__init__.py
@@ -0,0 +1,5 @@
+from eszett_replacer.core import *
+from eszett_replacer.tests import *
+
+if __name__ == "__main__":
+    main()
---
+++ packages/eszett-replacer/eszett_replacer-0.0.0.dev0-py3-none-any.whl/eszett_replacer/__main__.py
@@ -0,0 +1,4 @@
+from eszett_replacer import main
+
+if __name__ == "__main__":
+    main()
