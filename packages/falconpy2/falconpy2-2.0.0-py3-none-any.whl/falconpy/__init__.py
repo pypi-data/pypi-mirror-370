27c6a45ad1e6d705d5dd7a9e95b6dd8e8ef43c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:17 +0000

    Add package falconpy2/2.0.0/falconpy2-2.0.0-py3-none-any.whl

---
+++ packages/falconpy2/falconpy2-2.0.0-py3-none-any.whl/falconpy/__init__.py
@@ -0,0 +1,9 @@
+from ._hal import (
+    import_as,
+    install,
+    uninstall,
+    HALModuleProxy,
+    HALWrapper,
+)
+
+__all__ = ["import_as", "install", "uninstall", "HALModuleProxy", "HALWrapper"]
