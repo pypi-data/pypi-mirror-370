b8b42d12e8d7a8e743358d85ea00bc06b66e4f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 07:09:19 +0000

    Add package idrive-e2-client/0.1.0/idrive_e2_client-0.1.0-py3-none-any.whl

---
+++ packages/idrive-e2-client/idrive_e2_client-0.1.0-py3-none-any.whl/idrive_e2/__init__.py
@@ -0,0 +1,9 @@
+from .client import IDriveE2Client
+from .errors import IDriveE2Error, InvalidAuth, CannotConnect
+
+__all__ = [
+    "IDriveE2Client",
+    "IDriveE2Error",
+    "InvalidAuth",
+    "CannotConnect",
+]
