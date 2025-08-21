3704351365c64dcffd2d250f05889c36ba14d18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:30 +0000

    Add package abstract-gui/0.1.63.66/abstract_gui-0.1.63.66.tar.gz

---
+++ packages/abstract-gui/abstract_gui-0.1.63.66.tar.gz/abstract_gui-0.1.63.66/src/abstract_gui/QT6/initFuncs.py
@@ -0,0 +1,11 @@
+
+from ..imports import *
+from .functions import ()
+
+def initFuncs(self):
+    try:
+        for f in ():
+            setattr(self, f.__name__, f)
+    except Exception as e:
+        logger.info(f"{e}")
+    return self
