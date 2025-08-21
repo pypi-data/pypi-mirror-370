8afd5245572cc00b3a8199ab8890d1d913dd4f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:07:06 +0000

    Add package ammanuel-mymath/0.1.1/ammanuel_mymath-0.1.1.tar.gz

---
+++ packages/ammanuel-mymath/ammanuel_mymath-0.1.1.tar.gz/ammanuel_mymath-0.1.1/mymath/__init__.py
@@ -0,0 +1,2 @@
+# Expose functions at the top-level package
+from .operations import add, subtract, multiply, divide
---
+++ packages/ammanuel-mymath/ammanuel_mymath-0.1.1.tar.gz/ammanuel_mymath-0.1.1/tests/test_ops.py
@@ -0,0 +1,13 @@
+import mymath
+
+def test_add():
+    assert mymath.add(2, 3) == 5
+
+def test_subtract():
+    assert mymath.subtract(5, 3) == 2
+
+def test_multiply():
+    assert mymath.multiply(5, 3) == 15
+
+def test_divide():
+    assert mymath.divide(6, 3) == 2
