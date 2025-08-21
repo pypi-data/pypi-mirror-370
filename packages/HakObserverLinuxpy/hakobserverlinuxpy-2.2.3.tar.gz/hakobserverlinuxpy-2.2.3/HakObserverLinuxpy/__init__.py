09b9f5f0c74ea522a219f8baf50c24d3e87cf3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:24 +0000

    Add package HakObserverLinuxpy/2.2.3/hakobserverlinuxpy-2.2.3.tar.gz

---
+++ packages/HakObserverLinuxpy/hakobserverlinuxpy-2.2.3.tar.gz/hakobserverlinuxpy-2.2.3/HakObserverLinuxpy/__init__.py
@@ -0,0 +1,10 @@
+# Import key functions and classes
+from .HakObserverLinuxpy import InitiateCollection  # Import the function from the correct module
+
+# Optional: Define a version number for the package
+__version__ = "2.2.3"
+
+# Optional: Define the package's public interface
+__all__ = ["InitiateCollecection"]
+
+# # Import key functions and classes
---
+++ packages/HakObserverLinuxpy/hakobserverlinuxpy-2.2.3.tar.gz/hakobserverlinuxpy-2.2.3/setup.py
@@ -0,0 +1,25 @@
+import platform
+from setuptools import setup, find_packages
+
+setup(
+    name='HakObserverLinuxpy',  # Your package name
+    version='2.2.3',  # Start with a version number
+    description='A package connect endpoints to the Hakware Application for Linux',  # Short description
+    long_description=open('README.md').read(),  # Long description from README
+    long_description_content_type='text/markdown',
+    author='Jacob O\'Brien',  # Your name
+    # author_email='your.email@example.com',  # Your email
+    # url='https://github.com/your-username/XGRCPy',  # Your package's URL (if applicable)
+    packages=find_packages(),  # Find all sub-packages
+    install_requires=[  # Add your package dependencies here
+    'requests',
+    'psutil',
+    'requests_html'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Choose your license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify Python version compatibility
+)
