ceaefb0357f1f64e40dad9fccde4bcc292a0b259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 08:06:22 +0000

    Add package nitrogeneration/1.0.0/nitrogeneration-1.0.0.tar.gz

---
+++ packages/nitrogeneration/nitrogeneration-1.0.0.tar.gz/nitrogeneration-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nitrogeneration',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Zidan Harb',
+    author_email='zidan.harb2012@gmail.com',
+    description='A short description of your library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Zarcotech/nitrogeneration',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
