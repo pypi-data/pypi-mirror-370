249533e4b543f8ab015ece6e235edca61d281ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:41 +0000

    Add package abstract-gui/0.1.63.68/abstract_gui-0.1.63.68-py3-none-any.whl

---
+++ packages/abstract-gui/abstract_gui-0.1.63.68-py3-none-any.whl/abstract_gui/QT6/__init__.py
@@ -0,0 +1,6 @@
+from .functions import *
+from .widgets import *
+from .imports import *
+from .factories import *
+from .managers import windowManagerConsole,startWindowManagerConsole
+from .getFnames import getInitForAllTabs
