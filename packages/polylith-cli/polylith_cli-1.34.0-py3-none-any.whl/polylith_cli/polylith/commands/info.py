d5e4c73e3f5f2c956f9da9343da5ea4605ee24c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:07:40 +0000

    Add package polylith-cli/1.34.0/polylith_cli-1.34.0-py3-none-any.whl

---
+++ packages/polylith-cli/polylith_cli-1.34.0-py3-none-any.whl/polylith_cli/polylith/commands/info.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from polylith_cli.polylith import configuration, info, repo
+
+def run(short: bool):
+    root = repo.get_workspace_root(Path.cwd())
+    ns = configuration.get_namespace_from_config(root)
+    bases = info.get_bases(root, ns)
+    components = info.get_components(root, ns)
+    projects_data = info.get_bricks_in_projects(root, components, bases, ns)
+    info.print_workspace_summary(projects_data, bases, components)
+    if not components and (not bases):
+        return
+    if short:
+        info.print_compressed_view_for_bricks_in_projects(projects_data, bases, components)
+    else:
+        info.print_bricks_in_projects(projects_data, bases, components)
