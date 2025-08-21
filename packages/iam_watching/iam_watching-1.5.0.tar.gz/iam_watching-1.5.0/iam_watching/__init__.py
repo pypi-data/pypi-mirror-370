7825f378d3d7ebe1ddf3d1b24ee6818a45a39411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 15:05:35 +0000

    Add package iam_watching/1.5.0/iam_watching-1.5.0.tar.gz

---
+++ packages/iam_watching/iam_watching-1.5.0.tar.gz/iam_watching-1.5.0/iam_watching/__init__.py
@@ -0,0 +1,104 @@
+import boto3
+import botocore
+import json
+import time
+import botocore.exceptions
+from datetime import datetime, timedelta, timezone
+
+__version__ = "1.5  .0"
+VERBOSE = False
+SLEEP_SECONDS = 5
+MAX_RESULTS = 100
+USER = ""
+
+
+def main() -> None:
+
+    global USER
+    uniqueset: set = set()
+
+    now = datetime.now(timezone.utc)
+    now_formatted = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
+
+    try:
+        sts: botocore.client.STS = boto3.client("sts")
+        identity: dict = sts.get_caller_identity()
+        client: botocore.client.CloudTrail = boto3.client("cloudtrail")
+
+        auth_type: dict = identity["Arn"].split(":")[2]
+
+        if USER == "":
+            if auth_type == "sts":
+                USER = identity["Arn"].split("/")[2]
+                print(f"""
+        Using sts identity: {identity["Arn"]}""")
+            if auth_type == "iam":
+                USER = identity["Arn"].split("/")[1]
+                print(f"""
+        Using iam identity: {identity["Arn"]}""")
+
+    except botocore.exceptions.NoCredentialsError:
+        print("No AWS credentials found.")
+    except botocore.exceptions.PartialCredentialsError:
+        print("Incomplete AWS credentials.")
+    except botocore.exceptions.ClientError as e:
+        print(f"Authentication failed: {e}")
+
+    print(f"""
+        Watching for security actions being performed by {USER}
+        Events can take up to 2 minutes to show up""")
+
+    print(f"""
+        Displaying unique actions only from:
+        {now_formatted}""")
+
+    print("""
+        Hit Ctrl+C to stop watching security events
+    """)
+
+    try:
+        while True:
+
+            # Filter for a single principal
+            response: boto3.client.lookup_events = client.lookup_events(
+                LookupAttributes=[
+                    {
+                        "AttributeKey": "Username",
+                        "AttributeValue": f"{USER}"
+                    }
+                ],
+                MaxResults=MAX_RESULTS,
+                StartTime=now_formatted,
+                EndTime=now + timedelta(days=1),
+            )
+
+            if VERBOSE:
+                print(
+                    json.dumps(response, indent=2, default=str)
+                )
+
+            # Filter out lookups as this script spams them
+            for event in response["Events"]:
+                if event["EventName"] != "LookupEvents":
+
+                    event_source: str = event['EventSource'].split(".")[0]
+
+                    action: str = f"{event_source}:{event['EventName']}"
+
+                    if action not in uniqueset:
+                        print(f"{event["EventTime"]} | {action}")
+
+                    uniqueset.add(action)
+
+            # Don't exceed the API call limit of 2 per second.
+            time.sleep(SLEEP_SECONDS)
+
+    except KeyboardInterrupt:
+        print(f"""
+        The following actions were recently
+        performed by {USER}:
+        """)
+
+        print(f"""
+\"Action\": {json.dumps(list(uniqueset), indent=2)}
+        """)
