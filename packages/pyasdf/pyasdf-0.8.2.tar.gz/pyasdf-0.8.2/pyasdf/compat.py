060ee70b8e5a07dabae56915e39dc5b4ec26e94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 18:05:56 +0000

    Add package pyasdf/0.8.2/pyasdf-0.8.2.tar.gz

---
+++ packages/pyasdf/pyasdf-0.8.2.tar.gz/pyasdf-0.8.2/pyasdf/compat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Compatibility between Python version.
+
+:copyright:
+    Lion Krischer (lion.krischer@gmail.com), 2013-2021
+:license:
+    BSD 3-Clause ("BSD New" or "BSD Simplified")
+"""
+
+string_types = (bytes, str)
+unicode_type = str
