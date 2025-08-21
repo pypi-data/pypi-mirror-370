c64f56523a1b712c081cbf5efbe73f519eae4392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 03:22:39 +0000

    Add package centering-lgram/1.0.15/centering_lgram-1.0.15-py3-none-any.whl

---
+++ packages/centering-lgram/centering_lgram-1.0.15-py3-none-any.whl/lgram/models/simple_language_model.py
@@ -0,0 +1,25 @@
+
+from . import centering_theory, transition_analyzer
+
+class Config:
+    pass
+
+class ModelInitializer:
+    pass
+
+class TextLoader:
+    pass
+
+class EnhancedLanguageModel:
+    pass
+
+def create_language_model(*args, **kwargs):
+    pass
+
+__all__ = [
+    'EnhancedLanguageModel',
+    'Config',
+    'ModelInitializer',
+    'TextLoader',
+    'create_language_model',
+]
