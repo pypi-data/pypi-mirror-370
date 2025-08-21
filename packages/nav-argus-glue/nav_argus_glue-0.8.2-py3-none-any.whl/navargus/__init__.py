cc3430167ecff499592c1c89c81bd2673a899088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:40 +0000

    Add package nav-argus-glue/0.8.2/nav_argus_glue-0.8.2-py3-none-any.whl

---
+++ packages/nav-argus-glue/nav_argus_glue-0.8.2-py3-none-any.whl/navargus/__init__.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2020, 2021 UNINETT
+# Copyright (C) 2022, 2024 Sikt
+#
+# This file is part of nav-argus-glue.
+#
+# nav-argus-glue is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with nav-argus-glue. If not, see <http://www.gnu.org/licenses/>.
+#
+"""NAV Event Engine -> Argus Exporter - AKA Argus glue service for NAV.
+
+Package definition. The main executable is contained in glue.py.
+"""
+
+__version__ = "0.8.2"
