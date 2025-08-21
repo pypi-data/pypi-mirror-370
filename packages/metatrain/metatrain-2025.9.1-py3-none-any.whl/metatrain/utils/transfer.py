f4660ff4752ef6c579be1cc304df00a3b858590b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:50 +0000

    Add package metatrain/2025.9.1/metatrain-2025.9.1-py3-none-any.whl

---
+++ packages/metatrain/metatrain-2025.9.1-py3-none-any.whl/metatrain/utils/transfer.py
@@ -0,0 +1,36 @@
+from typing import Dict, List, Optional
+
+import torch
+from metatensor.torch import TensorMap
+from metatomic.torch import System
+
+from . import torch_jit_script_unless_coverage
+
+
+@torch_jit_script_unless_coverage
+def batch_to(
+    systems: List[System],
+    targets: Dict[str, TensorMap],
+    extra_data: Optional[Dict[str, TensorMap]] = None,
+    dtype: Optional[torch.dtype] = None,
+    device: Optional[torch.device] = None,
+):
+    """
+    Changes the systems and targets to the specified floating point data type.
+
+    :param systems: List of systems.
+    :param targets: Dictionary of targets.
+    :param dtype: Desired floating point data type.
+    """
+
+    systems = [system.to(dtype=dtype, device=device) for system in systems]
+    targets = {
+        key: value.to(dtype=dtype, device=device) for key, value in targets.items()
+    }
+    if extra_data is not None:
+        extra_data = {
+            key: value.to(dtype=dtype, device=device)
+            for key, value in extra_data.items()
+        }
+
+    return systems, targets, extra_data
