ed1ebc2a40c55f507b59684edc71cfc4039c161d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 03:22:10 +0000

    Add package bluez-peripheral/0.2.0a4/bluez_peripheral-0.2.0a4-py3-none-any.whl

---
+++ packages/bluez-peripheral/bluez_peripheral-0.2.0a4-py3-none-any.whl/bluez_peripheral/advert.py
@@ -0,0 +1,229 @@
+from dbus_fast.aio import MessageBus
+from dbus_fast.aio.proxy_object import ProxyInterface
+from dbus_fast.constants import PropertyAccess
+from dbus_fast.service import ServiceInterface, method, dbus_property
+
+from enum import Enum, Flag, auto
+from typing import Collection, Dict, Union, Callable, Optional
+import struct
+from uuid import UUID
+
+from .uuid16 import UUID16
+from .util import *
+
+
+class PacketType(Enum):
+    BROADCAST = 0
+    """The relevant service(s) will be broadcast and do not require pairing.
+    """
+    PERIPHERAL = 1
+    """The relevant service(s) are associated with a peripheral role.
+    """
+
+
+class AdvertisingIncludes(Flag):
+    NONE = 0
+    TX_POWER = auto()
+    """Transmission power should be included.
+    """
+    APPEARANCE = auto()
+    """Device appearance number should be included.
+    """
+    LOCAL_NAME = auto()
+    """The local name of this device should be included.
+    """
+
+
+class Advertisement(ServiceInterface):
+    """
+    An advertisement for a particular service or collection of services that can be registered and broadcast to nearby devices.
+
+    Args:
+        localName: The device name to advertise.
+        serviceUUIDs: A list of service UUIDs advertise.
+        appearance: The appearance value to advertise.
+            `See the Bluetooth SIG recognised values. <https://specificationrefs.bluetooth.com/assigned-values/Appearance%20Values.pdf>`_
+        timeout: The time from registration until this advert is removed (defaults to zero meaning never timeout).
+        discoverable: Whether or not the device this advert should be generally discoverable.
+        packet_type: The type of advertising packet requested.
+        manufacturerData: Any manufacturer specific data to include in the advert.
+        solicitUUIDs: Array of service UUIDs to attempt to solicit (not widely used).
+        serviceData: Any service data elements to include.
+        includes: Fields that can be optionally included in the advertising packet.
+            Only the :class:`AdvertisingIncludes.TX_POWER` flag seems to work correctly with bluez.
+        duration: Duration of the advert when multiple adverts are ongoing.
+        releaseCallback: A function to call when the advert release function is called.
+    """
+
+    _INTERFACE = "org.bluez.LEAdvertisement1"
+    _MANAGER_INTERFACE = "org.bluez.LEAdvertisingManager1"
+
+    _defaultPathAdvertCount = 0
+
+    def __init__(
+        self,
+        localName: str,
+        serviceUUIDs: Collection[Union[str, bytes, UUID, UUID16, int]],
+        appearance: Union[int, bytes],
+        timeout: int = 0,
+        discoverable: bool = True,
+        packetType: PacketType = PacketType.PERIPHERAL,
+        manufacturerData: Dict[int, bytes] = {},
+        solicitUUIDs: Collection[Union[str, bytes, UUID, UUID16, int]] = [],
+        serviceData: Dict[Union[str, bytes, UUID, UUID16, int], bytes] = {},
+        includes: AdvertisingIncludes = AdvertisingIncludes.NONE,
+        duration: int = 2,
+        releaseCallback: Optional[Callable[[], None]] = None,
+    ):
+        self._type = packetType
+        # Convert any string uuids to uuid16.
+        self._serviceUUIDs = [
+            UUID16.parse_uuid(uuid) for uuid in serviceUUIDs
+        ]
+        self._localName = localName
+        # Convert the appearance to a uint16 if it isn't already an int.
+        self._appearance = (
+            appearance if type(appearance) is int else struct.unpack("H", appearance)[0]
+        )
+        self._timeout = timeout
+
+        self._manufacturerData = {}
+        for key, value in manufacturerData.items():
+            self._manufacturerData[key] = Variant("ay", value)
+
+        self._solicitUUIDs = [
+            UUID16.parse_uuid(uuid) for uuid in solicitUUIDs
+        ]
+       
+        self._serviceData = {}
+        for key, value in serviceData.items():
+            self._serviceData[key] = Variant("ay", value)
+
+        self._discoverable = discoverable
+        self._includes = includes
+        self._duration = duration
+        self.releaseCallback = releaseCallback
+
+        self._exportBus: Optional[MessageBus] = None
+        self._exportPath: Optional[str] = None
+        self._adapter: Optional[Adapter] = None
+
+        super().__init__(self._INTERFACE)
+
+    async def register(
+        self,
+        bus: MessageBus,
+        adapter: Adapter = None,
+        path: Optional[str] = None,
+    ):
+        """Register this advert with bluez to start advertising.
+
+        Args:
+            bus: The message bus used to communicate with bluez.
+            adapter: The adapter to use.
+            path: The dbus path to use for registration.
+        """
+        # Generate a unique path name for this advert if one isn't already given.
+        if path is None:
+            path = "/com/spacecheese/bluez_peripheral/advert" + str(
+                Advertisement._defaultPathAdvertCount
+            )
+            Advertisement._defaultPathAdvertCount += 1
+
+        self._exportBus = bus
+        self._exportPath = path
+
+        # Export this advert to the dbus.
+        bus.export(path, self)
+
+        if adapter is None:
+            adapter = await Adapter.get_first(bus)
+
+        self._adapter = adapter
+
+        # Get the LEAdvertisingManager1 interface for the target adapter.
+        interface = adapter._proxy.get_interface(self._MANAGER_INTERFACE)
+        await interface.call_register_advertisement(path, {})
+
+    async def unregister(self):
+        """
+        Unregister this advertisement from bluez to stop advertising.
+        """
+        if not self._exportBus or not self._adapter or not self._exportPath:
+            return
+
+        interface = self._adapter._proxy.get_interface(self._MANAGER_INTERFACE)
+
+        await interface.call_unregister_advertisement(self._exportPath)
+
+        self.Release()
+
+    @classmethod
+    async def GetSupportedIncludes(cls, adapter: Adapter) -> AdvertisingIncludes:
+        interface = adapter._proxy.get_interface(cls._MANAGER_INTERFACE)
+        includes = await interface.get_supported_includes()
+        flags = AdvertisingIncludes.NONE
+        for inc in includes:
+            inc = AdvertisingIncludes[kebab_to_shouting_snake(inc)]
+            # Combine all the included flags.
+            flags |= inc
+        return flags
+
+    @method()
+    def Release(self):  # type: ignore
+        self._exportBus.unexport(self._exportPath, self._INTERFACE)
+
+        self._exportBus = None
+        self._adapter = None
+        self._exportPath = None
+
+        if self.releaseCallback is not None:
+            self.releaseCallback()
+
+    @dbus_property(PropertyAccess.READ)
+    def Type(self) -> "s":  # type: ignore
+        return self._type.name.lower()
+
+    @dbus_property(PropertyAccess.READ)
+    def ServiceUUIDs(self) -> "as":  # type: ignore
+        return [str(id) for id in self._serviceUUIDs]
+
+    @dbus_property(PropertyAccess.READ)
+    def LocalName(self) -> "s":  # type: ignore
+        return self._localName
+
+    @dbus_property(PropertyAccess.READ)
+    def Appearance(self) -> "q":  # type: ignore
+        return self._appearance
+
+    @dbus_property(PropertyAccess.READ)
+    def Timeout(self) -> "q":  # type: ignore
+        return self._timeout
+
+    @dbus_property(PropertyAccess.READ)
+    def ManufacturerData(self) -> "a{qv}":  # type: ignore
+        return self._manufacturerData
+
+    @dbus_property(PropertyAccess.READ)
+    def SolicitUUIDs(self) -> "as":  # type: ignore
+        return [str(id) for id in self._solicitUUIDs]
+
+    @dbus_property(PropertyAccess.READ)
+    def ServiceData(self) -> "a{sv}":  # type: ignore
+        return dict((str(id), val) for id, val in self._serviceData.items())
+
+    @dbus_property(PropertyAccess.READ)
+    def Discoverable(self) -> "b":  # type: ignore
+        return self._discoverable
+
+    @dbus_property(PropertyAccess.READ)
+    def Includes(self) -> "as":  # type: ignore
+        return [
+            snake_to_kebab(inc.name)
+            for inc in AdvertisingIncludes
+            if self._includes & inc
+        ]
+
+    @dbus_property(PropertyAccess.READ)
+    def Duration(self) -> "q":  # type: ignore
+        return self._duration
---
+++ packages/bluez-peripheral/bluez_peripheral-0.2.0a4-py3-none-any.whl/bluez_peripheral/gatt/service.py
@@ -0,0 +1,270 @@
+from dbus_fast.aio.proxy_object import ProxyObject
+from dbus_fast.constants import PropertyAccess
+from dbus_fast.service import ServiceInterface, dbus_property
+from dbus_fast.aio import MessageBus
+
+from .characteristic import characteristic
+from ..uuid16 import UUID16
+from ..util import *
+
+from uuid import UUID
+from typing import Union
+import inspect
+
+# See https://git.kernel.org/pub/scm/bluetooth/bluez.git/tree/doc/gatt-api.txt
+class Service(ServiceInterface):
+    """Create a bluetooth service with the specified uuid.
+
+    Args:
+        uuid: The UUID of this service. A full list of recognized values is provided by the `Bluetooth SIG <https://btprodspecificationrefs.blob.core.windows.net/assigned-values/16-bit%20UUID%20Numbers%20Document.pdf>`_
+        primary: True if this service is a primary service (instead of a secondary service). False otherwise. Defaults to True.
+        includes: Any services to include in this service.
+            Services must be registered at the time Includes is read to be included.
+    """
+
+    _INTERFACE = "org.bluez.GattService1"
+
+    def _populate(self):
+        # Only interested in characteristic members.
+        members = inspect.getmembers(type(self), lambda m: type(m) is characteristic)
+
+        for _, member in members:
+            member._set_service(self)
+
+            # Some characteristics will occur multiple times due to different decorators.
+            if not member in self._characteristics:
+                self.add_characteristic(member)
+
+    def __init__(
+        self,
+        uuid: Union[str, bytes, UUID, UUID16, int],
+        primary: bool = True,
+        includes: Collection["Service"] = [],
+    ):
+        # Make sure uuid is a uuid16.
+        self._uuid = UUID16.parse_uuid(uuid)
+        self._primary = primary
+        self._characteristics = []
+        self._path = None
+        self._includes = includes
+        self._populate()
+
+        super().__init__(self._INTERFACE)
+
+    def is_registered(self) -> bool:
+        """Check if this service is registered with the bluez service manager.
+
+        Returns:
+            bool: True if the service is registered. False otherwise.
+        """
+        return not self._path is None
+
+    def add_characteristic(self, char: characteristic):
+        """Add the specified characteristic to this service declaration.
+
+        Args:
+            char: The characteristic to add.
+
+        Raises:
+            ValueError: Raised when the service is registered with the bluez service manager and thus cannot be modified.
+        """
+        if self.is_registered():
+            raise ValueError(
+                "Registered services cannot be modified. Please unregister the containing application."
+            )
+
+        self._characteristics.append(char)
+
+    def remove_characteristic(self, char: characteristic):
+        """Remove the specified characteristic from this service declaration.
+
+        Args:
+            char: The characteristic to remove.
+
+        Raises:
+            ValueError: Raised if the service is registered with the bluez service manager and thus cannot be modified.
+        """
+        if self.is_registered():
+            raise ValueError(
+                "Registered services cannot be modified. Please unregister the containing application."
+            )
+
+        self._characteristics.remove(char)
+
+    def _export(self, bus: MessageBus, path: str):
+        self._path = path
+
+        # Export this and number each child characteristic.
+        bus.export(path, self)
+        i = 0
+        for char in self._characteristics:
+            char._export(bus, path, i)
+            i += 1
+
+    def _unexport(self, bus: MessageBus):
+        # Unexport this and every child characteristic.
+        bus.unexport(self._path, self._INTERFACE)
+        for char in self._characteristics:
+            char._unexport(bus)
+
+        self._path = None
+
+    async def register(
+        self,
+        bus: MessageBus,
+        path: str = "/com/spacecheese/bluez_peripheral",
+        adapter: Adapter = None,
+    ):
+        """Register this service as a standalone service.
+        Using this multiple times will cause path conflicts.
+
+        Args:
+            bus: The bus to use when providing this service.
+            path: The base dbus path to export this service to.
+            adapter: The adapter that will provide this service or None to select the first adapter.
+        """
+        self._collection = ServiceCollection([self])
+        await self._collection.register(bus, path, adapter)
+
+    async def unregister(self):
+        """Unregister this service.
+        You may only use this if the service was registered using :class:`Service.register()`
+        """
+        collection = getattr(self, "_collection", None)
+
+        if collection is None:
+            return
+
+        await collection.unregister()
+
+    @dbus_property(PropertyAccess.READ)
+    def UUID(self) -> "s":  # type: ignore
+        return str(self._uuid)
+
+    @dbus_property(PropertyAccess.READ)
+    def Primary(self) -> "b":  # type: ignore
+        return self._primary
+
+    @dbus_property(PropertyAccess.READ)
+    def Includes(self) -> "ao":  # type: ignore
+        paths = []
+
+        for service in self._includes:
+            if not service._path is None:
+                paths.append(service._path)
+
+        paths.append(self._path)
+        return paths
+
+
+class ServiceCollection:
+    """A collection of services that are registered with the bluez GATT manager as a group."""
+
+    _MANAGER_INTERFACE = "org.bluez.GattManager1"
+
+    def _init(self, services: Collection[Service]):
+        self._path = None
+        self._adapter = None
+        self._services = services
+
+    def __init__(self, services: Collection[Service] = []):
+        """Create a service collection populated with the specified list of services.
+
+        Args:
+            services: The services to provide.
+        """
+        self._path = None
+        self._adapter = None
+        self._services = services
+
+    def add_service(self, service: Service):
+        """Add the specified service to this service collection.
+
+        Args:
+            service: The service to add.
+        """
+        if self.is_registered():
+            raise ValueError(
+                "You may not modify a registered service or service collection."
+            )
+
+        self._services.append(service)
+
+    def remove_service(self, service: Service):
+        """Remove the specified service from this collection.
+
+        Args:
+            service: The service to remove.
+        """
+        if self.is_registered():
+            raise ValueError(
+                "You may not modify a registered service or service collection."
+            )
+
+        self._services.remove(service)
+
+    async def _get_manager_interface(self):
+        return self._adapter._proxy.get_interface(self._MANAGER_INTERFACE)
+
+    def is_registered(self) -> bool:
+        """Check if this service collection is registered with the bluez service manager.
+
+        Returns:
+            True if the service is registered. False otherwise.
+        """
+        return not self._path is None
+
+    async def register(
+        self,
+        bus: MessageBus,
+        path: str = "/com/spacecheese/bluez_peripheral",
+        adapter: Adapter = None,
+    ):
+        """Register this collection of services with the bluez service manager.
+        Services and service collections that are registered may not be modified until they are unregistered.
+
+        Args:
+            bus: The bus to use for registration and management of this service.
+            path: The base dbus path to use when registering the collection.
+                Each service will be an automatically numbered child of this base.
+            adapter: The adapter that should be used to deliver the collection of services.
+        """
+        if self.is_registered():
+            return
+
+        self._bus = bus
+        self._path = path
+        self._adapter = await Adapter.get_first(bus) if adapter is None else adapter
+
+        manager = await self._get_manager_interface()
+
+        # Number and export each service.
+        i = 0
+        for service in self._services:
+            service._export(bus, self._path + "/service{:d}".format(i))
+            i += 1
+
+        class EmptyServiceInterface(ServiceInterface):
+            pass
+
+        # Export an empty interface on the root path so that bluez has an object manager to find.
+        bus.export(self._path, EmptyServiceInterface(self._path.replace("/", ".")[1:]))
+        await manager.call_register_application(self._path, {})
+
+    async def unregister(self):
+        """Unregister this service using the bluez service manager."""
+        if not self.is_registered():
+            return
+
+        manager = await self._get_manager_interface()
+
+        await manager.call_unregister_application(self._path)
+
+        for service in self._services:
+            service._unexport(self._bus)
+        # Unexport the root object manager.
+        self._bus.unexport(self._path)
+
+        self._path = None
+        self._adapter = None
+        self._bus = None
