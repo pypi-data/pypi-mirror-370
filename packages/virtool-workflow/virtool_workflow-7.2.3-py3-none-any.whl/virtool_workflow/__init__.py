4b69000181ced52a55e4e12e970172fdc6a72b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 07:08:48 +0000

    Add package virtool-workflow/7.2.3/virtool_workflow-7.2.3-py3-none-any.whl

---
+++ packages/virtool-workflow/virtool_workflow-7.2.3-py3-none-any.whl/virtool_workflow/__init__.py
@@ -0,0 +1,14 @@
+"""
+A framework for defining Virtool workflows.
+"""
+
+from virtool_workflow.decorators import step
+from virtool_workflow.runtime.run_subprocess import RunSubprocess
+from virtool_workflow.workflow import Workflow, WorkflowStep
+
+__all__ = [
+    "step",
+    "RunSubprocess",
+    "Workflow",
+    "WorkflowStep",
+]
---
+++ packages/virtool-workflow/virtool_workflow-7.2.3-py3-none-any.whl/virtool_workflow/cli.py
@@ -0,0 +1,67 @@
+"""Command Line Interface to virtool_workflow"""
+
+import asyncio
+from pathlib import Path
+
+import click
+
+from virtool_workflow.runtime.run import start_runtime
+
+
+@click.option(
+    "--dev",
+    help="Run in development mode.",
+    is_flag=True,
+)
+@click.option(
+    "--jobs-api-connection-string",
+    help="The URL of the jobs API.",
+    default="https://localhost:9950",
+)
+@click.option(
+    "--mem",
+    help="The amount of memory to use in GB.",
+    type=int,
+    default=8,
+)
+@click.option(
+    "--proc",
+    help="The number of processes to use.",
+    type=int,
+    default=2,
+)
+@click.option(
+    "--redis-connection-string",
+    help="The URL for connecting to Redis.",
+    default="redis://localhost:6317",
+)
+@click.option(
+    "--redis-list-name",
+    help="The name of the Redis list to watch for incoming jobs.",
+    required=True,
+)
+@click.option(
+    "--sentry-dsn",
+    help="A Sentry DSN. Sentry will not be configured if no value is provided.",
+    default=None,
+)
+@click.option(
+    "--timeout",
+    help="Maximum time to wait for an incoming job",
+    default=1000,
+)
+@click.option(
+    "--work-path",
+    default="temp",
+    help="The path where temporary files will be stored.",
+    type=click.Path(path_type=Path),
+)
+@click.command()
+def run_workflow(**kwargs):
+    """Run a workflow."""
+    asyncio.run(start_runtime(**kwargs))
+
+
+def cli_main():
+    """Main pip entrypoint."""
+    run_workflow(auto_envvar_prefix="VT")
