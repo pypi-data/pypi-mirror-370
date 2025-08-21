c280187389fb86238be9ee6b40f310217b73fba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:47 +0000

    Add package apipeline/0.2.2/apipeline-0.2.2.tar.gz

---
+++ packages/apipeline/apipeline-0.2.2.tar.gz/apipeline-0.2.2/apipeline/processors/filters/null_filter.py
@@ -0,0 +1,9 @@
+
+from apipeline.processors.frame_processor import FrameProcessor
+
+
+class NullFilter(FrameProcessor):
+    """This filter doesn't allow passing any frames up or downstream."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
