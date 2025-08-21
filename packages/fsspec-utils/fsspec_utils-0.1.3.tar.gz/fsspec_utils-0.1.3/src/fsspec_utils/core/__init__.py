1fdbaa171a064c2de27e18ada49a20c54781f53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 03:22:49 +0000

    Add package fsspec-utils/0.1.3/fsspec_utils-0.1.3.tar.gz

---
+++ packages/fsspec-utils/fsspec_utils-0.1.3.tar.gz/fsspec_utils-0.1.3/src/fsspec_utils/core/__init__.py
@@ -0,0 +1,22 @@
+"""Core filesystem functionality for fsspec-utils."""
+
+from .base import (
+    GitLabFileSystem,
+    MonitoredSimpleCacheFileSystem,
+    filesystem,
+    get_filesystem,
+)
+
+# Conditional imports for extended functionality
+try:
+    from .ext import AbstractFileSystem
+except ImportError:
+    from fsspec import AbstractFileSystem
+
+__all__ = [
+    "GitLabFileSystem",
+    "MonitoredSimpleCacheFileSystem",
+    "AbstractFileSystem",
+    "filesystem",
+    "get_filesystem",
+]
