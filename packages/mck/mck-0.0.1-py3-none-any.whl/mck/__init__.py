3eb4701e688732d4d951fef1cdb1ef5c035bbef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:08:15 +0000

    Add package mck/0.0.1/mck-0.0.1-py3-none-any.whl

---
+++ packages/mck/mck-0.0.1-py3-none-any.whl/mck/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2025-present Maaruuuf <abc.maruf12@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from .add_numbers import add_num
+
+__all__ = ["add_num"]
---
+++ packages/mck/mck-0.0.1-py3-none-any.whl/mck/add_numbers.py
@@ -0,0 +1,13 @@
+"""this is a module that adds two numbers together"""
+
+def add_num(a:int, b:int) -> int:
+    """Function that add two int and return a int
+
+    Args:
+        a (int): the first input int
+        b (int): the second input int
+
+    Returns:
+        int: the sum of inputs a and b
+    """    
+    return a + b 
