775a4a9044109be175f021ee23a06a23a4f0747a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:26 +0000

    Add package nolano/0.0.1/nolano-0.0.1.tar.gz

---
+++ packages/nolano/nolano-0.0.1.tar.gz/nolano-0.0.1/tests/test_placeholder.py
@@ -0,0 +1,8 @@
+"""
+Placeholder test file to ensure pytest can discover tests.
+This file will be replaced with actual tests later.
+"""
+
+def test_placeholder():
+    """Placeholder test that always passes."""
+    assert True 
