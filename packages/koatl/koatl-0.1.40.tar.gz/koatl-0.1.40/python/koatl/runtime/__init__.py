68a9f824e3b6cbcc78464581fba79d04eff8ccbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:40 +0000

    Add package koatl/0.1.40/koatl-0.1.40.tar.gz

---
+++ packages/koatl/koatl-0.1.40.tar.gz/koatl-0.1.40/python/koatl/runtime/__init__.py
@@ -0,0 +1,47 @@
+import functools
+import importlib
+from types import SimpleNamespace
+
+from koatl.runtime.record import Record
+from . import meta_finder, vattr
+
+meta_finder.install_hook()
+
+__all__ = ["__tl__"]
+
+
+def set_exports(package_name, globals_dict, exports, module_star_exports):
+    exports = set(exports)
+
+    for module in module_star_exports:
+        mod = importlib.import_module(module, package_name)
+
+        if hasattr(mod, "__all__"):
+            for name in mod.__all__:
+                exports.add(name)
+        else:
+            for name in dir(mod):
+                if name.startswith("_"):
+                    continue
+
+                exports.add(name)
+
+    if "__all__" not in globals_dict:
+        globals_dict["__all__"] = ()
+
+    globals_dict["__all__"] = tuple(set(globals_dict["__all__"]) | exports)
+
+
+__tl__ = SimpleNamespace(
+    Exception=Exception,
+    slice=slice,
+    type=type,
+    partial=functools.partial,
+    #
+    record_literal=Record.from_dict_ref,
+    #
+    set_exports=set_exports,
+    #
+    vget=vattr.vget,
+    vhas=vattr.vhas,
+)
