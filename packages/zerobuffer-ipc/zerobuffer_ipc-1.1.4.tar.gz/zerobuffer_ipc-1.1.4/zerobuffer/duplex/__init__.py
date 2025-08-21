6e77a428a349d13b92c641577f5cbfe72932a909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:06:58 +0000

    Add package zerobuffer-ipc/1.1.4/zerobuffer_ipc-1.1.4.tar.gz

---
+++ packages/zerobuffer-ipc/zerobuffer_ipc-1.1.4.tar.gz/zerobuffer_ipc-1.1.4/zerobuffer/duplex/__init__.py
@@ -0,0 +1,33 @@
+"""
+ZeroBuffer Duplex Channel implementation
+
+Provides request-response communication patterns on top of ZeroBuffer.
+"""
+
+from .interfaces import (
+    IDuplexClient,
+    IDuplexServer,
+    IImmutableDuplexServer,
+    IMutableDuplexServer,
+    IDuplexChannelFactory,
+    DuplexResponse
+)
+
+from .factory import DuplexChannelFactory
+from .client import DuplexClient
+from .server import ImmutableDuplexServer, MutableDuplexServer
+from .processing_mode import ProcessingMode
+
+__all__ = [
+    'IDuplexClient',
+    'IDuplexServer', 
+    'IImmutableDuplexServer',
+    'IMutableDuplexServer',
+    'IDuplexChannelFactory',
+    'DuplexResponse',
+    'DuplexChannelFactory',
+    'DuplexClient',
+    'ImmutableDuplexServer',
+    'MutableDuplexServer',
+    'ProcessingMode'
+]
