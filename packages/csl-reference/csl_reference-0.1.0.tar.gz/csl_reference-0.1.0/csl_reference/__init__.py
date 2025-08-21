586c1c0fd8a0dc550915230896e2d2730fe4a838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:07:40 +0000

    Add package csl-reference/0.1.0/csl_reference-0.1.0.tar.gz

---
+++ packages/csl-reference/csl_reference-0.1.0.tar.gz/csl_reference-0.1.0/csl_reference/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025-present The CSL-Reference Team <https://github.com/Fusion-Power-Plant-Framework/csl-reference>
+#
+# SPDX-License-Identifier: MIT
+"""Package to represent references."""
+
+from csl_reference._reference import DateVariable, NameVariable, Reference, ReferenceType
+
+__all__ = ["DateVariable", "NameVariable", "Reference", "ReferenceType"]
