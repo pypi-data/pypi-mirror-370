c354c4ae0dcb809c6ef0fcfaeec81f791b0b2c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:06:53 +0000

    Add package lionagi/0.15.8/lionagi-0.15.8-py3-none-any.whl

---
+++ packages/lionagi/lionagi-0.15.8-py3-none-any.whl/lionagi/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+
+from pydantic import BaseModel, Field
+
+from . import _types as types
+from .operations import Operation
+from .operations import OperationGraphBuilder as Builder
+from .operations import brainstorm, flow, plan
+from .service.imodel import iModel
+from .session.session import Branch, Session
+from .version import __version__
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+__all__ = (
+    "Session",
+    "Branch",
+    "iModel",
+    "types",
+    "__version__",
+    "BaseModel",
+    "Field",
+    "logger",
+    "Builder",
+    "Operation",
+    "brainstorm",
+    "flow",
+    "plan",
+)
