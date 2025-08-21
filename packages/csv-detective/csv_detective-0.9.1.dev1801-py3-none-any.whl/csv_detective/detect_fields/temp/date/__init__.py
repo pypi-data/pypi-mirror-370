52794dc2cdd078f6f29e1cd91bf486001abf65d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:27 +0000

    Add package csv-detective/0.9.1.dev1801/csv_detective-0.9.1.dev1801-py3-none-any.whl

---
+++ packages/csv-detective/csv_detective-0.9.1.dev1801-py3-none-any.whl/csv_detective/detect_fields/temp/date/__init__.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Optional
+
+from dateparser import parse as date_parser
+from dateutil.parser import ParserError
+from dateutil.parser import parse as dateutil_parser
+
+PROPORTION = 1
+# /!\ this is only for dates, not datetimes which are handled by other utils
+
+
+def date_casting(val: str) -> Optional[datetime]:
+    """For performance reasons, we try first with dateutil and fallback on dateparser"""
+    try:
+        return dateutil_parser(val)
+    except ParserError:
+        return date_parser(val)
+    except Exception:
+        return None
+
+
+threshold = 0.3
+
+
+def _is(val):
+    """Renvoie True si val peut être une date, False sinon"""
+    # early stops, to cut processing time
+    if not isinstance(val, str) or len(val) > 20 or len(val) < 8:
+        return False
+    if sum([char.isdigit() for char in val]) / len(val) < threshold:
+        return False
+    res = date_casting(val)
+    if not res or res.hour or res.minute or res.second:
+        return False
+    return True
---
+++ packages/csv-detective/csv_detective-0.9.1.dev1801-py3-none-any.whl/csv_detective/detect_fields/temp/datetime_naive/__init__.py
@@ -0,0 +1,23 @@
+from typing import Any, Optional
+
+from csv_detective.detect_fields.temp.date import date_casting
+
+PROPORTION = 1
+
+
+def _is(val: Optional[Any]) -> bool:
+    """Detects naive datetimes only"""
+    # early stops, to cut processing time
+    # 15 is the minimal length of a datetime format YYMMDDTHH:MM:SS
+    # 26 is the maximal length of an ISO datetime format YYYY-MM-DDTHH:MM:SS.dddddd, keeping some slack
+    if not isinstance(val, str) or len(val) > 30 or len(val) < 15:
+        return False
+    threshold = 0.7
+    if sum([char.isdigit() or char in {"-", "/", ":", " "} for char in val]) / len(val) < threshold:
+        return False
+    res = date_casting(val)
+    return (
+        res is not None
+        and bool(res.hour or res.minute or res.second or res.microsecond)
+        and not bool(res.tzinfo)
+    )
