cf7c4d918e8b40c60faddd2901890b059b795cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:07:49 +0000

    Add package QUARK-framework/0.4.2/quark_framework-0.4.2.tar.gz

---
+++ packages/QUARK-framework/quark_framework-0.4.2.tar.gz/quark_framework-0.4.2/src/quark/interface_types/__init__.py
@@ -0,0 +1,6 @@
+from quark.interface_types.graph import Graph
+from quark.interface_types.ising import Ising
+from quark.interface_types.other import Other
+from quark.interface_types.qubo import Qubo
+
+InterfaceType = Graph | Ising | Other | Qubo | None
