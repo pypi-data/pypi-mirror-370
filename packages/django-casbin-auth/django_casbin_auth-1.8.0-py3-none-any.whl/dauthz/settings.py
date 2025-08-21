23510d21e5dc0b95ff1c461ef9421b49763a8d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 06:06:42 +0000

    Add package django-casbin-auth/1.8.0/django_casbin_auth-1.8.0-py3-none-any.whl

---
+++ packages/django-casbin-auth/django_casbin_auth-1.8.0-py3-none-any.whl/dauthz/settings.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+# Dauthz
+DAUTHZ = {
+    # DEFAULT Dauthz enforcer
+    "DEFAULT": {
+        # Casbin model setting.
+        "MODEL": {
+            # Available Settings: "file", "text"
+            "CONFIG_TYPE": "file",
+            "CONFIG_FILE_PATH": Path(__file__).parent.joinpath("dauthz-model.conf"),
+            "CONFIG_TEXT": "",
+        },
+        # Casbin adapter .
+        "ADAPTER": {
+            "NAME": "casbin_adapter.adapter.Adapter",
+            # 'OPTION_1': '',
+        },
+        "LOG": {
+            # Changes whether Dauthz will log messages to the Logger.
+            "ENABLED": False,
+        },
+        "CACHE": {},
+    },
+}
+
+AUTHENTICATION_BACKENDS = [
+    "dauthz.backends.CasbinBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
