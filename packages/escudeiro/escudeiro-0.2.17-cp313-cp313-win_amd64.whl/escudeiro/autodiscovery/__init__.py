18a5ae5c65c1940dc6dbb611f102be7526a25293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:06:57 +0000

    Add package escudeiro/0.2.17/escudeiro-0.2.17-cp313-cp313-win_amd64.whl

---
+++ packages/escudeiro/escudeiro-0.2.17-cp313-cp313-win_amd64.whl/escudeiro/autodiscovery/__init__.py
@@ -0,0 +1,33 @@
+from .base import PathConverter, make_modulename_converter, smart_import
+from .runtime import (
+    RuntimeAutoDiscovery,
+    runtime_attr_with_value,
+    runtime_child_of,
+    runtime_contains_attr,
+    runtime_instance_of,
+)
+from .static import (
+    StaticAutoDiscovery,
+    static_autoload_validator,
+    static_chain_validate,
+    static_child_of,
+    static_instance_of,
+    static_modulename_validator,
+)
+
+__all__ = [
+    "StaticAutoDiscovery",
+    "static_autoload_validator",
+    "static_chain_validate",
+    "static_child_of",
+    "static_instance_of",
+    "static_modulename_validator",
+    "RuntimeAutoDiscovery",
+    "runtime_attr_with_value",
+    "runtime_child_of",
+    "runtime_contains_attr",
+    "runtime_instance_of",
+    "PathConverter",
+    "make_modulename_converter",
+    "smart_import",
+]
