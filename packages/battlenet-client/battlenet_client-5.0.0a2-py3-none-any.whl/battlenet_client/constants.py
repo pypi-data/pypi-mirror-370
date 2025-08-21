2d16dced025542344497e384e6b7c52ec1a590ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:43 +0000

    Add package battlenet-client/5.0.0a2/battlenet_client-5.0.0a2-py3-none-any.whl

---
+++ packages/battlenet-client/battlenet_client-5.0.0a2-py3-none-any.whl/battlenet_client/constants.py
@@ -0,0 +1,17 @@
+"""Defines the constants used with the package
+
+.. moduleauthor: David "Gahd" Couples <gahdania@gahd.io>
+"""
+
+UNITED_STATES = 'us'
+EUROPE = 'eu'
+TAIWAN = 'tw'
+KOREA = 'kr'
+CHINA = 'cn'
+
+WOW = {'abbrev': 'wow', 'name': 'World of Warcraft'}
+D3 = {'abbrev': 'd3', 'name': 'Diablo III'}
+SC2 = {'abbrev': 'sc2', 'name': 'Starcraft II'}
+HS = {'abbrev': 'hs', 'name': 'Heartsthone'}
+OW = {'abbrev': 'ow', 'name': 'Overwatch'}
+BNET = {'abbrev': 'bnet', 'name': 'Battle.net Data'}
