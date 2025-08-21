df603dca917ea841ec200d4e5e7756959965d2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:08:29 +0000

    Add package link-shortly/0.0.2/link_shortly-0.0.2.tar.gz

---
+++ packages/link-shortly/link_shortly-0.0.2.tar.gz/link_shortly-0.0.2/src/shortly/shortly.py
@@ -0,0 +1,41 @@
+"""
+Link-Shortly - A simple URL shortening library.
+
+@author:   RknDeveloper
+@contact:  https://t.me/RknDeveloperr
+@license:  MIT License, see LICENSE file
+
+Copyright (c) 2025-present RknDeveloper
+"""
+
+from .utils import convert
+import requests
+
+class Shortly:
+    def __init__(self, api_key: str, base_url: str):
+        """
+        Initialize Shortly instance.
+        
+        Input:
+            api_key (str)  -> API key for authentication
+            base_url (str) -> Base API URL of the shortening service
+        
+        Output:
+            Stores api_key and base_url in the object
+        """
+        self.api_key = api_key
+        self.base_url = base_url   # fixed (was base_site)
+
+    def convert(self, link: str, alias=None, timeout=10):
+        """
+        Convert a long link into a short one using alias.
+
+        Input:
+            link (str)    -> The long URL to shorten
+            alias (str)   -> Custom alias for the shortened URL
+            timeout (int) -> Request timeout in seconds (default: 10)
+
+        Output:
+            Returns shortened link or error response from utils.convert
+        """
+        return convert(self, link, alias, timeout)
