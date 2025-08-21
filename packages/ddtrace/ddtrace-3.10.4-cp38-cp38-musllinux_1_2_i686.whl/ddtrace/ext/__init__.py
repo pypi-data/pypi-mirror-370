7fc56ad32043f9266365752786b0b47f1691c583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:08:51 +0000

    Add package ddtrace/3.10.4/ddtrace-3.10.4-cp38-cp38-musllinux_1_2_i686.whl

---
+++ packages/ddtrace/ddtrace-3.10.4-cp38-cp38-musllinux_1_2_i686.whl/ddtrace/ext/__init__.py
@@ -0,0 +1,42 @@
+class SpanTypes(object):
+    CACHE = "cache"
+    CASSANDRA = "cassandra"
+    ELASTICSEARCH = "elasticsearch"
+    GRPC = "grpc"
+    GRAPHQL = "graphql"
+    HTTP = "http"
+    MONGODB = "mongodb"
+    REDIS = "redis"
+    SERVERLESS = "serverless"
+    SQL = "sql"
+    TEMPLATE = "template"
+    TEST = "test"
+    WEB = "web"
+    WORKER = "worker"
+    AUTH = "auth"
+    SYSTEM = "system"
+    LLM = "llm"
+    VALKEY = "valkey"
+
+
+class SpanKind(object):
+    CLIENT = "client"
+    SERVER = "server"
+    PRODUCER = "producer"
+    CONSUMER = "consumer"
+    INTERNAL = "internal"
+
+
+EXIT_SPAN_TYPES = frozenset(
+    {
+        SpanTypes.CACHE,
+        SpanTypes.CASSANDRA,
+        SpanTypes.ELASTICSEARCH,
+        SpanTypes.GRPC,
+        SpanTypes.HTTP,
+        SpanTypes.REDIS,
+        SpanTypes.SQL,
+        SpanTypes.WORKER,
+        SpanTypes.VALKEY,
+    }
+)
