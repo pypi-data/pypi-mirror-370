8e0e43942ee138daccc7c2bafe63127c26da46e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 15:05:21 +0000

    Add package HakObserverLinuxpy/2.2.1/hakobserverlinuxpy-2.2.1.tar.gz

---
+++ packages/HakObserverLinuxpy/hakobserverlinuxpy-2.2.1.tar.gz/hakobserverlinuxpy-2.2.1/HakObserverLinuxpy/__init__.py
@@ -0,0 +1,9 @@
+# Import key functions and classes
+from .HakObserverLinuxpy import HakObserverLinuxpy
+from .HakObserverLinuxpy import InitiateCollection
+
+# Optional: Define a version number for the package
+__version__ = "2.2.1"
+
+# Optional: Define the package's public interface
+__all__ = ["HakObserverLinuxpy", "InitiateCollection"]
---
+++ packages/HakObserverLinuxpy/hakobserverlinuxpy-2.2.1.tar.gz/hakobserverlinuxpy-2.2.1/README.md
@@ -0,0 +1,10 @@
+# Hakware-py
+
+HakObserver Linux is a Python package that allows endpoints to connect to the Hakware solution
+
+## Installation
+
+To install HakObserverLinuxpy, ensure you have Python installed (version 3.6 or higher), then use pip to install the package:
+
+```bash
+pip install HakObserverLinuxpy
