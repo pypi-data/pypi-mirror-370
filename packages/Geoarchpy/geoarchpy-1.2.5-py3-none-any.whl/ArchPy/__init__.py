8a6c41a29b32c49c3a7d663536a8ca5c653e596a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:06:30 +0000

    Add package Geoarchpy/1.2.5/geoarchpy-1.2.5-py3-none-any.whl

---
+++ packages/Geoarchpy/geoarchpy-1.2.5-py3-none-any.whl/ArchPy/__init__.py
@@ -0,0 +1,14 @@
+# __version__ = '1.0.5'
+
+from . import base
+from . import data_transfo
+from . import ineq
+from . import infer
+from . import inputs
+from . import x_valid
+from . import _version
+from . import ap_mf
+from . import uppy
+from . import automatic_pile
+
+__version__ = _version.__version__
