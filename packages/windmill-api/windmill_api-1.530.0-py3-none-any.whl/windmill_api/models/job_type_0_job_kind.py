d12637be20ae08081d0871ab1f0ae77e24b669a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 07:10:34 +0000

    Add package windmill-api/1.530.0/windmill_api-1.530.0-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.530.0-py3-none-any.whl/windmill_api/models/job_type_0_job_kind.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class JobType0JobKind(str, Enum):
+    APPDEPENDENCIES = "appdependencies"
+    APPSCRIPT = "appscript"
+    DEPENDENCIES = "dependencies"
+    DEPLOYMENTCALLBACK = "deploymentcallback"
+    FLOW = "flow"
+    FLOWDEPENDENCIES = "flowdependencies"
+    FLOWNODE = "flownode"
+    FLOWPREVIEW = "flowpreview"
+    FLOWSCRIPT = "flowscript"
+    IDENTITY = "identity"
+    PREVIEW = "preview"
+    SCRIPT = "script"
+    SCRIPT_HUB = "script_hub"
+    SINGLESCRIPTFLOW = "singlescriptflow"
+
+    def __str__(self) -> str:
+        return str(self.value)
