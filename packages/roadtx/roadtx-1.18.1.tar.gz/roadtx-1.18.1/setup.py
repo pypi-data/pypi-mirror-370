6a98ff5257d7ec4e34e5398f36da9b0163914782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:50 +0000

    Add package roadtx/1.18.1/roadtx-1.18.1.tar.gz

---
+++ packages/roadtx/roadtx-1.18.1.tar.gz/roadtx-1.18.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+setup(name='roadtx',
+      version='1.18.1',
+      description='ROADtools Token eXchange',
+      author='Dirk-jan Mollema',
+      author_email='dirkjan@outsidersecurity.nl',
+      url='https://github.com/dirkjanm/ROADtools/',
+      license='MIT',
+      classifiers=[
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+      ],
+      packages=['roadtools.roadtx'],
+      package_data={'roadtools.roadtx': ['firstpartyscopes.json']},
+      install_requires=[
+          'roadlib>=1.4',
+          'requests',
+          'selenium',
+          'selenium-wire',
+          'pyotp',
+          'pycryptodomex',
+          'signxml>3',
+          'setuptools',
+          'blinker<1.8.0'
+      ],
+      zip_safe=False,
+      include_package_data=True,
+      entry_points={
+          'console_scripts': ['roadtx=roadtools.roadtx.main:main',]
+      }
+      )
