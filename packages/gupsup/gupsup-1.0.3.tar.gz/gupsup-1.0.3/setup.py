c42541ab2ea11be5de9d352bb31b72341adece1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:07:03 +0000

    Add package gupsup/1.0.3/gupsup-1.0.3.tar.gz

---
+++ packages/gupsup/gupsup-1.0.3.tar.gz/gupsup-1.0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="terminalchat",
+    version="1.0.2",
+    author="Rahul Deka",
+    author_email="rahuldeka072@gmail.com",
+    description="Terminal-based chat app using WebSockets with improved error handling and reconnection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamRahul21/terminalchat",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "websockets>=11.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'terminalchat=terminalchat.client:run_client',
+        ],
+    },
+    keywords="chat terminal websocket real-time",
+)
