b31cd2de008ee52ef5b3405aa86538d61334a749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 03:22:56 +0000

    Add package writer-sdk/2.3.1/writer_sdk-2.3.1.tar.gz

---
+++ packages/writer-sdk/writer_sdk-2.3.1.tar.gz/writer_sdk-2.3.1/src/writerai/types/graph_question_params.py
@@ -0,0 +1,40 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Union
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["GraphQuestionParamsBase", "GraphQuestionParamsNonStreaming", "GraphQuestionParamsStreaming"]
+
+
+class GraphQuestionParamsBase(TypedDict, total=False):
+    graph_ids: Required[List[str]]
+    """The unique identifiers of the Knowledge Graphs to query."""
+
+    question: Required[str]
+    """The question to answer using the Knowledge Graph."""
+
+    subqueries: bool
+    """Specify whether to include subqueries."""
+
+
+class GraphQuestionParamsNonStreaming(GraphQuestionParamsBase, total=False):
+    stream: Literal[False]
+    """Determines whether the model's output should be streamed.
+
+    If true, the output is generated and sent incrementally, which can be useful for
+    real-time applications.
+    """
+
+
+class GraphQuestionParamsStreaming(GraphQuestionParamsBase):
+    stream: Required[Literal[True]]
+    """Determines whether the model's output should be streamed.
+
+    If true, the output is generated and sent incrementally, which can be useful for
+    real-time applications.
+    """
+
+
+GraphQuestionParams = Union[GraphQuestionParamsNonStreaming, GraphQuestionParamsStreaming]
