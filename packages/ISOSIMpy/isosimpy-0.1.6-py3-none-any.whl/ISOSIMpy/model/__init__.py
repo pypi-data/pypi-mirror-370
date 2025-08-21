41553d113b461449cecfd4b8063272126512e3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:50 +0000

    Add package ISOSIMpy/0.1.6/isosimpy-0.1.6-py3-none-any.whl

---
+++ packages/ISOSIMpy/isosimpy-0.1.6-py3-none-any.whl/ISOSIMpy/model/__init__.py
@@ -0,0 +1,11 @@
+from .model import Model
+from .solver import Solver
+from .units import EPMUnit, PMUnit, Unit
+
+__all__ = [
+    "Model",
+    "Solver",
+    "Unit",
+    "EPMUnit",
+    "PMUnit",
+]
