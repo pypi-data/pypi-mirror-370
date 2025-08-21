c1edd543db4fd3d778417de5995da75a0cfbd195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 08:06:20 +0000

    Add package lcn/0.0.1/lcn-0.0.1-py3-none-any.whl

---
+++ packages/lcn/lcn-0.0.1-py3-none-any.whl/lcn/__init__.py
@@ -0,0 +1,16 @@
+"""
+Learning Compositional Negation (LCN) Library
+
+A research library for studying compositional negation in neural networks.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Adrian Brasoveanu"
+__email__ = "abrsvn@gmail.com"
+
+def hello_lcn():
+    """A simple stub function to test the package."""
+    return "Hello from Learning Compositional Negation!"
+
+# Package is currently a stub for name reservation
+# More functionality will be added in future versions
