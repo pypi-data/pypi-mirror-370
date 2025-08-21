6874f8d478cb5e7db3e5532172475728b4b05d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 07:10:39 +0000

    Add package dz-locations/0.1.2/dz_locations-0.1.2.tar.gz

---
+++ packages/dz-locations/dz_locations-0.1.2.tar.gz/dz_locations-0.1.2/dz_locations/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Wilaya, Commune
+
+@admin.register(Wilaya)
+class WilayaAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'code']
+    list_display = ['name', 'code' , 'ar_name']
+
+@admin.register(Commune)
+class CommuneAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'post_code']
+    list_display = ['name', 'ar_name' ,'post_code', 'wilaya']
+    autocomplete_fields = ['wilaya']
