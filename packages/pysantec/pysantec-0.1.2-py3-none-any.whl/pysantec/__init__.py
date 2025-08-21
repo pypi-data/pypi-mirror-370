e78e293a41db7a0f962e0439b0e08cd2a6951087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 15:05:22 +0000

    Add package pysantec/0.1.2/pysantec-0.1.2-py3-none-any.whl

---
+++ packages/pysantec/pysantec-0.1.2-py3-none-any.whl/pysantec/__init__.py
@@ -0,0 +1,27 @@
+# pysantec/__init__.py
+
+"""
+PySantec - Python Package for Santec Insertion Loss
+and Polarization Dependent Loss Swept Test System.
+"""
+
+from .logger import get_logger
+from .drivers import load_dlls
+
+logger = get_logger(__name__)
+
+try:
+    # Initialize and Load the Santec DLLs
+    setup_dlls_result = load_dlls()
+    logger.info("Santec DLLs loaded successfully.")
+except Exception as e:
+    logger.error("Error while Santec DLLs: ", str(e))
+    raise
+
+
+from .instruments.instrument_manager import InstrumentManager
+
+
+__all__ = [
+    "InstrumentManager",
+]
