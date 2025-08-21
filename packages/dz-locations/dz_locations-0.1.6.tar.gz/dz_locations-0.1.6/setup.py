01eecade5ef92383e091b6bbdc4c075248e30aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 08:06:19 +0000

    Add package dz-locations/0.1.6/dz_locations-0.1.6.tar.gz

---
+++ packages/dz-locations/dz_locations-0.1.6.tar.gz/dz_locations-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dz_locations",
+    version="0.1.6",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=5.2",
+        "djangorestframework>=3.12"
+    ],
+    description="Algerian Wilaya & Commune data for Django projects",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Mohammed Taha Khamed",
+    url="https://github.com/khamedtaha/dz_locations",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
