fdc9d7acead52968095b24704720b4f93f0818ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:22 +0000

    Add package merobox/0.1.11/merobox-0.1.11.tar.gz

---
+++ packages/merobox/merobox-0.1.11.tar.gz/merobox-0.1.11/merobox/commands/__init__.py
@@ -0,0 +1,34 @@
+"""
+Commands module - All available CLI commands.
+"""
+
+from merobox.commands.manager import CalimeroManager
+from merobox.commands.run import run
+from merobox.commands.stop import stop
+from merobox.commands.list import list
+from merobox.commands.logs import logs
+from merobox.commands.health import health
+from merobox.commands.install import install
+from merobox.commands.nuke import nuke
+from merobox.commands.context import context
+from merobox.commands.identity import identity
+from merobox.commands.bootstrap import bootstrap
+from merobox.commands.call import call
+from merobox.commands.join import join
+from merobox.commands.docs import docs
+
+__all__ = [
+    "CalimeroManager",
+    "run",
+    "stop",
+    "list",
+    "logs",
+    "health",
+    "install",
+    "nuke",
+    "context",
+    "identity",
+    "bootstrap",
+    "call",
+    "join",
+]
