31339bdbd4795d7cc0e1b391633f08a768c4194f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:06:32 +0000

    Add package pycoro/0.1.20/pycoro-0.1.20.tar.gz

---
+++ packages/pycoro/pycoro-0.1.20.tar.gz/pycoro-0.1.20/src/pycoro/subsystems/function.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from queue import Full, Queue, ShutDown
+from threading import Thread
+from typing import TYPE_CHECKING, Any
+
+from pycoro.bus import CQE, SQE
+
+if TYPE_CHECKING:
+    from pycoro.aio import AIO
+
+
+class FunctionSubsystem:
+    def __init__(self, aio: AIO, size: int = 100, workers: int = 1) -> None:
+        self._aio = aio
+        self._sq = Queue[SQE[Callable[[], Any], Any]](size)
+        self._workers = workers
+        self._threads: list[Thread] = []
+
+    @property
+    def size(self) -> int:
+        return self._sq.maxsize
+
+    @property
+    def kind(self) -> str:
+        return "function"
+
+    def start(self) -> None:
+        assert len(self._threads) == 0
+
+        for _ in range(self._workers):
+            t = Thread(target=self.worker, daemon=True)
+            t.start()
+            self._threads.append(t)
+
+    def shutdown(self) -> None:
+        assert len(self._threads) == self._workers
+        self._sq.shutdown()
+        for t in self._threads:
+            t.join()
+
+        self._threads.clear()
+        self._sq.join()
+
+    def enqueue[T](self, sqe: SQE[Callable[[], T], T]) -> bool:
+        try:
+            self._sq.put_nowait(sqe)
+        except Full:
+            return False
+        return True
+
+    def flush(self, time: int) -> None:
+        return
+
+    def process[T](self, sqes: list[SQE[Callable[[], T], T]]) -> list[CQE[T]]:
+        assert self._workers > 0, "must be at least one worker"
+        sqe = sqes[0]
+        return [CQE(sqe.v(), sqe.cb)]
+
+    def worker(self) -> None:
+        while True:
+            try:
+                sqe = self._sq.get()
+            except ShutDown:
+                break
+
+            self._aio.enqueue((self.process([sqe])[0], "function"))
+            self._sq.task_done()
---
+++ packages/pycoro/pycoro-0.1.20.tar.gz/pycoro-0.1.20/src/pycoro/subsystems/store/__init__.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from collections.abc import Hashable
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Protocol, assert_never
+
+from pycoro.bus import CQE, SQE
+
+if TYPE_CHECKING:
+    from queue import Queue
+
+
+# Submission
+@dataclass(frozen=True)
+class StoreSubmission:
+    transaction: Transaction
+
+    @property
+    def kind(self) -> str:
+        return "store"
+
+
+@dataclass(frozen=True)
+class Transaction[T: Hashable]:
+    cmds: list[T]
+
+
+# Completion
+@dataclass(frozen=True)
+class StoreCompletion:
+    results: list[Any]
+
+    @property
+    def kind(self) -> str:
+        return "store"
+
+
+class StoreSubsystem(Protocol):
+    def execute(self, transactions: list[Transaction]) -> list[list[Any]]: ...
+
+
+def process(
+    store: StoreSubsystem,
+    sqes: list[SQE[StoreSubmission, StoreCompletion]],
+) -> list[CQE[StoreCompletion]]:
+    transactions = [sqe.v.transaction for sqe in sqes]
+
+    try:
+        result = store.execute(transactions)
+        assert len(transactions) == len(result), "transactions and results must have equal length"
+    except Exception as e:
+        result = e
+
+    return [
+        CQE(result if isinstance(result, Exception) else StoreCompletion(result[i]), sqe.cb)
+        for i, sqe in enumerate(sqes)
+    ]
+
+
+def collect(
+    c: Queue[SQE[StoreSubmission, StoreCompletion] | int], n: int
+) -> list[SQE[StoreSubmission, StoreCompletion]]:
+    assert n > 0, "batch size must be greater than 0"
+
+    batch: list[SQE[StoreSubmission, StoreCompletion]] = []
+    for _ in range(n):
+        sqe = c.get()
+
+        match sqe:
+            case SQE():
+                batch.append(sqe)
+                c.task_done()
+            case int():
+                c.task_done()
+                return batch
+            case _:
+                assert_never(sqe)
+
+    return batch
