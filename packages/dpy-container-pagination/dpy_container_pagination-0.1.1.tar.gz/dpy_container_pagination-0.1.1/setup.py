78f0166aa90ffe136d6f9bd2b5de4b4e6d579bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 18:05:51 +0000

    Add package dpy-container-pagination/0.1.1/dpy_container_pagination-0.1.1.tar.gz

---
+++ packages/dpy-container-pagination/dpy_container_pagination-0.1.1.tar.gz/dpy_container_pagination-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="dpy-container-pagination",
+    version="0.1.1",
+    description="Easily create pagination for your containers.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=["Paginator"],
+    package_dir={'': "src"},
+    url="https://github.com/Migan178/dpy-container-pagination",
+    author="Migan178",
+    author_email="me@migan.co.kr",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ]
+)
