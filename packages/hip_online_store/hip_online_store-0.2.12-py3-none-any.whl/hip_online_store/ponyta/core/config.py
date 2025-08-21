ab2497db0d1fde03ffdf625f622d6b351e7be013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:26 +0000

    Add package hip_online_store/0.2.12/hip_online_store-0.2.12-py3-none-any.whl

---
+++ packages/hip_online_store/hip_online_store-0.2.12-py3-none-any.whl/hip_online_store/ponyta/core/config.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    AWS_DEFAULT_REGION: str
+    AWS_DEFAULT_REGION = "ap-southeast-2"
+    DATABRICKS_CLUSTER_HOST: str
+    DATABRICKS_CLUSTER_HOST = os.getenv("DATABRICKS_PROD_CLUSTER_HOST")
+    DATABRICKS_PAT_TOKEN: str
+    DATABRICKS_PAT_TOKEN = os.getenv("DATABRICKS_PAT_TOKEN")
+    DATABRICKS_SQL_CLUSTER_PATH: str
+    DATABRICKS_SQL_CLUSTER_PATH = os.getenv("DATABRICKS_SQL_CLUSTER_PATH")
+    ONFS_PREFIX: str
+    ONFS_PREFIX = "ponyta"
+    UNITY_CATALOG: str
+    UNITY_CATALOG = os.getenv("UNITY_CATALOG")
+    DATABRICKS_TOKEN_URL: str
+    DATABRICKS_TOKEN_URL = os.getenv("DATABRICKS_TOKEN_URL")
+    DATABRICKS_ACCEPT_PROFILE: str
+    DATABRICKS_ACCEPT_PROFILE = "ml_features"
+    DATABRICKS_CLIENT_ID: str
+    DATABRICKS_CLIENT_ID = os.getenv("DATABRICKS_CLIENT_ID")
+    DATABRICKS_CLIENT_SECRET: str
+    DATABRICKS_CLIENT_SECRET = os.getenv("DATABRICKS_CLIENT_SECRET")
+
+
+load_dotenv()
+settings = Settings()
