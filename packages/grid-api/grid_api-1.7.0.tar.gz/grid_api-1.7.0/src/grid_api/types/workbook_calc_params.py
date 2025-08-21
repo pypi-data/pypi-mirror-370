7744ab8a9fe922e388848135e8e19c41c84db68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 03:22:40 +0000

    Add package grid-api/1.7.0/grid_api-1.7.0.tar.gz

---
+++ packages/grid-api/grid_api-1.7.0.tar.gz/grid_api-1.7.0/src/grid_api/types/workbook_calc_params.py
@@ -0,0 +1,14 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, List, Union, Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["WorkbookCalcParams"]
+
+
+class WorkbookCalcParams(TypedDict, total=False):
+    read: Required[List[str]]
+
+    apply: Optional[Dict[str, Union[float, str, bool, None]]]
