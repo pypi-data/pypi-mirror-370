0881f5df29c368c15866fc44f20c26dae892613d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:07:08 +0000

    Add package gluestick/2.2.24/gluestick-2.2.24.tar.gz

---
+++ packages/gluestick/gluestick-2.2.24.tar.gz/gluestick-2.2.24/gluestick/__init__.py
@@ -0,0 +1,7 @@
+"""Import functions and classes to gluestick."""
+
+from .etl_utils import *  # noqa
+from .pandas_utils import *  # noqa
+from .singer import *  # noqa
+from .reader import *  # noqa
+
---
+++ packages/gluestick/gluestick-2.2.24.tar.gz/gluestick-2.2.24/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gluestick",
+    version="2.2.24",
+    description="ETL utility functions built on Pandas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hotgluexyz/gluestick",
+    install_requires=[
+        "singer-python>=4.0.0",
+        "numpy>=1.4",
+        "pandas>=1.2.5",
+        "pyarrow>=8.0.0",
+        "pytz>=2022.6"
+    ],
+    author="hotglue",
+    author_email="hello@hotglue.xyz",
+    license="MIT",
+    packages=["gluestick"],
+    zip_safe=False,
+)
