8bf6620c091b0b43adde887bdb2eff3c10f64f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:08:01 +0000

    Add package penaltyblog/1.5.1/penaltyblog-1.5.1-cp310-cp310-win32.whl

---
+++ packages/penaltyblog/penaltyblog-1.5.1-cp310-cp310-win32.whl/penaltyblog/implied/__init__.py
@@ -0,0 +1,17 @@
+from .implied import (  # noqa
+    additive,
+    differential_margin_weighting,
+    multiplicative,
+    odds_ratio,
+    power,
+    shin,
+)
+
+__all__ = [
+    "additive",
+    "differential_margin_weighting",
+    "multiplicative",
+    "odds_ratio",
+    "power",
+    "shin",
+]
