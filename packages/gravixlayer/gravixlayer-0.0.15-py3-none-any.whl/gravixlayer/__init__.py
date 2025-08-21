b7f6d61d3e9b873dc204206eacc19b52fc02d3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 05:07:34 +0000

    Add package gravixlayer/0.0.15/gravixlayer-0.0.15-py3-none-any.whl

---
+++ packages/gravixlayer/gravixlayer-0.0.15-py3-none-any.whl/gravixlayer/__init__.py
@@ -0,0 +1,36 @@
+"""
+GravixLayer Python SDK - OpenAI Compatible
+"""
+__version__ = "0.0.15"
+
+from .client import GravixLayer
+from .types.async_client import AsyncGravixLayer
+from .types.chat import (
+    ChatCompletion,
+    ChatCompletionMessage,
+    ChatCompletionChoice,
+    ChatCompletionUsage,
+    FunctionCall,
+    ToolCall,
+)
+from .types.embeddings import (
+    EmbeddingResponse,
+    EmbeddingObject,
+    EmbeddingUsage,
+)
+
+__all__ = [
+    "GravixLayer",
+    "AsyncGravixLayer",
+    "ChatCompletion",
+    "ChatCompletionMessage",
+    "ChatCompletionChoice",
+    "ChatCompletionUsage",
+    "FunctionCall",
+    "ToolCall",
+    "EmbeddingResponse",
+    "EmbeddingObject",
+    "EmbeddingUsage",
+]
+
+OpenAI = GravixLayer
