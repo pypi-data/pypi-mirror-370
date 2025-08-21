ec2ca2fefea2178ebdeefcf043f8934ecffda2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:06:35 +0000

    Add package pydiverse-transform/0.5.5/pydiverse_transform-0.5.5.tar.gz

---
+++ packages/pydiverse-transform/pydiverse_transform-0.5.5.tar.gz/pydiverse_transform-0.5.5/src/pydiverse/transform/_internal/tree/ast.py
@@ -0,0 +1,31 @@
+# Copyright (c) QuantCo and pydiverse contributors 2025-2025
+# SPDX-License-Identifier: BSD-3-Clause
+
+from collections.abc import Iterable
+from uuid import UUID
+
+
+class AstNode:
+    __slots__ = ["name"]
+
+    name: str
+
+    def clone(self) -> "AstNode":
+        return self._clone()[0]
+
+    def _clone(
+        self,
+    ) -> tuple["AstNode", dict["AstNode", "AstNode"], dict[UUID, UUID]]: ...
+
+    def iter_subtree_postorder(self) -> Iterable["AstNode"]: ...
+
+    def iter_subtree_preorder(self) -> Iterable["AstNode"]: ...
+
+    def __repr__(self) -> str:
+        return self.ast_repr(verb_depth=7, expr_depth=2)
+
+    def ast_repr(
+        self, verb_depth: int = -1, expr_depth: int = -1, *, oneline: bool = False
+    ) -> str: ...
+
+    def _ast_node_repr(self, expr_depth: int): ...
