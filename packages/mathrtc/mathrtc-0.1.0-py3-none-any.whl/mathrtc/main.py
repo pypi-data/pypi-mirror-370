d45a6f3bee8a9680aa59e653270985df8a2c4966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:26 +0000

    Add package mathrtc/0.1.0/mathrtc-0.1.0-py3-none-any.whl

---
+++ packages/mathrtc/mathrtc-0.1.0-py3-none-any.whl/mathrtc/main.py
@@ -0,0 +1,11 @@
+def fact():
+ # Factorial using recursion
+  def factorial(n):
+      if n == 0 or n == 1:
+          return 1
+      else:
+          return n * factorial(n - 1)
+
+# Example
+  num = int(input("Enter a number: "))
+  print("Factorial of", num, "is:", factorial(num))
