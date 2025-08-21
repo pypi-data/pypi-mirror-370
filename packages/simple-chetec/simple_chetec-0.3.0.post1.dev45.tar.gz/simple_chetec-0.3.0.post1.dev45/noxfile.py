5ac9aced0767d2344f29fd1d3cd6343dc3cd21c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 16:06:36 +0000

    Add package simple-chetec/0.3.0.post1.dev45/simple_chetec-0.3.0.post1.dev45.tar.gz

---
+++ packages/simple-chetec/simple_chetec-0.3.0.post1.dev45.tar.gz/simple_chetec-0.3.0.post1.dev45/noxfile.py
@@ -0,0 +1,23 @@
+import nox
+
+@nox.session(python=["3.9"])
+@nox.parametrize(
+    ("numpy", "matplotlib", "scipy", "h5py", "pyyaml"),
+    [("1.22", "3.5", "1.8", "3.6", "5.4")]
+)
+def test_minimum(session, numpy, matplotlib, scipy, h5py, pyyaml):
+    # Not all the minimum versions support newer versions of python
+    # e.g. cannot test numpy 1.22 with python 3.12
+    session.install(f"numpy=={numpy}")
+    session.install(f"matplotlib=={matplotlib}")
+    session.install(f"scipy=={scipy}")
+    session.install(f"h5py=={h5py}")
+    session.install(f"pyyaml=={pyyaml}")
+
+    session.install(".[test]")
+    session.run("pytest")
+
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+def test_latest(session):
+    session.install(".[test]")
+    session.run("pytest")
