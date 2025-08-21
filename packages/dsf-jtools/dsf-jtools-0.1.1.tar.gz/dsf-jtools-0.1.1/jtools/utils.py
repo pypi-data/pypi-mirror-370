177246e7eddcbeb89b3a52acbf29bf18ad806128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 15:05:34 +0000

    Add package dsf-jtools/0.1.1/dsf-jtools-0.1.1.tar.gz

---
+++ packages/dsf-jtools/dsf-jtools-0.1.1.tar.gz/dsf-jtools-0.1.1/jtools/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+from jtools import consts as C
+
+
+def get_trading_dates(market, startdate: str, enddate: str):
+    """获取交易日"""
+    exdates = C.HOLIDAYS.get(market, 'SH')
+    if startdate < "20241231":
+        fp_his = os.path.join(os.path.dirname(__file__), f'data/trddt_{market}.csv')
+        all_hisdates = pd.read_csv(fp_his, header=None, dtype=str)
+    else:
+        all_hisdates = []
+    all_bdates = pd.bdate_range(start=max(startdate, "20241231"), end=enddate).strftime("%Y%m%d").tolist()
+    all_trddts = list(sorted(set(all_hisdates + all_bdates) - set(exdates)))
+    return all_trddts
+
