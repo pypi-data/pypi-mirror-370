058b5d49e907044ec1e8698a92314d6505d536d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 14:05:21 +0000

    Add package fleet-python/0.2.28/fleet_python-0.2.28.tar.gz

---
+++ packages/fleet-python/fleet_python-0.2.28.tar.gz/fleet_python-0.2.28/fleet/_async/env/client.py
@@ -0,0 +1,29 @@
+from ..client import AsyncFleet, AsyncEnv
+from ...models import Environment as EnvironmentModel, AccountResponse
+from typing import List, Optional
+
+
+async def make_async(env_key: str, region: Optional[str] = None) -> AsyncEnv:
+    return await AsyncFleet().make(env_key, region=region)
+
+
+async def list_envs_async() -> List[EnvironmentModel]:
+    return await AsyncFleet().list_envs()
+
+
+async def list_regions_async() -> List[str]:
+    return await AsyncFleet().list_regions()
+
+
+async def list_instances_async(
+    status: Optional[str] = None, region: Optional[str] = None
+) -> List[AsyncEnv]:
+    return await AsyncFleet().instances(status=status, region=region)
+
+
+async def get_async(instance_id: str) -> AsyncEnv:
+    return await AsyncFleet().instance(instance_id)
+
+
+async def account_async() -> AccountResponse:
+    return await AsyncFleet().account()
