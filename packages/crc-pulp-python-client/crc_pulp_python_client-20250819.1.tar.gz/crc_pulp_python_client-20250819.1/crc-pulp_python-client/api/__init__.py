7f45abcad79ba9436a381dcabe2debe4e2d1f826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-21 04:07:05 +0000

    Add package crc-pulp-python-client/20250819.1/crc_pulp_python_client-20250819.1.tar.gz

---
+++ packages/crc-pulp-python-client/crc_pulp_python_client-20250819.1.tar.gz/crc_pulp_python_client-20250819.1/crc-pulp_python-client/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from crc-pulp_python-client.api.api_pypi_api import ApiPypiApi
+from crc-pulp_python-client.api.api_pypi_legacy_api import ApiPypiLegacyApi
+from crc-pulp_python-client.api.api_pypi_simple_api import ApiPypiSimpleApi
+from crc-pulp_python-client.api.content_packages_api import ContentPackagesApi
+from crc-pulp_python-client.api.distributions_pypi_api import DistributionsPypiApi
+from crc-pulp_python-client.api.publications_pypi_api import PublicationsPypiApi
+from crc-pulp_python-client.api.pypi_metadata_api import PypiMetadataApi
+from crc-pulp_python-client.api.remotes_python_api import RemotesPythonApi
+from crc-pulp_python-client.api.repositories_python_api import RepositoriesPythonApi
+from crc-pulp_python-client.api.repositories_python_versions_api import RepositoriesPythonVersionsApi
+
---
+++ packages/crc-pulp-python-client/crc_pulp_python_client-20250819.1.tar.gz/crc_pulp_python_client-20250819.1/services-pulp_python-client/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from services-pulp_python-client.api.api_pypi_api import ApiPypiApi
+from services-pulp_python-client.api.api_pypi_legacy_api import ApiPypiLegacyApi
+from services-pulp_python-client.api.api_pypi_simple_api import ApiPypiSimpleApi
+from services-pulp_python-client.api.content_packages_api import ContentPackagesApi
+from services-pulp_python-client.api.distributions_pypi_api import DistributionsPypiApi
+from services-pulp_python-client.api.publications_pypi_api import PublicationsPypiApi
+from services-pulp_python-client.api.pypi_metadata_api import PypiMetadataApi
+from services-pulp_python-client.api.remotes_python_api import RemotesPythonApi
+from services-pulp_python-client.api.repositories_python_api import RepositoriesPythonApi
+from services-pulp_python-client.api.repositories_python_versions_api import RepositoriesPythonVersionsApi
+
